c27d1186f6fef367cac6fabb3f16eec5ace43135
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-05-21 20:47:36 +0000

    Script flux instruction -> problème

--- trunk/app/vendors/shells/instruction.php
+++ trunk/app/vendors/shells/instruction.php
@@ -1,4 +1,61 @@
 <?php
+class ArrayToXML
+{
+    /**
+     * The main function for converting to an XML document.
+     * Pass in a multi dimensional array and this recrusively loops through and builds up an XML document.
+     *
+     * @param array $data
+     * @param string $rootNodeName - what you want the root node to be - defaultsto data.
+     * @param SimpleXMLElement $xml - should only be used recursively
+     * @return string XML
+     */
+    public static function toXml($data, $rootNodeName = 'data', $xml=null)
+    {
+        // turn off compatibility mode as simple xml throws a wobbly if you don't.
+        if (ini_get('zend.ze1_compatibility_mode') == 1)
+        {
+            ini_set ('zend.ze1_compatibility_mode', 0);
+        }
+
+        if ($xml == null)
+        {
+            $xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
+        }
+
+        // loop through the data passed in.
+        foreach($data as $key => $value)
+        {
+            // no numeric keys in our xml please!
+            if (is_numeric($key))
+            {
+                // make string key...
+                $key = "unknownNode_". (string) $key;
+            }
+
+            // replace anything not alpha numeric
+            $key = preg_replace('/[^a-z]/i', '', $key);
+
+            // if there is another array found recrusively call this function
+            if (is_array($value))
+            {
+                $node = $xml->addChild($key);
+                // recrusive call.
+                ArrayToXML::toXml($value, $rootNodeName, $node);
+            }
+            else
+            {
+                // add single node.
+                                $value = htmlentities($value);
+                $xml->addChild($key,$value);
+            }
+
+        }
+        // pass back as string. or simple xml object if you want!
+        return $xml->asXML();
+    }
+}
+
     class InstructionShell extends Shell
     {
         var $uses = array( 'Dossier', 'Foyer', 'Personne' );
@@ -45,86 +102,95 @@
         function endElement( $xmlParser, $currentTagName ) {
             $currentTagName = strtolower( $currentTagName );
 
-            if( $currentTagName == 'demandersa' ) {
-                $demandersa = $this->_cdataElements['racine']['infodemandersa']['identificationrsa']['demandersa'];
-                $this->Dossier->create();
-                $this->Dossier->save( $demandersa );
+            switch( $currentTagName ) {
+                case 'infodemandersa':
+                /**
+                    Fin d'un dossier RSA
+                */
+                    $infodemandersa = simplexml_load_string( ArrayToXML::toXml( $this->_cdataElements['racine']['infodemandersa'], 'infodemandersa' ) );
+                    debug( $infodemandersa );
+                    $this->_cdataElements['racine']['infodemandersa'] = array();
+                    $this->_dossier = null;
+                    $this->_foyer = null;
+                    break;
+                /**
 
-                $this->_dossier = $this->Dossier->find(
-                    'first',
-                    array(
-                        'conditions' => array( 'numdemrsa' => $demandersa['numdemrsa'] )
-                    )
-                );
-
-                //-------------------------------------------------------------
-
-                if( !empty( $this->_cdataElements['racine']['infodemandersa']['donneesadministratives'] ) ) {
-                    $foyer = $this->_cdataElements['racine']['infodemandersa']['donneesadministratives']['logement'];
-                }
-
-                $this->_foyer = $this->Foyer->find(
-                    'first',
-                    array(
-                        'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] )
-                    )
-                );
-
-                if( !empty( $this->_foyer ) ) {
-                    $foyer['id'] = $this->_foyer['Foyer']['id'];
-                }
-
-                $foyer['dossier_rsa_id'] = $this->_dossier['Dossier']['id'];
-                $this->Foyer->create();
-                $this->Foyer->save( $foyer );
-
-                $this->_foyer = $this->Foyer->find(
-                    'first',
-                    array(
-                        'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] )
-                    )
-                );
-
-                //-------------------------------------------------------------
-debug( $this->_cdataElements['racine'] );
-//                 if( !empty( $this->_cdataElements['racine']['infodemandersa']['donneesadministratives'] ) ) {
+                */
+//                 case 'demandersa':
+//                     $demandersa = $this->_cdataElements['racine']['infodemandersa']['identificationrsa']['demandersa'];
+//
+//                     $this->_dossier = $this->Dossier->find(
+//                         'first',
+//                         array(
+//                             'conditions' => array( 'numdemrsa' => $demandersa['numdemrsa'] ),
+//                             'recursive' => -1
+//                         )
+//                     );
+//
+//                     $this->_dossier['Dossier'] = Set::merge( $this->_dossier['Dossier'], $demandersa );
+//
+//                     $this->Dossier->create();
+//                     $this->Dossier->save( $this->_dossier['Dossier'] );
+//
+//                     $this->_dossier = $this->Dossier->find(
+//                         'first',
+//                         array(
+//                             'conditions' => array( 'numdemrsa' => $demandersa['numdemrsa'] ),
+//                             'recursive' => -1
+//                         )
+//                     );
+//
+//                     assert( !empty( $this->_dossier ) );
+//                     break;
+                /**
+                */
+//                 case 'logement':
 //                     $foyer = $this->_cdataElements['racine']['infodemandersa']['donneesadministratives']['logement'];
-//                 }
-
-//                 $this->_foyer = $this->Foyer->find(
-//                     'first',
-//                     array(
-//                         'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] )
-//                     )
-//                 );
+//                     $foyer['dossier_rsa_id'] = $this->_dossier['Dossier']['id'];
 //
-//                 if( !empty( $this->_foyer ) ) {
-//                     $foyer['id'] = $this->_foyer['Foyer']['id'];
-//                 }
+//                     $this->_foyer = $this->Foyer->find(
+//                         'first',
+//                         array(
+//                             'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] ),
+//                             'recursive' => -1
+//                         )
+//                     );
 //
-//                 $foyer['dossier_rsa_id'] = $this->_dossier['Dossier']['id'];
-//                 $this->Foyer->create();
-//                 $this->Foyer->save( $foyer );
+//                     $this->_foyer['Foyer'] = Set::merge( $this->_foyer['Foyer'], $foyer );
+//                     $this->Foyer->create();
+//                     $this->Foyer->save( $this->_foyer['Foyer'] );
 //
-//                 $this->_foyer = $this->Foyer->find(
-//                     'first',
-//                     array(
-//                         'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] )
-//                     )
-//                 );
-
-                //-------------------------------------------------------------
-
-                $this->_cdataElements = array();
+//                     $this->_foyer = $this->Foyer->find(
+//                         'first',
+//                         array(
+//                             'conditions' => array( 'dossier_rsa_id' => $this->_dossier['Dossier']['id'] ),
+//                             'recursive' => -1
+//                         )
+//                     );
+//                     assert( !empty( $this->_foyer ) );
+//                     break;
+                /**
+                */
+//                 case 'personne':
+//                     debug( $this->_foyer );
+//                     break;
+//                     $personne = $this->_cdataElements['racine']['infodemandersa']['personne']['identification'];
+//                     $personne['foyer_id'] = $this->_foyer['Foyer']['id'];
+// //                     debug( $personne );
+//                     break;
             }
+
+            //-----------------------------------------------------------------
+
 //             else if( $currentTagName == 'personne' ) { // Personne/Identification
 //                 // FIXME: trouver si la personne  existe  pour faire les modifs
 //                 $personne = $this->_cdataElements['racine']['infodemandersa']['personne']['identification'];
 //                 $personne['foyer_id'] = $this->_foyer['Foyer']['id'];
-//                 $this->Personne->create();
-//                 $this->Personne->save( $personne );
+//                 debug( $personne );
+// //                 $this->Personne->create();
+// //                 $this->Personne->save( $personne );
 //
-// //                 debug( $personne );
+//                 $this->_cdataElements['racine']['infodemandersa']['personne'] = array();
 //             }
             array_pop( $this->_stack );
         }
@@ -132,9 +198,13 @@ debug( $this->_cdataElements['racine'] );
         //*********************************************************************
 
         function main() {
-            // FIXME: assert isset $this->Dispatch->args[0] file_exists( $this->Dispatch->args[0] )
             $this_start = microtime( true );
             echo "Démarrage: ".date( 'Y-m-d H:i:s' )."\n";
+
+            //-----------------------------------------------------------------
+
+            assert( !empty( $this->Dispatch->args[0] ) );
+            assert( file_exists( $this->Dispatch->args[0] ) );
 
             //-----------------------------------------------------------------
 
