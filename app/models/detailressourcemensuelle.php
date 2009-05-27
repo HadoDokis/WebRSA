af8ce6d237420db80cf46d11cf0681aa168e684f
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-05-27 15:15:15 +0000

    Réparation du Wizard: dates des ressources

---
+++ app/models/detailressourcemensuelle.php
@@ -0,0 +1,15 @@
+<?php
+    // FIXME: passe pas dedans ?
+    class Detailressourcemensuelle extends AppModel
+    {
+        var $name = 'Detailressourcemensuelle';
+        var $useTable = 'detailsressourcesmensuelles';
+
+        var $validate = array(
+            'dfpercress' => array(
+                'rule' => 'date',
+                'message' => 'Veuillez entrer une date valide'
+            )
+        );
+    }
+?>
