4e205423eb3cfe0a8ac18cdba2f230a76d62cd5e
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-06-03 22:29:49 +0000

    Patch modif table servstructures referentes

--- trunk/app/config/sql/patches/20090604-00h20-patch-servicesreferents.sql
+++ trunk/app/config/sql/patches/20090604-00h20-patch-servicesreferents.sql
@@ -1,2 +1,5 @@
 ALTER TABLE servicesinstructeurs DROP COLUMN nom_rue;
 ALTER TABLE servicesinstructeurs ADD COLUMN nom_rue VARCHAR(100);
+
+ALTER TABLE structuresreferentes DROP COLUMN lib_struc;
+ALTER TABLE structuresreferentes ADD COLUMN lib_struc VARCHAR(100);
