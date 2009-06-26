ce6f070309b6451945ae5cb09c5bcf34bd7fb7a5
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-06-26 16:29:53 +0000

    Ajout de commentaires

--- app/mysvn.sh
+++ app/mysvn.sh
@@ -13,14 +13,22 @@ echo "" > tmp/logs/debug.log
 echo "" > tmp/logs/error.log
 
 # http://snipt.net/nick/svn-delete-all-files-marked-for-deletion/
-svn status |grep '^!' |sed 's/^!      /svn delete "/g' |sed 's/$/"/g' |sh
+svn status |grep '^!' |sed 's/^!      /svn delete "/g' | sed 's/$/"/g' | sh
 
 # http://snipt.net/nick/svn-add-all-files-marked-for-add/
-svn status |grep '^?' |sed 's/^?      /svn add "/g' |sed 's/$/"/g' |sh
+svn status |grep '^?' |sed 's/^?      /svn add "/g' | sed 's/$/"/g' | sh
 
 # svn status | grep "^\?" | sed -e 's/? *//' | sed -e 's/ /\\ /g' | xargs svn add
 # svn status | grep "^\!" | sed -e 's/! *//' | sed -e 's/ /\\ /g' | xargs svn del
 
+# http://svnbook.red-bean.com/en/1.1/ch07s02.html
 NOW=`date +"%Y-%m-%d %H:%M:%S %z (%a, %d %b %Y)"`
 sed -i "s/\\\$LastChangedDate[^\\\$]*\\\$/\$LastChangedDate: $NOW\$/g" views/elements/footer.ctp
 svn propset svn:keywords "Date Author Revision" views/elements/footer.ctp
+
+# svn copy \
+# svn+ssh://cbuffin@svn.adullact.net/svnroot/webrsa/trunk/ \
+# svn+ssh://cbuffin@svn.adullact.net/svnroot/webrsa/tags/1.0.2.1/ \
+# -m "Version 1.0.2.1 (installation CG93 23/06/09 12h52) avec correction bug modes contact"
+
+# svn export svn+ssh://cbuffin@svn.adullact.net/svnroot/webrsa/tags/1.0.2.1/
