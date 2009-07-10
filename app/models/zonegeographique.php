bb4aaa49d9769718c8da1929af5d6b5908c1fcc4
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-07-10 13:39:03 +0000

    Assignation des droits par défaut en fonction du groupe auquel on appartient

--- app/models/zonegeographique.php
+++ app/models/zonegeographique.php
@@ -3,6 +3,7 @@
     {
         var $name = 'Zonegeographique';
         var $useTable = 'zonesgeographiques';
+        var $displayField = 'libelle';
 
         //---------------------------------------------------------------------
 
