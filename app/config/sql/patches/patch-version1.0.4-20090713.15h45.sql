38098df389438ac3b4da648223b41eb6238858d0
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-07-13 16:07:53 +0000

    Patch pour remplacer dossierscaf.numdemrsaprece de char(11) en varchar(11) + mise à jour des ids pour prestations

---
+++ app/config/sql/patches/patch-version1.0.4-20090713.15h45.sql
@@ -0,0 +1,3 @@
+--------------- Ajout du 13/07/2009 à 15h45 ------------------
+ALTER TABLE dossierscaf ALTER COLUMN numdemrsaprece TYPE VARCHAR(11);
+ALTER TABLE dossierscaf ALTER COLUMN numdemrsaprece SET DEFAULT NULL;
