e45298e2f5259f2217ed392ac22c461afae83365
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-07-29 14:47:55 +0000

    Modification de la table structuresreferentes_zonesgeographiques pour maintient de la restriction des zones

--- app/config/sql/patches/patch-version1.0.5-20090724.12h30.sql
+++ app/config/sql/patches/patch-version1.0.5-20090724.12h30.sql
@@ -1,2 +1,7 @@
 --------------- Ajout du 24/07/2009 à 12h30 ------------------
 ALTER TABLE prestations ADD CONSTRAINT personneidfk FOREIGN KEY (personne_id) REFERENCES personnes (id) MATCH FULL;
+
+--------------- Ajout du 29/07/2009 à 16h41 ------------------
+ALTER TABLE structuresreferentes_zonesgeographiques DROP CONSTRAINT structuresreferentes_zonesgeographiques_pkey;
+ALTER TABLE structuresreferentes_zonesgeographiques ADD COLUMN id SERIAL NOT NULL PRIMARY KEY;
+ALTER TABLE structuresreferentes ADD COLUMN filtre_zone_geo BOOLEAN DEFAULT true;
