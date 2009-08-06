a48fc77bb4626b75a215951cb80a497eb1a8378a
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-08-06 14:15:54 +0000

    Nettoyage des sources

---
+++ app/config/sql/patches/patch-version1.0.6-20090726-16h41.sql
@@ -0,0 +1,7 @@
+--------------- Ajout du 29/07/2009 à 16h41 ------------------
+ALTER TABLE structuresreferentes_zonesgeographiques DROP CONSTRAINT structuresreferentes_zonesgeographiques_pkey;
+ALTER TABLE structuresreferentes_zonesgeographiques ADD COLUMN id SERIAL NOT NULL PRIMARY KEY;
+ALTER TABLE structuresreferentes ADD COLUMN filtre_zone_geo BOOLEAN DEFAULT true;
+
+--------------- Ajout du 29/07/2009 à 16h41 ------------------
+ALTER TABLE contratsinsertion ADD COLUMN forme_ci CHAR(1);
