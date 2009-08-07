9f6049b3dbe7db36808a4102e10ca7817698de13
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-08-07 09:47:34 +0000

    Ajout des recours

---
+++ app/controllers/recours_controller.php
@@ -0,0 +1,52 @@
+<?php 
+
+    class RecoursController extends AppController{
+        var $name = 'Recours';
+        var $uses = array( 'Infofinanciere', 'Option' );
+
+
+        function gracieux( $dossier_rsa_id = null ){
+
+            $this->assert( valid_int( $dossier_rsa_id ), 'invalidParameter' );
+
+            $recours = $this->Infofinanciere->find(
+                'first',
+                array(
+                    'conditions' => array(
+                        'Infofinanciere.dossier_rsa_id' => $dossier_rsa_id
+                    ),
+                'recursive' => -1
+                )
+
+            );
+
+            $this->assert( !empty( $recours ), 'error404' );
+
+            $this->set( 'dossier_rsa_id', $dossier_rsa_id );
+            $this->set( 'recours', $recours );
+        }
+
+        function contentieux( $dossier_rsa_id = null ){
+
+            $this->assert( valid_int( $dossier_rsa_id ), 'invalidParameter' );
+
+            $recours = $this->Infofinanciere->find(
+                'first',
+                array(
+                    'conditions' => array(
+                        'Infofinanciere.dossier_rsa_id' => $dossier_rsa_id
+                    ),
+                'recursive' => -1
+                )
+
+            );
+
+            $this->assert( !empty( $recours ), 'error404' );
+
+
+
+            $this->set( 'dossier_rsa_id', $dossier_rsa_id );
+            $this->set( 'recours', $recours );
+        }
+    }
+?>
---
+++ app/views/recours/contentieux.ctp
@@ -0,0 +1,17 @@
+<?php  $this->pageTitle = 'Recours contentieux';?>
+
+<?php  echo $this->element( 'dossier_menu', array( 'id' => $dossier_rsa_id ) );?>
+
+
+<div class="with_treemenu">
+    <h1><?php echo $this->pageTitle;?></h1>
+
+    <?php if( empty( $recours ) ):?>
+        <p class="notice">Ce dossier ne possède pas encore de détails sur les droits.</p>
+
+    <?php else:?>
+        <p class="notice">On va le créer</p>
+
+    <?php endif;?>
+</div>
+<div class="clearer"><hr /></div>
