3ed3ed2ed6a5828a3c8bdad19e28249323e209f9
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-08-10 13:25:30 +0000

    Ajout du -1 pour l'état du dossier RSA lorsque le champ est vide

--- app/models/option.php
+++ app/models/option.php
@@ -366,13 +366,14 @@
 
         function etatdosrsa() {
             return array(
-                '0' => 'Nouvelle demande en attente de décision CG pour ouverture du droit',
-                '1' => 'Droit refusé',
-                '2' => 'Droit ouvert et versable',
-                '3' => 'Droit ouvert et suspendu (le montant du droit est calculable, mais l\'existence du droit est remis en cause)',
-                '4' => 'Droit ouvert mais versement suspendu (le montant du droit n\'est pas calculable)',
-                '5' => 'Droit clos',
-                '6' => 'Droit clos sur mois antérieur ayant eu des créances transferées ou une régularisation dans le mois de référence pour une période antérieure.'
+                '-1' => 'Non défini',
+                '0'  => 'Nouvelle demande en attente de décision CG pour ouverture du droit',
+                '1'  => 'Droit refusé',
+                '2'  => 'Droit ouvert et versable',
+                '3'  => 'Droit ouvert et suspendu (le montant du droit est calculable, mais l\'existence du droit est remis en cause)',
+                '4'  => 'Droit ouvert mais versement suspendu (le montant du droit n\'est pas calculable)',
+                '5'  => 'Droit clos',
+                '6'  => 'Droit clos sur mois antérieur ayant eu des créances transferées ou une régularisation dans le mois de référence pour une période antérieure.'
 
             );
         }
