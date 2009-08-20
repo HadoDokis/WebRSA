cd7aa27ac7db299b0d4ecc922d15412bdbb3dad3
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-08-20 14:34:02 +0000

    Ajout d'un patch sur la table structure referente (2 champs augmentés)

---
+++ trunk/app/config/sql/patches/patch-version1.0.7-20090820-15h19.sql
@@ -0,0 +1,3 @@
+--------------- Ajout du 20/08/2009 à 15h20 ------------------
+ALTER TABLE structuresreferentes ALTER COLUMN num_voie TYPE VARCHAR(15);
+ALTER TABLE structuresreferentes ALTER COLUMN nom_voie TYPE VARCHAR(50);
