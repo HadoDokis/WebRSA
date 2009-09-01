89e5a61adc5cd3285e697150e54fc70ecfb6c256
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-09-01 08:22:00 +0000

    Modification du model suivi insertion

--- trunk/app/models/suiviinsertion.php
+++ trunk/app/models/suiviinsertion.php
@@ -3,52 +3,6 @@
     {
         var $name = 'Suiviinsertion';
 
-
-        function search( $criteres ) {
-//             /// Conditions de base
-//             $conditions = array();
-// 
-//             /// Critères
-//             $mois = Set::extract( $criteres, 'Filtre.dtcreaflux' );
-// 
-//             /// Date du flux financier
-//             if( !empty( $mois ) && dateComplete( $criteres, 'Filtre.dtcreaflux' ) ) {
-//                 $mois = $mois['month'];
-//                 $conditions[] = 'EXTRACT(MONTH FROM Identificationflux.dtcreaflux) = '.$mois;
-//             }
-// 
-//             /// Requête
-//             $this->Dossier =& ClassRegistry::init( 'Dossier' );
-// 
-//             $query = array(
-//                 'fields' => array(
-//                     '"Totalisationacompte"."type_totalisation"',
-//                     'SUM("Totalisationacompte"."mttotsoclrsa") AS "Totalisationacompte__mttotsoclrsa"',
-//                     'SUM("Totalisationacompte"."mttotsoclmajorsa") AS "Totalisationacompte__mttotsoclmajorsa"',
-//                     'SUM("Totalisationacompte"."mttotlocalrsa") AS "Totalisationacompte__mttotlocalrsa"',
-//                     'SUM("Totalisationacompte"."mttotrsa") AS "Totalisationacompte__mttotrsa"'
-//                 ),
-//                 'recursive' => -1,
-//                 'joins' => array(
-//                     array(
-//                         'table'      => 'identificationsflux',
-//                         'alias'      => 'Identificationflux',
-//                         'type'       => 'INNER',
-//                         'foreignKey' => false,
-//                         'conditions' => array( 'Totalisationacompte.identificationflux_id = Identificationflux.id' ),
-//                     )
-//                 ),
-//                 'group' => array(
-//                     'Totalisationacompte.type_totalisation',
-//                     'Totalisationacompte.id'
-//                 ),
-//                 'order' => array( '"Totalisationacompte"."id" ASC' ),
-//                 'conditions' => $conditions
-//             );
-// 
-//             return $query;
-// 
-        }
     }
 
 ?>
