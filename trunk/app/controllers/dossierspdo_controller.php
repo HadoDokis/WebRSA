8fc82e053cecd948508e39022c7eed60ab684503
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-09-02 15:12:35 +0000

    Modification du controleur avec déplacement d'une requête dans le modèle

--- trunk/app/controllers/dossierspdo_controller.php
+++ trunk/app/controllers/dossierspdo_controller.php
@@ -11,6 +11,9 @@
             $this->set( 'avisdero', $this->Option->avisdero() );
             $this->set( 'typdero', $this->Option->typdero() );
             $this->set( 'pieecpres', $this->Option->pieecpres() );
+            $this->set( 'commission', $this->Option->commission() );
+            $this->set( 'motidempdo', $this->Option->motidempdo() );
+            $this->set( 'motideccg', $this->Option->motideccg() );
         }
 
 
@@ -32,6 +35,18 @@
             /// Récupération des listes des PDO
             $options = $this->Dossierpdo->prepare( 'derogation', array( 'conditions' => $conditions ) );
             $pdos = $this->Derogation->find( 'all', $options );
+
+            /// Calcul du numéro du PDO
+            $nbPDO = $this->Derogation->find(
+                'count',
+                array(
+                    'conditions' => array(
+                        'Derogation.avispcgpersonne_id' => $pdos[0]['Avispcgpersonne']['id']
+                    ),
+                    'order' => 'Derogation.ddavisdero DESC'
+                )
+            );
+            $this->set( 'nbPDO', $nbPDO );
 
             $this->set( 'dossier_rsa_id', $dossier_rsa_id );
             $this->set( 'pdos', $pdos );
@@ -55,43 +70,64 @@
         }
 
 
-        function _add_edit( $dossier_rsa_id = null ){
-                        // Vérification du format de la variable
-            $this->assert( valid_int( $dossier_rsa_id ), 'invalidParameter' );
+        function _add_edit( $id = null ) {
+            // Vérification du format de la variable
+            $this->assert( valid_int( $id ), 'invalidParameter' );
 
-            $dossier_rsa_id = $this->Dossier->findById( $dossier_rsa_id );
-            $this->assert( !empty( $dossier_rsa_id ), 'invalidParameter' );
+            if( $this->action == 'add' ) {
+                $dossier_rsa_id = $id;
+                $dossier_rsa = $this->Dossier->findById( $dossier_rsa_id );
+                $this->assert( !empty( $dossier_rsa ), 'invalidParameter' );
+            }
+            else if( $this->action == 'edit' ) {
+                $derogation_id = $id;
+                $dossier_rsa_id = $this->Derogation->dossierId( $derogation_id );
+                $dossier_rsa = $this->Dossier->findById( $dossier_rsa_id );
+                $this->assert( !empty( $dossier_rsa ), 'invalidParameter' );
+            }
 
-            $this->Dossier->begin();
+//              debug( $this->data );
+
+            $this->Derogation->begin();
 
             if( !$this->Jetons->check( $dossier_rsa_id ) ) {
-                $this->Dossier->rollback();
+                $this->Derogation->rollback();
             }
             $this->assert( $this->Jetons->get( $dossier_rsa_id ), 'lockedDossier' );
 
             // Essai de sauvegarde
             if( !empty( $this->data ) ) {
-                if( $this->Dossier->saveAll( $this->data, array( 'validate' => 'only', 'atomic' => false ) ) ) {
-                    if( $this->Dossier->saveAll( $this->data, array( 'validate' => 'first', 'atomic' => false ) ) ) {
+                if( $this->Derogation->saveAll( $this->data, array( 'validate' => 'only', 'atomic' => false ) ) ) {
+                    if( $this->Derogation->saveAll( $this->data, array( 'validate' => 'first', 'atomic' => false ) ) ) {
 
                         $this->Jetons->release( $dossier_rsa_id );
-                        $this->Dossier->commit();
+                        $this->Derogation->commit();
                         $this->Session->setFlash( 'Enregistrement effectué', 'flash/success' );
-//                         $this->redirect( array(  'controller' => 'dossierspdo','action' => 'index', $this->data['Derogation']['id'] ) );
+                        $this->redirect( array(  'controller' => 'dossierspdo','action' => 'index', $dossier_rsa_id/*$this->data['Derogation']['id']*/ ) );
                     }
                     else {
                         $this->Session->setFlash( 'Erreur lors de l\'enregistrement', 'flash/error' );
                     }
                 }
             }
-            // Afficage des données
-            else {
-
+            // Affichage des données
+            else if( $this->action == 'add' ) {
+                $avispcgpersonne_id = $this->Avispcgpersonne->idFromDossierId( $dossier_rsa_id );
+                $this->assert( !empty( $avispcgpersonne_id ), 'invalidParameter' );
+                $this->data['Derogation']['avispcgpersonne_id'] = $avispcgpersonne_id;
             }
+            else if( $this->action == 'edit' ) {
+                // ...
+                $derogation = $this->Derogation->findById( $id, null, null, -1 );
+                $this->assert( !empty( $derogation ), 'invalidParameter' );
 
-            $this->Dossier->commit();
+                // Assignation au formulaire
+                $this->data = $derogation;
+            }
+            $this->Derogation->commit();
+
+            $this->set( 'dossier_rsa_id', $dossier_rsa_id );
             $this->render( $this->action, null, 'add_edit' );
-            debug( $this->action );
         }
     }
 
