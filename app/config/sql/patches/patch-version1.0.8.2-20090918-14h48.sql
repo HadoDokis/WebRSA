f9362d82772b58d948908bb112d02447ab166235
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-09-18 14:34:20 +0000

    Ajout d'un champ commentaire_action à la table contratsinsertion + partie actions/aides antérieures commentée

--- app/config/sql/patches/patch-version1.0.8.2-20090918-14h48.sql
+++ app/config/sql/patches/patch-version1.0.8.2-20090918-14h48.sql
@@ -1,2 +1,4 @@
 --------------- Ajout du 18/09/2009 à 14h48 ------------------
 ALTER TABLE propospdos ADD COLUMN motifpdo CHAR(1);
+
+ALTER TABLE contratsinsertion ADD COLUMN commentaire_action TEXT;
