dde5ed39723e88d50032d619ebb7f94d38a8398d
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-09-22 14:33:24 +0000

    Mise en place de l'impression par cohorte des relances

--- app/models/relance.php
+++ app/models/relance.php
@@ -1,5 +1,6 @@
 <?php
     App::import( 'Sanitize' );
+
     class Relance extends AppModel
     {
         var $name = 'Relance';
@@ -17,6 +18,107 @@
             )
         );
 
+        var $_querydata = array(
+            'gedooo' => array(
+                'fields' => array(
+                    '"Orientstruct"."id"',
+                    '"Orientstruct"."personne_id"',
+                    '"Orientstruct"."typeorient_id"',
+                    '"Orientstruct"."structurereferente_id"',
+                    '"Orientstruct"."date_valid"',
+                    '"Orientstruct"."statut_orient"',
+                    '"Orientstruct"."daterelance"',
+                    '"Orientstruct"."statutrelance"',
+                    '"Dossier"."id"',
+                    '"Dossier"."numdemrsa"',
+                    '"Dossier"."dtdemrsa"',
+                    '"Dossier"."matricule"',
+                    '"Personne"."id"',
+                    '"Personne"."nom"',
+                    '"Personne"."prenom"',
+                    '"Personne"."dtnai"',
+                    '"Personne"."nir"',
+                    '"Personne"."qual"',
+                    '"Personne"."nomcomnai"',
+                    '"Adresse"."numvoie"',
+                    '"Adresse"."typevoie"',
+                    '"Adresse"."nomvoie"',
+                    '"Adresse"."compladr"',
+                    '"Adresse"."locaadr"',
+                    '"Adresse"."codepos"',
+                    '"Structurereferente"."lib_struc"',
+                    '"Structurereferente"."code_postal"',
+                    '"Structurereferente"."num_voie"',
+                    '"Structurereferente"."nom_voie"',
+                    '"Structurereferente"."ville"',
+                    '"Structurereferente"."type_voie"',
+                    '"Serviceinstructeur"."lib_service"',
+                    '"Serviceinstructeur"."num_rue"',
+                    '"Serviceinstructeur"."type_voie"',
+                    '"Serviceinstructeur"."nom_rue"',
+                    '"Serviceinstructeur"."code_postal"',
+                    '"Serviceinstructeur"."ville"',
+                    '"User"."nom"',
+                    '"User"."prenom"'
+                ),
+                'joins' => array(
+                    array(
+                        'table'      => 'personnes',
+                        'alias'      => 'Personne',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Personne.id = Orientstruct.personne_id' )
+                    ),
+                    array(
+                        'table'      => 'prestations',
+                        'alias'      => 'Prestation',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array(
+                            'Personne.id = Prestation.personne_id',
+                            'Prestation.natprest = \'RSA\'',
+                            '( Prestation.rolepers = \'DEM\' OR Prestation.rolepers = \'CJT\' )',
+                        )
+                    ),
+                    array(
+                        'table'      => 'foyers',
+                        'alias'      => 'Foyer',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Personne.foyer_id = Foyer.id' )
+                    ),
+                    array(
+                        'table'      => 'dossiers_rsa',
+                        'alias'      => 'Dossier',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Foyer.dossier_rsa_id = Dossier.id' )
+                    ),
+                    array(
+                        'table'      => 'adresses_foyers',
+                        'alias'      => 'Adressefoyer',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Foyer.id = Adressefoyer.foyer_id', 'Adressefoyer.rgadr = \'01\'' )
+                    ),
+                    array(
+                        'table'      => 'adresses',
+                        'alias'      => 'Adresse',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Adresse.id = Adressefoyer.adresse_id' )
+                    ),
+                    array(
+                        'table'      => 'structuresreferentes',
+                        'alias'      => 'Structurereferente',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Structurereferente.id = Orientstruct.structurereferente_id' )
+                    )
+                ),
+                'recursive' => -1
+            )
+        );
 
 
         function mountComparator($data) {
@@ -24,6 +126,63 @@
             $nbjours = Set::extract( $this->data, 'Relance.nbjours' );
             return ( ( empty( $compare ) && empty( $nbjours ) ) || ( !empty( $compare ) && !empty( $nbjours ) ) );
         }
+
+
+        function querydata( $type, $params = array() ) {
+            $types = array_keys( $this->_querydata );
+            if( !in_array( $type, $types ) ) {
+                trigger_error( 'Invalid parameter "'.$type.'" for '.$this->name.'::prepare()', E_USER_WARNING );
+            }
+            else {
+                $querydata = $this->_querydata[$type];
+                $conditions = array(
+                    'Orientstruct.statut_orient = \'Orienté\'',
+                    'Orientstruct.statutrelance ILIKE \'R\''
+                );
+
+                $jetons = Set::extract( $params, 'Jetons.id' );
+                if( !empty( $jetons ) ) {
+                    $conditions['Dossier.id NOT IN'] = $jetons;
+                }
+
+                if( Set::extract( $params, 'User.filtre_zone_geo' ) ) {
+                    $zonesgeographiques = Set::extract( $params, 'Zonegeographique' );
+                    if( !empty( $zonesgeographiques ) ) {
+                        $conditions['Adresse.numcomptt'] = $zonesgeographiques;
+                    }
+                    else {
+                        $conditions[] = 'FALSE';
+                    }
+                }
+
+                $serviceinstructeur_id = Set::extract( $params, 'User.serviceinstructeur_id' );
+                if( !empty( $serviceinstructeur_id ) ) {
+                    $querydata['joins'][] = array(
+                        'table'      => 'servicesinstructeurs',
+                        'alias'      => 'Serviceinstructeur',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Serviceinstructeur.id = '.$serviceinstructeur_id )
+                    );
+                }
+
+                $user_id = Set::extract( $params, 'User.id' );
+                if( !empty( $user_id ) ) {
+                    $querydata['joins'][] = array(
+                        'table'      => 'users',
+                        'alias'      => 'User',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'User.id = '.$user_id )
+                    );
+                }
+
+                $querydata['conditions'] = $conditions;
+
+                return $querydata;
+            }
+        }
+
 
         function search( $statutRelance, $mesCodesInsee, $filtre_zone_geo, $criteresrelance, $lockedDossiers ) {
             /// Conditions de base
