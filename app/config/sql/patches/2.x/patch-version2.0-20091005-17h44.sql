1513f5e567d079617db6ab0de9865d3ade477877
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-10-05 16:08:33 +0000

    Déplacement et rangement des scripts sql par version (1.x + 2.x)

---
+++ app/config/sql/patches/2.x/patch-version2.0-20091005-17h44.sql
@@ -0,0 +1,3 @@
+--------------- Ajout du 05/10/2009 à 17h44 ------------------
+ALTER TABLE contratsinsertion ADD COLUMN raison_ci CHAR(1);
+ALTER TABLE contratsinsertion ADD COLUMN aviseqpluri CHAR(1);
