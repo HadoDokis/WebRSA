6bcff37e5b4c64db532713a417e1af3f359102d9
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-10-18 08:39:34 +0000

    Modification: XhtmlHelper::details retourne une liste de définitions

--- trunk/app/views/helpers/xhtml.php
+++ trunk/app/views/helpers/xhtml.php
@@ -8,29 +8,49 @@
         ** ********************************************************************/
 
         function details( $rows = array(), $htmlAttributes = array(), $allowEmpty = true ) {
+			$type = 'list';// FIXME + $allowEmpty + $htmlAttributes -> array options
             $return = null;
             if( count( $rows ) > 0 ) {
                 $class = 'odd';
                 foreach( $rows as $row ) {
                     if( $allowEmpty || ( !empty( $row[1] ) || valid_int( $row[1] ) ) ) {
-                        $return .= $this->tag(
-                            'tr',
-                            $this->tag( 'th', $row[0] ).$this->tag( 'td', ( ( !empty( $row[1] ) || valid_int( $row[1] ) ) ? $row[1] : ' ' ) ),
-                            array( 'class' => $class )
-                        );
+						$question = $row[0];
+						$answer = ( ( !empty( $row[1] ) || valid_int( $row[1] ) ) ? $row[1] : ' ' );
+
+						if( $type == 'table' ) {
+							$return .= $this->tag(
+								'tr',
+								$this->tag( 'th', $question ).$this->tag( 'td', $answer ),
+								array( 'class' => $class )
+							);
+						}
+						else if( $type == 'list' ) {
+							$return .= $this->tag( 'dt', $question, array( 'class' => $class ) );
+							$return .= $this->tag( 'dd', $answer, array( 'class' => $class ) );
+						}
+
                         $class = ( ( $class == 'odd' ) ? 'even' : 'odd' );
                     }
                 }
 
                 if( !empty( $return ) ) {
-                    $return = $this->tag(
-                        'table',
-                        $this->tag(
-                            'tbody',
-                            $return
-                        ),
-                        Set::merge( $htmlAttributes, array( 'class' => 'details' ) )
-                    );
+					if( $type == 'table' ) {
+						$return = $this->tag(
+							'table',
+							$this->tag(
+								'tbody',
+								$return
+							),
+							Set::merge( $htmlAttributes, array( 'class' => 'details' ) )
+						);
+					}
+					else if( $type == 'list' ) {
+						$return = $this->tag(
+							'dl',
+							$return,
+							Set::merge( $htmlAttributes, array( 'class' => 'details' ) )
+						);
+					}
                 }
             }
 
