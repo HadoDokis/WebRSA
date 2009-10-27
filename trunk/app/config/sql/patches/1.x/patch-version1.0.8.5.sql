f6a8c6bb4c67f1cc93a87fde803f64379123d92c
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-10-27 15:26:12 +0000

    Nettoyage des sources

---
+++ trunk/app/config/sql/patches/1.x/patch-version1.0.8.5.sql
@@ -0,0 +1,80 @@
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+SET search_path = public, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+-- *****************************************************************************
+--------------- Ajout du 05/10/2009 à 17h44 ------------------
+ALTER TABLE contratsinsertion ADD COLUMN raison_ci CHAR(1);
+ALTER TABLE contratsinsertion ADD COLUMN aviseqpluri CHAR(1);
+
+--------------- Ajout du 06/10/2009 à 15h40 ------------------
+ALTER TABLE referents ADD COLUMN fonction VARCHAR(30);
+
+--------------- Ajout du 07/10/2009 à 11h40 ------------------
+ALTER TABLE contratsinsertion ADD COLUMN sitfam_ci TEXT;
+ALTER TABLE contratsinsertion ADD COLUMN sitpro_ci TEXT;
+ALTER TABLE contratsinsertion ADD COLUMN observ_benef TEXT;
+
+--------------- Ajout du 07/10/2009 à 11h40 ------------------
+
+
+ALTER TABLE contratsinsertion ADD COLUMN referent_id INT REFERENCES referents(id);
+CREATE INDEX contratsinsertion_referent_id_idx ON contratsinsertion (referent_id);
+
+--------------- Ajout du 12/10/2009 à 08h49 ------------------
+ALTER TABLE rendezvous ADD COLUMN heurerdv TIME;
+
+ALTER TABLE rendezvous ADD COLUMN referent_id INT REFERENCES referents(id);
+CREATE INDEX rendezvous_referent_id_idx ON rendezvous (referent_id);
+
+ALTER TABLE actionsinsertion ADD COLUMN commentaire_action TEXT;
+
+--------------- Ajout du 12/10/2009 à 08h49 ------------------
+ALTER TABLE rendezvous ADD COLUMN permanence_id INTEGER REFERENCES permanences(id);
+
+--------------- Ajout du 20/10/2009 à 10h17 ------------------
+CREATE TABLE typespdos (
+    id            SERIAL NOT NULL PRIMARY KEY,
+    libelle       VARCHAR(30)
+);
+
+CREATE TABLE decisionspdos (
+    id            SERIAL NOT NULL PRIMARY KEY,
+    libelle       VARCHAR(30)
+);
+
+CREATE TABLE typesnotifspdos (
+    id              SERIAL NOT NULL PRIMARY KEY,
+    libelle         VARCHAR(30),
+    modelenotifpdo  VARCHAR(50)
+);
+
+
+ALTER TABLE propospdos DROP COLUMN decisionpdo;
+ALTER TABLE propospdos DROP COLUMN typepdo;
+
+ALTER TABLE propospdos ADD COLUMN typepdo_id INTEGER NOT NULL REFERENCES typespdos(id);
+ALTER TABLE propospdos ADD COLUMN decisionpdo_id INTEGER REFERENCES decisionspdos(id);
+ALTER TABLE propospdos ADD COLUMN typenotifpdo_id INTEGER REFERENCES typesnotifspdos(id);
+
+
+CREATE TABLE piecespdos (
+    id              SERIAL NOT NULL PRIMARY KEY,
+    propopdo_id     INTEGER REFERENCES propospdos(id),
+    libelle         VARCHAR(50),
+    dateajout       DATE
+);
+CREATE INDEX piecespdos_propopdo_id_idx ON piecespdos (propopdo_id);
+
+--------------- Ajout du 22/10/2009 à 10h17 ------------------
+CREATE TABLE propospdos_typesnotifspdos (
+    id                  SERIAL NOT NULL PRIMARY KEY,
+    propopdo_id         INTEGER NOT NULL REFERENCES propospdos(id),
+    typenotifpdo_id     INTEGER NOT NULL REFERENCES typesnotifspdos(id),
+    datenotifpdo        DATE
+);
