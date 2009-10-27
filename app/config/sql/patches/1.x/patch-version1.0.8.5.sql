3b13f497c8fedc26b11fdf46f279db3d7897931b
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-10-27 15:22:11 +0000

    Modification du patch 1.0.8.5 avec ajout des nouvelles tabels propospdos

--- app/config/sql/patches/1.x/patch-version1.0.8.5.sql
+++ app/config/sql/patches/1.x/patch-version1.0.8.5.sql
@@ -36,3 +36,45 @@ ALTER TABLE actionsinsertion ADD COLUMN commentaire_action TEXT;
 
 --------------- Ajout du 12/10/2009 à 08h49 ------------------
 ALTER TABLE rendezvous ADD COLUMN permanence_id INTEGER REFERENCES permanences(id);
+
+--------------- Ajout du 20/10/2009 à 10h17 ------------------
+CREATE TABLE typespdos (
+    id            SERIAL NOT NULL PRIMARY KEY,
+    libelle       VARCHAR(30)
+);
+
+CREATE TABLE decisionspdos (
+    id            SERIAL NOT NULL PRIMARY KEY,
+    libelle       VARCHAR(30)
+);
+
+CREATE TABLE typesnotifspdos (
+    id              SERIAL NOT NULL PRIMARY KEY,
+    libelle         VARCHAR(30),
+    modelenotifpdo  VARCHAR(50)
+);
+
+
+ALTER TABLE propospdos DROP COLUMN decisionpdo;
+ALTER TABLE propospdos DROP COLUMN typepdo;
+
+ALTER TABLE propospdos ADD COLUMN typepdo_id INTEGER NOT NULL REFERENCES typespdos(id);
+ALTER TABLE propospdos ADD COLUMN decisionpdo_id INTEGER REFERENCES decisionspdos(id);
+ALTER TABLE propospdos ADD COLUMN typenotifpdo_id INTEGER REFERENCES typesnotifspdos(id);
+
+
+CREATE TABLE piecespdos (
+    id              SERIAL NOT NULL PRIMARY KEY,
+    propopdo_id     INTEGER REFERENCES propospdos(id),
+    libelle         VARCHAR(50),
+    dateajout       DATE
+);
+CREATE INDEX piecespdos_propopdo_id_idx ON piecespdos (propopdo_id);
+
+--------------- Ajout du 22/10/2009 à 10h17 ------------------
+CREATE TABLE propospdos_typesnotifspdos (
+    id                  SERIAL NOT NULL PRIMARY KEY,
+    propopdo_id         INTEGER NOT NULL REFERENCES propospdos(id),
+    typenotifpdo_id     INTEGER NOT NULL REFERENCES typesnotifspdos(id),
+    datenotifpdo        DATE
+);
