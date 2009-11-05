1fe9f65c22e92fb0ba52eee70ea73694c01d991f
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2009-11-05 16:39:12 +0000

    Ajout du menu APRE pour récupérer les listes des APRE existantes + rech multi criteres our les trouver

---
+++ trunk/app/models/critereapre.php
@@ -0,0 +1,177 @@
+<?php
+    class Critereapre extends AppModel
+    {
+        var $name = 'Critereapre';
+        var $useTable = false;
+
+        function search( $mesCodesInsee, $filtre_zone_geo, $criteresapres, $lockedDossiers ) {
+            /// Conditions de base
+            $conditions = array();
+
+
+            /// Filtre zone géographique
+            if( $filtre_zone_geo ) {
+                $mesCodesInsee = ( !empty( $mesCodesInsee ) ? $mesCodesInsee : '0' );
+                $conditions[] = 'Adresse.numcomptt IN ( \''.implode( '\', \'', $mesCodesInsee ).'\' )';
+            }
+
+            /// Dossiers lockés
+            if( !empty( $lockedDossiers ) ) {
+                $conditions[] = 'Dossier.id NOT IN ( '.implode( ', ', $lockedDossiers ).' )';
+            }
+
+            /// Critères
+            $datedemandeapre = Set::extract( $criteresapres, 'Filtre.datedemandeapre' );
+            $locaadr = Set::extract( $criteresapres, 'Filtre.locaadr' );
+            $numcomptt = Set::extract( $criteresapres, 'Filtre.numcomptt' );
+            $nir = Set::extract( $criteresapres, 'Filtre.nir' );
+
+
+            /// Critères sur le CI - date de saisi contrat
+            if( isset( $criteresapres['Filtre']['datedemandeapre'] ) && !empty( $criteresapres['Filtre']['datedemandeapre'] ) ) {
+                $valid_from = ( valid_int( $criteresapres['Filtre']['datedemandeapre_from']['year'] ) && valid_int( $criteresapres['Filtre']['datedemandeapre_from']['month'] ) && valid_int( $criteresapres['Filtre']['datedemandeapre_from']['day'] ) );
+                $valid_to = ( valid_int( $criteresapres['Filtre']['datedemandeapre_to']['year'] ) && valid_int( $criteresapres['Filtre']['datedemandeapre_to']['month'] ) && valid_int( $criteresapres['Filtre']['datedemandeapre_to']['day'] ) );
+                if( $valid_from && $valid_to ) {
+                    $conditions[] = 'Contratinsertion.datedemandeapre BETWEEN \''.implode( '-', array( $criteresapres['Filtre']['datedemandeapre_from']['year'], $criteresapres['Filtre']['datedemandeapre_from']['month'], $criteresapres['Filtre']['datedemandeapre_from']['day'] ) ).'\' AND \''.implode( '-', array( $criteresapres['Filtre']['datedemandeapre_to']['year'], $criteresapres['Filtre']['datedemandeapre_to']['month'], $criteresapres['Filtre']['datedemandeapre_to']['day'] ) ).'\'';
+                }
+            }
+
+            // Critères sur une personne du foyer - nom, prénom, nom de jeune fille -> FIXME: seulement demandeur pour l'instant
+            $filtersPersonne = array();
+            foreach( array( 'nom', 'prenom', 'nomnai' ) as $criterePersonne ) {
+                if( isset( $criteresapres['Filtre'][$criterePersonne] ) && !empty( $criteresapres['Filtre'][$criterePersonne] ) ) {
+                    $conditions[] = 'Personne.'.$criterePersonne.' ILIKE \'%'.$criteresapres['Filtre'][$criterePersonne].'%\'';
+                }
+            }
+
+            // Localité adresse
+            if( !empty( $locaadr ) ) {
+                $conditions[] = 'Adresse.locaadr ILIKE \'%'.Sanitize::clean( $locaadr ).'%\'';
+            }
+
+            /// Critères sur l'adresse - canton
+            if( Configure::read( 'CG.cantons' ) ) {
+                if( isset( $criteresapres['Canton']['canton'] ) && !empty( $criteresapres['Canton']['canton'] ) ) {
+                    $this->Canton =& ClassRegistry::init( 'Canton' );
+                    $conditions[] = $this->Canton->queryConditions( $criteresapres['Canton']['canton'] );
+                }
+            }
+
+            // NIR
+            if( !empty( $nir ) ) {
+                $conditions[] = 'Personne.nir ILIKE \'%'.Sanitize::clean( $nir ).'%\'';
+            }
+
+            // Commune au sens INSEE
+            if( !empty( $numcomptt ) ) {
+                $conditions[] = 'Adresse.numcomptt ILIKE \'%'.Sanitize::clean( $numcomptt ).'%\'';
+            }
+
+            /// Requête
+            $this->Dossier =& ClassRegistry::init( 'Dossier' );
+
+            $query = array(
+                'fields' => array(
+                    '"Apre"."id"',
+                    '"Apre"."personne_id"',
+                    '"Apre"."numeroapre"',
+                    '"Apre"."typedemandeapre"',
+                    '"Apre"."datedemandeapre"',
+                    '"Apre"."naturelogement"',
+                    '"Apre"."anciennetepoleemploi"',
+                    '"Apre"."activitebeneficiaire"',
+                    '"Apre"."dateentreeemploi"',
+                    '"Apre"."typecontrat"',
+                    '"Dossier"."id"',
+                    '"Dossier"."numdemrsa"',
+                    '"Dossier"."dtdemrsa"',
+                    '"Dossier"."matricule"',
+                    '"Personne"."id"',
+                    '"Personne"."nom"',
+                    '"Personne"."prenom"',
+                    '"Personne"."dtnai"',
+                    '"Personne"."nir"',
+                    '"Personne"."qual"',
+                    '"Personne"."nomcomnai"',
+                    '"Adresse"."locaadr"',
+                    '"Adresse"."codepos"',
+                    '"Adresse"."numcomptt"'
+                ),
+                'recursive' => -1,
+                'joins' => array(
+                    array(
+                        'table'      => 'personnes',
+                        'alias'      => 'Personne',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Personne.id = Apre.personne_id' )
+                    ),
+                    array(
+                        'table'      => 'prestations',
+                        'alias'      => 'Prestation',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array(
+                            'Personne.id = Prestation.personne_id',
+                            'Prestation.natprest = \'RSA\'',
+                            '( Prestation.rolepers = \'DEM\' OR Prestation.rolepers = \'CJT\' )',
+                        )
+                    ),
+                    array(
+                        'table'      => 'foyers',
+                        'alias'      => 'Foyer',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Personne.foyer_id = Foyer.id' )
+                    ),
+                    array(
+                        'table'      => 'dossiers_rsa',
+                        'alias'      => 'Dossier',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Foyer.dossier_rsa_id = Dossier.id' )
+                    ),
+                    array(
+                        'table'      => 'adresses_foyers',
+                        'alias'      => 'Adressefoyer',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Foyer.id = Adressefoyer.foyer_id', 'Adressefoyer.rgadr = \'01\'' )
+                    ),
+                    array(
+                        'table'      => 'adresses',
+                        'alias'      => 'Adresse',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Adresse.id = Adressefoyer.adresse_id' )
+                    )/*,
+                    array(
+                        'table'      => 'situationsdossiersrsa',
+                        'alias'      => 'Situationdossierrsa',
+                        'type'       => 'INNER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Situationdossierrsa.dossier_rsa_id = Dossier.id AND ( Situationdossierrsa.etatdosrsa IN ( \''.implode( '\', \'', $Situationdossierrsa->etatOuvert() ).'\' ) )' )
+                    ),
+                    array(
+                        'table'      => 'detailsdroitsrsa',
+                        'alias'      => 'Detaildroitrsa',
+                        'type'       => 'LEFT OUTER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Detaildroitrsa.dossier_rsa_id = Dossier.id' )
+                    ),
+                    array(
+                        'table'      => 'detailscalculsdroitsrsa',
+                        'alias'      => 'Detailcalculdroitrsa',
+                        'type'       => 'LEFT OUTER',
+                        'foreignKey' => false,
+                        'conditions' => array( 'Detailcalculdroitrsa.detaildroitrsa_id = Detaildroitrsa.id' )
+                    )*/
+                ),
+                'limit' => 10,
+                'conditions' => $conditions
+            );
+
+            return $query;
+        }
+    }
+?>
---
+++ trunk/app/views/criteresapres/index.ctp
@@ -0,0 +1,179 @@
+<?php echo $html->css( array( 'all.form' ), 'stylesheet', array( 'media' => 'all' ), false );?>
+<?php $this->pageTitle = 'Recherche d\'APREs';?>
+
+<h1>Recherche pour APRE</h1>
+
+<script type="text/javascript">
+    document.observe("dom:loaded", function() {
+        observeDisableFieldsetOnCheckbox( 'FiltreDatedemandeapre', $( 'FiltreDatedemandeapreFromDay' ).up( 'fieldset' ), false );
+    });
+</script>
+
+<?php
+    function value( $array, $index ) {
+        $keys = array_keys( $array );
+        $index = ( ( $index == null ) ? '' : $index );
+        if( @in_array( $index, $keys ) && isset( $array[$index] ) ) {
+            return $array[$index];
+        }
+        else {
+            return null;
+        }
+    }
+
+    if( isset( $apres ) ) {
+        $paginator->options( array( 'url' => $this->passedArgs ) );
+        $params = array( 'format' => 'Résultats %start% - %end% sur un total de %count%.' );
+        $pagination = $html->tag( 'p', $paginator->counter( $params ) );
+
+        $pages = $paginator->first( '<<' );
+        $pages .= $paginator->prev( '<' );
+        $pages .= $paginator->numbers();
+        $pages .= $paginator->next( '>' );
+        $pages .= $paginator->last( '>>' );
+
+        $pagination .= $html->tag( 'p', $pages );
+    }
+    else {
+        $pagination = '';
+    }
+?>
+<?php
+    if( is_array( $this->data ) ) {
+        echo '<ul class="actionMenu"><li>'.$html->link(
+            $html->image(
+                'icons/application_form_magnify.png',
+                array( 'alt' => '' )
+            ).' Formulaire',
+            '#',
+            array( 'escape' => false, 'title' => 'Visibilité formulaire', 'onclick' => "$( 'Search' ).toggle(); return false;" )
+        ).'</li></ul>';
+    }
+
+?>
+
+<?php echo $form->create( 'Critereapre', array( 'type' => 'post', 'action' => '/index/', 'id' => 'Search', 'class' => ( ( is_array( $this->data ) && !empty( $this->data ) ) ? 'folded' : 'unfolded' ) ) );?>
+    <fieldset>
+        <legend>Recherche par personne</legend>
+        <?php echo $form->input( 'Filtre.nom', array( 'label' => 'Nom ', 'type' => 'text' ) );?>
+        <?php echo $form->input( 'Filtre.prenom', array( 'label' => 'Prénom ', 'type' => 'text' ) );?>
+        <?php echo $form->input( 'Filtre.nir', array( 'label' => 'NIR ', 'maxLength' => 15 ) );?>
+    </fieldset>
+    <fieldset>
+        <legend>Recherche par Contrat d'insertion</legend>
+            <?php echo $form->input( 'Filtre.recherche', array( 'label' => false, 'type' => 'hidden', 'value' => true ) );?>
+            <?php echo $form->input( 'Filtre.datedemandeapre', array( 'label' => 'Filtrer par date de demande APRE', 'type' => 'checkbox' ) );?>
+            <fieldset>
+                <legend>Date de saisie du contrat</legend>
+                <?php
+                    $datedemandeapre_from = Set::check( $this->data, 'Filtre.datedemandeapre_from' ) ? Set::extract( $this->data, 'Filtre.datedemandeapre_from' ) : strtotime( '-1 week' );
+                    $datedemandeapre_to = Set::check( $this->data, 'Filtre.datedemandeapre_to' ) ? Set::extract( $this->data, 'Filtre.datedemandeapre_to' ) : strtotime( 'now' );
+                ?>
+                <?php echo $form->input( 'Filtre.datedemandeapre_from', array( 'label' => 'Du', 'type' => 'date', 'dateFormat' => 'DMY', 'maxYear' => date( 'Y' ), 'minYear' => date( 'Y' ) - 120, 'selected' => $datedemandeapre_from ) );?>
+                <?php echo $form->input( 'Filtre.datedemandeapre_to', array( 'label' => 'Au', 'type' => 'date', 'dateFormat' => 'DMY', 'maxYear' => date( 'Y' ), 'minYear' => date( 'Y' ) - 120, 'selected' => $datedemandeapre_to ) );?>
+            </fieldset>
+            <?php echo $form->input( 'Filtre.locaadr', array( 'label' => 'Commune de l\'allocataire ', 'type' => 'text' ) );?>
+            <!-- <?php echo $form->input( 'Filtre.numcomptt', array( 'label' => 'Numéro de commune au sens INSEE' ) );?> -->
+            <?php echo $form->input( 'Filtre.numcomptt', array( 'label' => 'Numéro de commune au sens INSEE', 'type' => 'select', 'options' => $mesCodesInsee, 'empty' => true ) );?>
+            <?php
+                if( Configure::read( 'CG.cantons' ) ) {
+                    echo $form->input( 'Canton.canton', array( 'label' => 'Canton', 'type' => 'select', 'options' => $cantons, 'empty' => true ) );
+                }
+            ?>
+    </fieldset>
+
+    <div class="submit noprint">
+        <?php echo $form->button( 'Rechercher', array( 'type' => 'submit' ) );?>
+        <?php echo $form->button( 'Réinitialiser', array( 'type' => 'reset' ) );?>
+    </div>
+
+<?php echo $form->end();?>
+
+<!-- Résultats -->
+<?php if( isset( $apres ) ):?>
+
+    <h2 class="noprint">Résultats de la recherche</h2>
+    <?php echo $pagination;?>
+    <?php if( is_array( $apres ) && count( $apres ) > 0  ):?>
+
+        <table id="searchResults" class="tooltips_oupas">
+            <thead>
+                <tr>
+                    <th><?php echo $paginator->sort( 'N° CAF', 'Dossier.matricule' );?></th>
+                    <th><?php echo $paginator->sort( 'Nom de l\'allocataire', 'Personne.nom' );?></th>
+                    <th><?php echo $paginator->sort( 'Commune de l\'allocataire', 'Adresse.locaadr' );?></th>
+                    <th><?php echo $paginator->sort( 'Date de demande APRE', 'Apre.datedemandeapre' );?></th>
+                    <th class="action noprint">Actions</th>
+                    <th class="innerTableHeader noprint">Informations complémentaires</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach( $apres as $index => $apre ):?>
+                    <?php
+                        $title = $apre['Dossier']['numdemrsa'];
+
+                        $innerTable = '<table id="innerTable'.$index.'" class="innerTable">
+                            <tbody>
+                               <!-- <tr>
+                                    <th>Commune de naissance</th>
+                                    <td>'.$apre['Personne']['nomcomnai'].'</td>
+                                </tr> -->
+                                <tr>
+                                    <th>Date de naissance</th>
+                                    <td>'.date_short( $apre['Personne']['dtnai'] ).'</td>
+                                </tr>
+                                <tr>
+                                    <th>Code INSEE</th>
+                                    <td>'.$apre['Adresse']['numcomptt'].'</td>
+                                </tr>
+                                <tr>
+                                    <th>NIR</th>
+                                    <td>'.$apre['Personne']['nir'].'</td>
+                                </tr>
+                            </tbody>
+                        </table>';
+//                         debug( $apre );
+                        echo $html->tableCells(
+                            array(
+                                h( $apre['Dossier']['matricule'] ),
+                                h( $apre['Personne']['nom'].' '.$apre['Personne']['prenom'] ),
+                                h( $apre['Adresse']['locaadr'] ),
+                                h( $locale->date( 'Date::short', Set::extract( $apre, 'Apre.datedemandeapre' ) ) ),
+                                array(
+                                    $html->viewLink(
+                                        'Voir le dossier « '.$title.' »',
+                                        array( 'controller' => 'apres', 'action' => 'index', $apre['Apre']['personne_id'] )
+                                    ),
+                                    array( 'class' => 'noprint' )
+                                ),
+                                array( $innerTable, array( 'class' => 'innerTableCell noprint' ) ),
+                            ),
+                            array( 'class' => 'odd', 'id' => 'innerTableTrigger'.$index ),
+                            array( 'class' => 'even', 'id' => 'innerTableTrigger'.$index )
+                        );
+                    ?>
+                <?php endforeach;?>
+            </tbody>
+        </table>
+    <?php echo $pagination;?>
+        <ul class="actionMenu">
+            <li><?php
+                echo $html->printLinkJs(
+                    'Imprimer le tableau',
+                    array( 'onclick' => 'printit(); return false;', 'class' => 'noprint' )
+                );
+            ?></li>
+           <!-- <li><?php
+                echo $html->exportLink(
+                    'Télécharger le tableau',
+                    array( 'controller' => 'criteresapres', 'action' => 'exportcsv', implode_assoc( '/', ':', array_unisize( $this->data ) ) )
+                );
+            ?></li> -->
+        </ul>
+
+
+    <?php else:?>
+        <p>Vos critères n'ont retourné aucun dossier.</p>
+    <?php endif?>
+
+<?php endif?>
