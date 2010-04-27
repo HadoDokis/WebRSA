47beb2e8fb02f5091a5f139d601b4f1fc0a47184
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2010-04-27 16:18:09 +0000

    Correction: transaction

--- app/app_model.php
+++ app/app_model.php
@@ -11,7 +11,7 @@
         */
 
         public function begin() {
-            $return = parent::begin();
+            $return = $this->getDataSource()->begin($this);
             $this->query( 'LOCK TABLE "jetons" IN ACCESS EXCLUSIVE MODE;' );
             return $return;
         }
