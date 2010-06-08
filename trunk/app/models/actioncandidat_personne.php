3d3ed64bfa8f010478756c179136284fb03fddd7
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2010-06-08 09:29:11 +0000

    Correction: le RDV s'enregistre correctement dans la fiche de candidature si celui-ci est présent

--- trunk/app/models/actioncandidat_personne.php
+++ trunk/app/models/actioncandidat_personne.php
@@ -8,7 +8,8 @@
         var $belongsTo = array(
             'Personne',
             'Referent',
-            'Actioncandidat'
+            'Actioncandidat',
+            'Rendezvous'
         );
 
         var $actsAs = array (
