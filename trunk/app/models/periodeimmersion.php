3e6db3a4290652662d019fcde6aeb91aa936a739
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2010-07-09 10:45:19 +0000

    Ajout d'une règle de suppression des valeurs des periodes d'immersion en cas de changement d'avis lors d'une edition de CUI

--- trunk/app/models/periodeimmersion.php
+++ trunk/app/models/periodeimmersion.php
@@ -23,41 +23,41 @@
         );
 
         var $validate = array(
-            'typevoieentaccueil' => array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-            ),
-            'numvoieentaccueil' => array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-            ),
-            'nomvoieentaccueil' => array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-            ),
-            'codepostalentaccueil' => array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-            ),
-            'villeentaccueil' => array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-            ),
-            'numtelentaccueil' => array(
-                array(
-                    'rule' => 'notEmpty',
-                    'message' => 'Champ obligatoire'
-                ),
-                array(
-                    'rule' => array( 'between', 10, 14 ),
-                    'message' => 'Le numéro de téléphone est composé de 10 chiffres'
-                )
-            ),
-            'emailentaccueil' => array(
-                'rule' => 'email',
-                'message' => 'Email non valide',
-                'allowEmpty' => true
-            ),
+//             'typevoieentaccueil' => array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//             ),
+//             'numvoieentaccueil' => array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//             ),
+//             'nomvoieentaccueil' => array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//             ),
+//             'codepostalentaccueil' => array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//             ),
+//             'villeentaccueil' => array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//             ),
+//             'numtelentaccueil' => array(
+//                 array(
+//                     'rule' => 'notEmpty',
+//                     'message' => 'Champ obligatoire'
+//                 ),
+//                 array(
+//                     'rule' => array( 'between', 10, 14 ),
+//                     'message' => 'Le numéro de téléphone est composé de 10 chiffres'
+//                 )
+//             ),
+//             'emailentaccueil' => array(
+//                 'rule' => 'email',
+//                 'message' => 'Email non valide',
+//                 'allowEmpty' => true
+//             ),
 
         );
 
