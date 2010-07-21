f50d45d4be262d056e1e9d09d86ed61521e4a0e2
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2010-07-21 14:43:46 +0000

    Ajout d'un patch SQL 2.0rc10 pour mettre le schema à jour avec la V.30 du flux bénéficiaire

---
+++ app/config/sql/patches/2.x/patch-2.0-rc10.sql
@@ -0,0 +1,42 @@
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+SET search_path = public, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+-- *****************************************************************************
+BEGIN;
+-- *****************************************************************************
+
+-----------------------------------------------------------------------------
+-- 21/07/2010: mise à jour pour la V.30 du flux bénéficiaire
+-----------------------------------------------------------------------------
+
+ALTER TABLE situationsdossiersrsa ADD COLUMN motirefursa CHAR(3);
+
+-----------------------------------------------------------------------------
+
+CREATE TABLE controlesadministratifs (
+    id              	SERIAL NOT NULL PRIMARY KEY,
+    dteffcibcontro  	DATE,
+    cibcontro        	CHAR(3),
+    cibcontromsa        CHAR(3),
+    dtdeteccontro       DATE,
+    dtclocontro        	DATE,
+    libcibcontro        VARCHAR(45),
+    famcibcontro        CHAR(2),
+    natcibcontro        CHAR(3),
+    commacontro        	CHAR(3),
+    typecontro        	CHAR(2),
+    typeimpaccontro     CHAR(1),
+    mtindursacgcontro	DECIMAL(11,2),
+    mtraprsacgcontro    DECIMAL(11,2)
+);
+
+
+-- *****************************************************************************
+COMMIT;
+-- *****************************************************************************
