5ce2e3a89eaee698a66bf05c74c91ae432f2aece
dblirando <dblirando@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2010-12-31 14:12:17 +0000

    MAJ des scripts SQL et de la documentation.

--- trunk/talend/BENEFICIAIRE/BENEFICIAIRE_2_0/sqlScripts/webrsa.ETL.SCHEMA-2.0rc9-20101209.sql
+++ trunk/talend/BENEFICIAIRE/BENEFICIAIRE_2_0/sqlScripts/webrsa.ETL.SCHEMA-2.0rc9-20101209.sql
@@ -1,31 +1,8 @@
--- webrsa
+-- Tables de statistique d'intégration des flux INSTRUCTION, BENEFICIAIRE et FINANCIER
+-- *** VERSIONS  ***
+-- *** webrsa 2.0rc9
+-- *** iRSA v. 3.3 - Cristal v. 29 min
 ALTER USER webrsa SUPERUSER;
-
---------------------------- INSTRUCTION V32
-
---conditionsactivitesprealables
-CREATE TABLE conditionsactivitesprealables
-(
-  id serial NOT NULL,
-  ddcondactprea date NOT NULL,
-  dfcondactprea date NOT NULL,
-  topcondactprea type_booleannumber NOT NULL,
-  nbheuacttot integer NOT NULL,
-  personne_id integer NOT NULL,
-  CONSTRAINT conditionsactivitesprealables_pkey PRIMARY KEY (id),
-  CONSTRAINT conditionsactivitesprealables_personne_id_fkey FOREIGN KEY (personne_id)
-      REFERENCES personnes (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE
-)
-WITH (OIDS=FALSE);
-ALTER TABLE conditionsactivitesprealables OWNER TO webrsa;
-
---paiementsfoyers
-ALTER TABLE paiementsfoyers ADD COLUMN numdebiban VARCHAR(4) NOT NULL;
-
-ALTER TABLE paiementsfoyers ADD COLUMN numfiniban VARCHAR(7) NOT NULL;
-
-ALTER TABLE paiementsfoyers ADD COLUMN bic VARCHAR(11) NOT NULL;
 
 --------------------------- STATISTIQUES
 
