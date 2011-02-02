89f34b286b9dc394b8f824653b2823bea6e8e623
dblirando <dblirando@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-02-02 17:02:18 +0000

    Mise à jour de la documentation et des scripts

--- trunk/talend/BENEFICIAIRE/BENEFICIAIRE_2_0/sqlScripts/patches/patch-2.0rc9_1.2-STAGING_RSA-correction_types.sql
+++ trunk/talend/BENEFICIAIRE/BENEFICIAIRE_2_0/sqlScripts/patches/patch-2.0rc9_1.2-STAGING_RSA-correction_types.sql
@@ -1,7 +1,7 @@
--- patch-2.0rc9_1.0-STAGING_RSA-correction_raisoctieelectdom.sql
+-- patch-2.0rc9_1.2-STAGING_RSA-correction_types.sql
 -- *** VERSIONS  ***
 -- *** webrsa 2.0rc10
--- *** iRSA v. 3.3 à 5 : INSTRUCTION 1.2
+-- *** iRSA v. 6 : INSTRUCTION 2.0
 -- *** Cristal v. 29 min : BENEFICIAIRE/FINANCIER 2.0
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = off;
@@ -17,11 +17,21 @@ BEGIN;
 -- *****************************************************************************
 
 /*******************************************************************************
-	Correction du type de la colonne 'raisoctieelectdom' de 30 à 32 caractères
+	Corrections types
 *******************************************************************************/
 
-ALTER TABLE staging.electiondomicile ALTER COLUMN raisoctieelectdom TYPE character varying(32);
-ALTER TABLE elementaire.foyers ALTER COLUMN raisoctieelectdom TYPE character varying(32);
+-- staging.rib
+ALTER TABLE staging.rib ALTER COLUMN clerib TYPE character varying(2);
+ALTER TABLE staging.rib ALTER COLUMN numdebiban TYPE character varying(4);
+ALTER TABLE staging.rib ALTER COLUMN numfiniban TYPE character varying(7);
+ALTER TABLE staging.rib ALTER COLUMN bic TYPE character varying(11);
+
+-- elementaire.paiementsfoyers
+ALTER TABLE elementaire.paiementsfoyers ALTER COLUMN clerib TYPE character varying(2);
+ALTER TABLE elementaire.paiementsfoyers ALTER COLUMN numdebiban TYPE character varying(4);
+ALTER TABLE elementaire.paiementsfoyers ALTER COLUMN numfiniban TYPE character varying(7);
+ALTER TABLE elementaire.paiementsfoyers ALTER COLUMN bic TYPE character varying(11);
+
 
 -- *****************************************************************************
 COMMIT;
