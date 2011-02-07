ce2ea8012024d676f0e3dc85c3a3a1e318bcd2f3
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-02-07 17:05:45 +0000

    mise en place du formulaire d'ajout de CER pour passage en COV (cg58)

--- app/config/sql/patches/2.x/covs-schema-current.sql
+++ app/config/sql/patches/2.x/covs-schema-current.sql
@@ -15,7 +15,7 @@ DROP TABLE IF EXISTS themescovs58 CASCADE;
 DROP TABLE IF EXISTS covs58 CASCADE;
 DROP TABLE IF EXISTS dossierscovs58 CASCADE;
 DROP TABLE IF EXISTS proposorientationscovs58 CASCADE;
-DROP TABLE IF EXISTS decisionsproposorientationscovs58 CASCADE;
+DROP TABLE IF EXISTS proposcontratsinsertioncovs58 CASCADE;
 
 -- *****************************************************************************
 
@@ -80,9 +80,28 @@ CREATE INDEX proposorientationscovs58_referent_id_idx ON proposorientationscovs5
 CREATE INDEX proposorientationscovs58_covtypeorient_id_idx ON proposorientationscovs58(covtypeorient_id);
 CREATE INDEX proposorientationscovs58_covstructurereferente_id_idx ON proposorientationscovs58(covstructurereferente_id);
 
--- CREATE TABLE proposcontratsinsertion58 (
--- 	id SERIAL NOT NULL PRIMARY KEY
--- );
+CREATE TABLE proposcontratsinsertioncovs58 (
+	id 							SERIAL NOT NULL PRIMARY KEY,
+	dossiercov58_id				INTEGER NOT NULL REFERENCES dossierscovs58(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	structurereferente_id 		INTEGER NOT NULL REFERENCES structuresreferentes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	referent_id 				INTEGER DEFAULT NULL REFERENCES referents(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	datedemande 				DATE NOT NULL,
+	num_contrat					TYPE_NUM_CONTRAT NOT NULL,
+	dd_ci						DATE NOT NULL,
+	duree_engag					INTEGER NOT NULL,
+	df_ci						DATE NOT NULL,
+	forme_ci					VARCHAR(1) NOT NULL,
+	avisraison_ci				VARCHAR(1) DEFAULT NULL,
+	rg_ci						INTEGER DEFAULT NULL,
+	datevalidation				DATE DEFAULT NULL,
+	commentaire 				TEXT DEFAULT NULL
+);
+COMMENT ON TABLE proposcontratsinsertioncovs58 IS 'Contrats d''insertion en attente de validation par la COV (cg58)';
+-- ajouter contrainte que pour une personne que l'on n'ait qu'une seule proposition non traitée
+
+CREATE INDEX proposcontratsinsertioncovs58_dossiercov58_id_idx ON proposcontratsinsertioncovs58(dossiercov58_id);
+CREATE INDEX proposcontratsinsertioncovs58_structurereferente_id_idx ON proposcontratsinsertioncovs58(structurereferente_id);
+CREATE INDEX proposcontratsinsertioncovs58_referent_id_idx ON proposcontratsinsertioncovs58(referent_id);
 
 -- *****************************************************************************
 COMMIT;
