58547e2ebc1362d04e1ae2aabf0158bb204b04c6
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-02-22 09:33:35 +0000

    shell d'ajout des contraintes on delete et on update sur toutes les colonnes de clé étrangères (se finissant par '_id')

---
+++ app/vendors/shells/addconstraint.php
@@ -0,0 +1,63 @@
+<?php
+
+	App::import(array('Model', 'AppModel'));
+	
+	class AddconstraintShell extends Shell{
+		
+		function main() {
+			if ($this->args && $this->args[0] == '?') {
+				return $this->out('Usage: ./cake addcascade <table> [-all]');
+			}
+			
+			$options = array(
+				'all' => false,
+			);
+			
+			foreach ($this->params as $key => $val) {
+				foreach ($options as $name => $option) {
+					if (isset($this->params[$name]) || isset($this->params['-'.$name]) || isset($this->params[$name{0}])) {
+						$options[$name] = true;
+					}
+				}
+			}
+			
+			Configure::write( 'debug', 0 );
+			
+			$db = ConnectionManager::getDataSource('default');
+			if ($options['all']) {
+				$this->args = $db->listSources();
+			}
+			
+			if (empty($this->args)) {
+				return $this->err('Usage: ./cake addcascade <table>');
+			}
+			
+			$tables = $db->listSources();
+			
+			foreach ($this->args as $table) {
+				$name = Inflector::classify($table);
+				$Model = new AppModel(array(
+					'name' => $name,
+					'table' => $table,
+				));
+				
+				$fields = $Model->query("SELECT column_name FROM information_schema.columns WHERE table_name = '{$table}' AND column_name LIKE '%_id';");
+				
+				foreach( $fields as $field ) {
+					$fieldName = $field[0]['column_name'];
+					$parentTable = Inflector::tableize( substr( $fieldName, 0, -3 ) );
+					$Model->query( "ALTER TABLE {$table} DROP CONSTRAINT {$table}_{$fieldName}_fkey;" );
+					if( !$Model->_schema[$fieldName]['null'] ) {
+						$Model->query( "ALTER TABLE {$table} ADD CONSTRAINT {$table}_{$fieldName}_fkey FOREIGN KEY ({$fieldName}) REFERENCES {$parentTable}(id) ON DELETE CASCADE ON UPDATE CASCADE;" );
+					}
+					else {
+						$Model->query( "ALTER TABLE {$table} ADD CONSTRAINT {$table}_{$fieldName}_fkey FOREIGN KEY ({$fieldName}) REFERENCES {$parentTable}(id) ON DELETE SET NULL ON UPDATE CASCADE;" );
+					}
+				}
+				
+				$this->out( sprintf( 'Ajout des contraintes pour la table %s.', $table ) );
+			}
+		}
+	}
+
+?>
