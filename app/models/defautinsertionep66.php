53bd333754a4024e3ef45c3f1ad92f27b5896b0a
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-03-14 11:11:23 +0000

    remise en place de l'ancienne façon de faire le temps de le modifier

--- app/models/defautinsertionep66.php
+++ app/models/defautinsertionep66.php
@@ -636,8 +636,47 @@
 		public function qdRadies() {
 			// FIXME: et qui ne sont pas passés dans une EP pour ce motif depuis au moins 1 mois (?)
 			$queryData = $this->_qdSelection();
-			$queryData = array_merge( $queryData ,$this->Historiqueetatpe->Informationpe->qdRadies() );
-			
+			$queryData['joins'][] = array(
+				'table'      => 'informationspe', // FIXME:
+				'alias'      => 'Informationpe',
+				'type'       => 'INNER',
+				'foreignKey' => false,
+				'conditions' => array(
+					'OR' => array(
+						array(
+							'Informationpe.nir IS NOT NULL',
+							'Personne.nir IS NOT NULL',
+							'Informationpe.nir = Personne.nir',
+						),
+						array(
+							'Informationpe.nom = Personne.nom',
+							'Informationpe.prenom = Personne.prenom',
+							'Informationpe.dtnai = Personne.dtnai',
+						)
+					)
+				)
+			);
+			$queryData['joins'][] = array(
+				'table'      => 'historiqueetatspe', // FIXME:
+				'alias'      => 'Historiqueetatpe',
+				'type'       => 'INNER',
+				'foreignKey' => false,
+				'conditions' => array(
+					'Historiqueetatpe.informationpe_id = Informationpe.id',
+					'Historiqueetatpe.id IN (
+								SELECT h.id
+									FROM historiqueetatspe AS h
+									WHERE h.informationpe_id = Informationpe.id
+									ORDER BY h.date DESC
+									LIMIT 1
+					)'
+				)
+			);
+
+			// FIXME: seulement pour certains motifs
+			$queryData['conditions']['Historiqueetatpe.etat'] = 'radiation';
+			$queryData['order'] = array( 'Historiqueetatpe.date ASC' );
+
 			return $queryData;
 		}
 
