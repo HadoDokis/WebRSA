7f96a155f63f79eed20c448ceadabeb99dcd2859
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-03-30 15:57:26 +0000

    Amélioration: possibilité de filtrer la recherche par dossier/allocataire seulement sur le dernier dossier des personnes

--- trunk/app/models/dossier.php
+++ trunk/app/models/dossier.php
@@ -324,9 +324,6 @@
 				//$conditions[] = 'Serviceinstructeur.id = \''.Sanitize::paranoid( $params['Serviceinstructeur']['id'] ).'\'';
 			}
 
-
-
-
 			///FIXME : tranche d'âge pour la recherche mais non finalisé
 			$trancheAge = Set::extract( $params, 'Personne.trancheAge' );
 			if( $trancheAge == 0 ) {
@@ -352,6 +349,43 @@
 
 			if( !empty( $trancheAge )  ) {
 				$conditions[] = '( EXTRACT ( YEAR FROM AGE( Personne.dtnai ) ) ) BETWEEN '.$ageMin.' AND '.$ageMax;
+			}
+
+			// Trouver la dernière demande RSA pour chacune des personnes du jeu de résultats
+			if( $params['Dossier']['dernier'] ) {
+				$conditions[] = 'Dossier.id IN (
+					SELECT
+							dossiers.id
+						FROM personnes
+							INNER JOIN prestations ON (
+								personnes.id = prestations.personne_id
+								AND prestations.natprest = \'RSA\'
+							)
+							INNER JOIN foyers ON (
+								personnes.foyer_id = foyers.id
+							)
+							INNER JOIN dossiers ON (
+								dossiers.id = foyers.dossier_id
+							)
+						WHERE
+							prestations.rolepers IN ( \'DEM\', \'CJT\' )
+							AND (
+								(
+									nir_correct( Personne.nir )
+									AND nir_correct( personnes.nir )
+									AND personnes.nir = Personne.nir
+									AND personnes.dtnai = Personne.dtnai
+								)
+								OR
+								(
+									personnes.nom = Personne.nom
+									AND personnes.prenom = Personne.prenom
+									AND personnes.dtnai = Personne.dtnai
+								)
+							)
+						ORDER BY dossiers.dtdemrsa DESC
+						LIMIT 1
+				)';
 			}
 
 			/**
