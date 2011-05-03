d21fc7a2f30b9d160b542e360b0acdb8adfb199a
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-03 08:33:36 +0000

    finalisation de la thématique de sanction par rendez-vous pour le cg58

--- trunk/app/models/sanctionrendezvousep58.php
+++ trunk/app/models/sanctionrendezvousep58.php
@@ -40,6 +40,165 @@
 				'order' => ''
 			),
 		);
+
+		/**
+		* Querydata permettant d'obtenir les dossiers qui doivent être traités
+		* par liste pour la thématique de ce modèle.
+		*
+		* TODO: une autre liste pour avoir un tableau permettant d'accéder à la fiche
+		* TODO: que ceux avec accord, les autres en individuel
+		*
+		* @param integer $commissionep_id L'id technique de la séance d'EP
+		* @param string $niveauDecision Le niveau de décision ('ep' ou 'cg') pour
+		*	lequel il faut les dossiers à passer par liste.
+		* @return array
+		* @access public
+		*/
+
+		public function qdDossiersParListe( $commissionep_id, $niveauDecision ) {
+			// Doit-on prendre une décision à ce niveau ?
+			$themes = $this->Dossierep->Passagecommissionep->Commissionep->themesTraites( $commissionep_id );
+			$niveauFinal = $themes[Inflector::underscore($this->alias)];
+			if( ( $niveauFinal == 'ep' ) && ( $niveauDecision == 'cg' ) ) {
+				return array();
+			}
+
+			return array(
+				'conditions' => array(
+					'Dossierep.themeep' => Inflector::tableize( $this->alias ),
+					'Dossierep.id IN ( '.
+						$this->Dossierep->Passagecommissionep->sq(
+							array(
+								'fields' => array(
+									'passagescommissionseps.dossierep_id'
+								),
+								'alias' => 'passagescommissionseps',
+								'conditions' => array(
+									'passagescommissionseps.commissionep_id' => $commissionep_id
+								)
+							)
+						)
+					.' )'
+				),
+				'contain' => array(
+					'Personne' => array(
+						'Foyer' => array(
+							'fields' => array(
+								'id',
+								'dossier_id',
+								'sitfam',
+								'ddsitfam',
+								'typeocclog',
+								'mtvallocterr',
+								'mtvalloclog',
+								'contefichliairsa',
+								'mtestrsa',
+								'raisoctieelectdom',
+								"( SELECT COUNT(DISTINCT(personnes.id)) FROM personnes INNER JOIN prestations ON ( personnes.id = prestations.personne_id ) WHERE personnes.foyer_id = \"Foyer\".\"id\" AND prestations.natprest = 'RSA' AND prestations.rolepers = 'ENF' ) AS \"Foyer__nbenfants\"",
+							),
+							'Adressefoyer' => array(
+								'conditions' => array(
+									'Adressefoyer.rgadr' => '01'
+								),
+								'Adresse'
+							)
+						)
+					),
+					$this->alias => array(
+						'fields' => array(
+							'id',
+							'dossierep_id',
+							'typerdv_id',
+							'created',
+							'modified'
+
+						),
+						'Typerdv'
+					),
+					'Passagecommissionep' => array(
+						'conditions' => array(
+							'Passagecommissionep.commissionep_id' => $commissionep_id
+						),
+						'Decisionsanctionrendezvousep58' => array(
+							'order' => array( 'etape DESC' )
+						)
+					)
+				)
+			);
+		}
+
+		/**
+		* FIXME
+		*
+		* @param integer $commissionep_id L'id technique de la séance d'EP
+		* @param array $datas Les données des dossiers
+		* @param string $niveauDecision Le niveau de décision ('ep' ou 'cg') pour
+		*	lequel il faut préparer les données du formulaire
+		* @return array
+		* @access public
+		*/
+
+		public function prepareFormData( $commissionep_id, $datas, $niveauDecision ) {
+			// Doit-on prendre une décision à ce niveau ?
+			$themes = $this->Dossierep->Passagecommissionep->Commissionep->themesTraites( $commissionep_id );
+			$niveauFinal = $themes[Inflector::underscore( $this->alias )];
+			if( ( $niveauFinal == 'ep' ) && ( $niveauDecision == 'cg' ) ) {
+				return array();
+			}
+
+			$formData = array();
+			foreach( $datas as $key => $dossierep ) {
+				$formData['Decisionsanctionrendezvousep58'][$key]['passagecommissionep_id'] = @$datas[$key][Passagecommissionep][0]['id'];
+
+				if( $niveauDecision == 'ep' ) {
+					if( isset( $datas[$key]['Passagecommissionep'][0]['Decisionsanctionrendezvousep58'][0] ) ) { // Modification
+						$formData['Decisionsanctionrendezvousep58'][$key]['id'] = @$datas[$key]['Passagecommissionep'][0]['Decisionsanctionrendezvousep58'][0]['id'];
+						$formData['Decisionsanctionrendezvousep58'][$key]['decision'] = @$datas[$key]['Passagecommissionep'][0]['Decisionsanctionrendezvousep58'][0]['decision'];
+						$formData['Decisionsanctionrendezvousep58'][$key]['raisonnonpassage'] = @$datas[$key]['Passagecommissionep'][0]['Decisionsanctionrendezvousep58'][0]['raisonnonpassage'];
+					}
+					else {
+						$formData['Decisionsanctionrendezvousep58'][$key]['raisonnonpassage'] = null;
+					}
+				}
+			}
+
+			return $formData;
+		}
+
+		/**
+		* TODO: docs
+		*/
+
+		public function saveDecisions( $data, $niveauDecision ) {
+			// FIXME: filtrer les données
+			$themeData = Set::extract( $data, '/Decisionsanctionrendezvousep58' );
+			if( empty( $themeData ) ) {
+				return true;
+			}
+			else {
+				foreach( array_keys( $themeData ) as $key ) {
+					if ( empty( $themeData[$key]['Decisionsanctionrendezvousep58']['decision'] ) ) {
+						unset( $themeData[$key] );
+					}
+				}
+
+				$success = $this->Dossierep->Passagecommissionep->Decisionsanctionrendezvousep58->saveAll( $themeData, array( 'atomic' => false ) );
+				$this->Dossierep->Passagecommissionep->updateAll(
+					array( 'Passagecommissionep.etatdossierep' => '\'decision'.$niveauDecision.'\'' ),
+					array( '"Passagecommissionep"."id"' => Set::extract( $data, '/Decisionsanctionrendezvousep58/passagecommissionep_id' ) )
+				);
+				return $success;
+			}
+		}
+
+		/**
+		* TODO: docs
+		*/
+
+		public function finaliser( $commissionep_id, $etape ) {
+			// Aucune action utile ?
+			return true;
+		}
 		
 	}
 ?>
