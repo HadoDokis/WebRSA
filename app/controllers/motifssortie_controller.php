2c4fffa83b1421849ac4a15ddb23cd0a19e86134
tnemes <tnemes@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-06 12:49:15 +0000

    Modification de la fiche de candidature pour le CG66. Ajout du paramétrage pour les motifs de sortie.

---
+++ app/controllers/motifssortie_controller.php
@@ -0,0 +1,94 @@
+<?php
+
+    class MotifssortieController extends AppController 
+    {
+        var $name = 'Motifssortie';
+        var $uses = array( 'Motifsortie', 'Option' );
+        var $helpers = array( 'Xform', 'Default', 'Theme' );
+        var $components = array( 'Default' );
+        
+		var $commeDroit = array(
+			'view' => 'Motifssortie:index',
+			'add' => 'Motifssortie:edit'
+		);
+
+        /**
+        *   Ajout à la suite de l'utilisation des nouveaux helpers
+        *   - default.php
+        *   - theme.php
+        */
+
+        public function index() {
+            $this->set(
+                Inflector::tableize( $this->modelClass ),
+                $this->paginate( $this->modelClass )
+            );
+        }
+
+        /**
+        *
+        */
+
+        public function add() {
+            $args = func_get_args();
+            call_user_func_array( array( $this, '_add_edit' ), $args );
+        }
+
+        /**
+        *
+        */
+
+        public function edit() {
+            $args = func_get_args();
+            call_user_func_array( array( $this, '_add_edit' ), $args );
+        }
+
+        /**
+        *
+        */
+
+        function _add_edit( $id = null){
+//            $args = func_get_args();
+//            $this->Default->{$this->action}( $args );
+			if( !empty( $this->data ) ) {
+				$this->Motifsortie->create( $this->data );
+				$success = $this->Motifsortie->save();
+
+				$this->_setFlashResult( 'Save', $success );
+				if( $success ) {
+					$this->redirect( array( 'action' => 'index' ) );
+				}
+			}
+			else if( $this->action == 'edit' ) {
+				$this->data = $this->Motifsortie->find(
+					'first',
+					array(
+						'contain' => false,
+						'conditions' => array( 'Motifsortie.id' => $id )
+					)
+				);
+				$this->assert( !empty( $this->data ), 'error404' );
+			}
+
+			$this->render( null, null, 'add_edit' );            
+        }
+
+        /**
+        *
+        */
+
+        public function delete( $id ) {
+            $this->Default->delete( $id );
+        }
+
+        /**
+        *
+        */
+
+        public function view( $id ) {
+
+            $this->Default->view( $id );
+        }
+
+    }
+?>
