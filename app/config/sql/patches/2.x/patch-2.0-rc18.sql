d1a69be3bc5fe675fbe0ddbc35073a7a8d7d6239
tnemes <tnemes@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-06 12:44:23 +0000

    Ajout de la fiche de candidature

--- app/config/sql/patches/2.x/patch-2.0-rc18.sql
+++ app/config/sql/patches/2.x/patch-2.0-rc18.sql
@@ -252,6 +252,170 @@ ALTER TABLE personnes ALTER COLUMN haspiecejointe SET DEFAULT '0'::TYPE_BOOLEANN
 CREATE INDEX informationspe_nom_prenom_dtnai_idx ON informationspe USING btree (nom, prenom, dtnai);
 CREATE INDEX personnes_nom_prenom_dtnai_idx ON personnes USING btree (nom, prenom, dtnai);
 
+-- -------------------------------------------------------------------------------------------------------------
+-- 20110506: Fiche candidature pour CG66 
+-- -------------------------------------------------------------------------------------------------------------
+
+-- ACTIONSCANDIDATS & SES RELATIONS
+DROP TABLE IF EXISTS actionscandidats_partenaires;
+DROP TABLE IF EXISTS actionscandidats_personnes;
+DROP TABLE IF EXISTS actionscandidats_zonesgeographiques;
+DROP TABLE IF EXISTS actionscandidats;
+
+-- ACTIONSCANDIDATS
+DROP TYPE IF EXISTS type_contractualisation;
+CREATE TYPE type_contractualisation AS ENUM ('marche', 'subvention');
+
+CREATE TABLE actionscandidats
+(
+	id					SERIAL NOT NULL PRIMARY KEY,
+	name				character varying(250) NOT NULL,
+	themecode			INTEGER NOT NULL,
+	codefamille			character varying(1) NOT NULL,
+	numcodefamille		character varying(5) NOT NULL,
+	contractualisation	type_contractualisation DEFAULT NULL,
+	lieuaction			character varying(250) NOT NULL,
+	cantonaction		character varying(250) NOT NULL,
+	ddaction			DATE NOT NULL,
+	dfaction			DATE NOT NULL,
+	nbpostedispo		INTEGER NOT NULL,
+	nbposterestant		INTEGER,
+	correspondantaction	type_booleannumber DEFAULT '0'::type_booleannumber,
+	referent_id 		INTEGER REFERENCES referents(id),
+	hasfichecandidature	type_booleannumber NOT NULL DEFAULT '1'::type_booleannumber,
+	filtre_zone_geo 	type_booleannumber DEFAULT '0'::type_booleannumber
+);
+
+COMMENT ON TABLE actionscandidats IS 'Table pour les actions d''insertion liées à la fiche de candidature (CG66)';
+COMMENT ON COLUMN actionscandidats.id IS '[PK] Identifiant';
+COMMENT ON COLUMN actionscandidats.name	IS 'Intitulé de l''action';			
+COMMENT ON COLUMN actionscandidats.themecode IS 'Code de l''action : partie thème';
+COMMENT ON COLUMN actionscandidats.codefamille IS 'Code de l''action : partie code famille';
+COMMENT ON COLUMN actionscandidats.numcodefamille IS 'Code de l''action : numéro du code famille';
+COMMENT ON COLUMN actionscandidats.contractualisation IS '[ENUM type_contractualisation] ex : Marché public, Subvention,...';
+COMMENT ON COLUMN actionscandidats.lieuaction IS 'Lieu de l''action';
+COMMENT ON COLUMN actionscandidats.cantonaction IS 'Canton de l''action';
+COMMENT ON COLUMN actionscandidats.ddaction IS 'Date de début de l''action';
+COMMENT ON COLUMN actionscandidats.dfaction IS 'Date de fin de l''action';
+COMMENT ON COLUMN actionscandidats.nbpostedispo IS 'Nombre de postes disponibles';
+COMMENT ON COLUMN actionscandidats.nbposterestant IS 'Nombre de postes restants (calculé)';
+COMMENT ON COLUMN actionscandidats.correspondantaction IS 'Présence d''un correspondant de l''action';
+COMMENT ON COLUMN actionscandidats.referent_id IS 'Référent de l''action (si correspondantaction est 1)';
+COMMENT ON COLUMN actionscandidats.hasfichecandidature IS 'Présence d''une fiche de candidature';
+
+CREATE INDEX actionscandidats_name_idx ON actionscandidats( name );
+CREATE INDEX actionscandidats_themecode_idx ON actionscandidats( themecode );
+CREATE INDEX actionscandidats_codefamille_idx ON actionscandidats( codefamille );
+CREATE INDEX actionscandidats_numcodefamille_idx ON actionscandidats( numcodefamille );
+CREATE INDEX actionscandidats_contractualisation_idx ON actionscandidats( contractualisation );
+CREATE INDEX actionscandidats_lieuaction_idx ON actionscandidats( lieuaction );
+CREATE INDEX actionscandidats_cantonaction_idx ON actionscandidats( cantonaction );
+CREATE INDEX actionscandidats_nbpostedispo_idx ON actionscandidats( nbpostedispo );
+CREATE INDEX actionscandidats_correspondantaction_idx ON actionscandidats( correspondantaction );
+CREATE INDEX actionscandidats_referent_id_idx ON actionscandidats( referent_id );
+
+-- ACTIONSCANDIDATS_PERSONNES
+CREATE TABLE actionscandidats_personnes
+(
+  id serial NOT NULL PRIMARY KEY,
+  personne_id integer NOT NULL REFERENCES personnes (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  actioncandidat_id integer NOT NULL REFERENCES actionscandidats (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  referent_id integer NOT NULL REFERENCES referents (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  ddaction date,
+  dfaction date,
+  motifdemande text,
+  enattente type_no,
+  datesignature date,
+  bilanvenu type_venu,
+  bilanretenu type_retenu,
+  infocomplementaire text,
+  datebilan date,
+  rendezvouspartenaire type_booleannumber,
+  mobile type_booleannumber,
+  naturemobile text,
+  typemobile text,
+  bilanrecu type_no,
+  daterecu date,
+  personnerecu character varying(50),
+  pieceallocataire type_munir,
+  autrepiece character varying(50),
+  precisionmotif text,
+  presencecontrat type_no,
+  integrationaction type_no,
+  horairerdvpartenaire timestamp without time zone,
+  sortiele date,
+  motifsortie_id integer REFERENCES motifssortie (id) ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+COMMENT ON TABLE actionscandidats_personnes IS 'Table de liaison entre une personne, un référent et les actions de candidature';
+
+CREATE INDEX actionscandidats_personnes_actioncandidat_id_idx ON actionscandidats_personnes (actioncandidat_id);
+CREATE INDEX actionscandidats_personnes_bilanrecu_idx ON actionscandidats_personnes (bilanrecu);
+CREATE INDEX actionscandidats_personnes_bilanretenu_idx ON actionscandidats_personnes (bilanretenu);
+CREATE INDEX actionscandidats_personnes_bilanvenu_idx ON actionscandidats_personnes (bilanvenu);
+CREATE INDEX actionscandidats_personnes_datebilan_idx ON actionscandidats_personnes (datebilan);
+CREATE INDEX actionscandidats_personnes_daterecu_idx ON actionscandidats_personnes (daterecu);
+CREATE INDEX actionscandidats_personnes_datesignature_idx ON actionscandidats_personnes (datesignature);
+CREATE INDEX actionscandidats_personnes_ddaction_idx ON actionscandidats_personnes (ddaction);
+CREATE INDEX actionscandidats_personnes_dfaction_idx ON actionscandidats_personnes (dfaction);
+CREATE INDEX actionscandidats_personnes_enattente_idx ON actionscandidats_personnes (enattente);
+CREATE INDEX actionscandidats_personnes_integrationaction_idx ON actionscandidats_personnes (integrationaction);
+CREATE INDEX actionscandidats_personnes_mobile_idx ON actionscandidats_personnes (mobile);
+CREATE INDEX actionscandidats_personnes_personne_id_idx ON actionscandidats_personnes (personne_id);
+CREATE INDEX actionscandidats_personnes_pieceallocataire_idx ON actionscandidats_personnes (pieceallocataire);
+CREATE INDEX actionscandidats_personnes_presencecontrat_idx ON actionscandidats_personnes (presencecontrat);
+CREATE INDEX actionscandidats_personnes_referent_id_idx ON actionscandidats_personnes (referent_id);
+CREATE INDEX actionscandidats_personnes_rendezvouspartenaire_idx ON actionscandidats_personnes (rendezvouspartenaire);
+CREATE INDEX actionscandidats_personnes_motifsortie_id_idx ON actionscandidats_personnes( motifsortie_id );
+  
+-- ACTIONSCANDIDATS_PARTENAIRES
+CREATE TABLE actionscandidats_partenaires
+(
+  id 				SERIAL NOT NULL PRIMARY KEY,
+  actioncandidat_id	INTEGER NOT NULL REFERENCES actionscandidats(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  partenaire_id		INTEGER NOT NULL REFERENCES partenaires(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+COMMENT ON TABLE actionscandidats_partenaires IS 'Table de liaison entre les partenaires et les actions de candidature';
+
+-- ACTIONSCANDIDATS_ZONESGEOGRAPHIQUES
+CREATE TABLE actionscandidats_zonesgeographiques
+(
+	id					SERIAL NOT NULL PRIMARY KEY,
+	actioncandidat_id	INTEGER NOT NULL REFERENCES actionscandidats(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	zonegeographique_id	INTEGER NOT NULL REFERENCES zonesgeographiques(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+COMMENT ON TABLE actionscandidats_zonesgeographiques IS 'Table de liaison entre les actions d''insertion pour fiches de candidatures et les zones géographiques (CG66)';
+COMMENT ON COLUMN actionscandidats_zonesgeographiques.id IS '[PK] Identifiant';
+COMMENT ON COLUMN actionscandidats_zonesgeographiques.actioncandidat_id IS '[FK] actionscandidats(id)';
+COMMENT ON COLUMN actionscandidats_zonesgeographiques.zonegeographique_id IS '[FK] zonesgeographiques(id)';
+
+-- PARTENAIRES 
+SELECT alter_table_drop_column_if_exists('public', 'partenaires', 'codepartenaire');
+ALTER TABLE partenaires ADD COLUMN codepartenaire character varying(10);
+COMMENT ON COLUMN partenaires.codepartenaire IS 'Code partenaire (en lien avec le code action)';
+
+-- MOTIFSSORTIE
+DROP TABLE IF EXISTS motifssortie;
+CREATE TABLE motifssortie
+(
+	id	SERIAL NOT NULL PRIMARY KEY,
+	name	TEXT
+);
+
+COMMENT ON TABLE motifssortie IS 'Table pour les motifs de sortie d''une action d''insertion pour la fiche de candidature (CG66)';
+COMMENT ON COLUMN motifssortie.id IS '[PK] Identifiant';
+COMMENT ON COLUMN motifssortie.name IS 'Intitulé du motif de sortie';
+
+CREATE INDEX motifssortie_name_idx ON motifssortie( name );
+
+
+
+
+
+
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
