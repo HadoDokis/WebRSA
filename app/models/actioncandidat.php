6f7760f01f01234700c27991d3193b4cf2061911
tnemes <tnemes@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-06 12:51:24 +0000

    Modification de la fiche de candidature pour le CG66. Modification des relations, ajout d'une méthode pour formater le code action.

--- app/models/actioncandidat.php
+++ app/models/actioncandidat.php
@@ -3,24 +3,30 @@
 	{
 		public $name = 'Actioncandidat';
 
-		public $displayField = 'intitule';
+		public $displayField = 'name';
 
 		public $actsAs = array(
+			'Autovalidate',
 			'ValidateTranslate',
+			'Enumerable' => array(
+				'fields' => array(
+					'contractualisation', 'correspondantaction', 'hasfichecandidature'
+				)
+			)
 		);
 
-		public $validate = array(
-			'intitule' => array(
-				array(
-					'rule' => array('notEmpty'),
-				),
-			),
-			'code' => array(
-				array(
-					'rule' => array('notEmpty'),
-				),
-			),
-		);
+//		public $validate = array(
+//			'intitule' => array(
+//				array(
+//					'rule' => array('notEmpty'),
+//				),
+//			),
+//			'code' => array(
+//				array(
+//					'rule' => array('notEmpty'),
+//				),
+//			),
+//		);
 
 		public $hasAndBelongsToMany = array(
 			'Partenaire' => array(
@@ -54,7 +60,46 @@
 				'deleteQuery' => '',
 				'insertQuery' => '',
 				'with' => 'ActioncandidatPersonne'
-			)
+			),
+			'Zonegeographique' => array(
+				'className' => 'Zonegeographique',
+				'joinTable' => 'actionscandidats_zonesgeographiques',
+				'foreignKey' => 'actioncandidat_id',
+				'associationForeignKey' => 'zonegeographique_id',
+				'unique' => true,
+				'conditions' => '',
+				'fields' => '',
+				'order' => '',
+				'limit' => '',
+				'offset' => '',
+				'finderQuery' => '',
+				'deleteQuery' => '',
+				'insertQuery' => '',
+				'with' => 'ActioncandidatZonegeographique' // TODO
+			)			
 		);
+		
+
+		function afterFind($results,$primary = false)
+		{
+			$resultset = parent::afterFind( $results, $primary );
+
+			if( !empty( $resultset ) ) 
+			{
+				foreach( $resultset as $i => $results )
+				{
+					if( isset( $results['Actioncandidat']['id'] ) && isset( $results['Actioncandidat']['themecode'] ) )
+					{
+						$codeaction = $results['Actioncandidat']['themecode'].$results['Actioncandidat']['codefamille'].$results['Actioncandidat']['numcodefamille'];
+						$results['Actioncandidat']['codeaction'] = $codeaction;
+					}
+					$resultset[$i] = $results;
+				}
+			}
+			return $resultset;
+		}		
+		
+
+		
 	}
 ?>
