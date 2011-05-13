ec5de187c9432301fe3d884086a4f555276675ad
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-13 15:14:55 +0000

    suppression de règles superflues

--- app/config/sql/patches/2.x/patch-2.0-rc19.sql
+++ app/config/sql/patches/2.x/patch-2.0-rc19.sql
@@ -20,11 +20,7 @@ SELECT add_missing_table_field ('public', 'membreseps', 'ville', 'VARCHAR(100)')
 ALTER TABLE actionscandidats ALTER COLUMN contractualisation SET NOT NULL;
 
 SELECT add_missing_table_field ('public', 'proposorientationscovs58', 'decisioncov', 'CHAR(10)');
-UPDATE proposorientationscovs58 SET decisioncov = 'accepte' WHERE decisioncov IS NULL;
-ALTER TABLE proposorientationscovs58 ALTER COLUMN decisioncov SET NOT NULL;
 SELECT add_missing_table_field ('public', 'proposcontratsinsertioncovs58', 'decisioncov', 'VARCHAR(10)');
-UPDATE proposcontratsinsertioncovs58 SET decisioncov = 'accepte' WHERE decisioncov IS NULL;
-ALTER TABLE proposcontratsinsertioncovs58 ALTER COLUMN decisioncov SET NOT NULL;
 
 -- *****************************************************************************
 COMMIT;
