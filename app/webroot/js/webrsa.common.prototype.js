29e2ac7cacfd1fc4b4c9b5db9018a34aeac1d52b
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-18 09:02:07 +0000

    ajout d'une fonction pour simuler le changement sur un select

--- app/webroot/js/webrsa.common.prototype.js
+++ app/webroot/js/webrsa.common.prototype.js
@@ -937,3 +937,16 @@ function textareaMakeItCount(textareaId, maxsize, limited){
         textareaCharCounter(textareaId,maxsize,limited);
     }
 }
+
+// http://jehiah.cz/a/firing-javascript-events-properly
+function fireEvent(element,event) {
+	if (document.createEventObject) {// dispatch for IE
+		var evt = document.createEventObject();
+		return element.fireEvent('on'+event,evt)
+	}
+	else { // dispatch for firefox + others
+		var evt = document.createEvent("HTMLEvents");
+		evt.initEvent(event, true, true ); // event type,bubbling,cancelable
+		return !element.dispatchEvent(evt);
+	}
+}
