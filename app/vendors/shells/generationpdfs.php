4ae556cf018f1ced1f067ff07fe1dbef3d7d0671
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-05-19 08:34:30 +0000

    Mise à jour de messages d'erreur pour l'impression de cohortes, mise à jour de la documentation sur les shells et de l'aide du shell generationpdfs

--- app/vendors/shells/generationpdfs.php
+++ app/vendors/shells/generationpdfs.php
@@ -3,8 +3,8 @@
 	*
 	*/
 
-    class GenerationpdfsShell extends AppShell
-    {
+	class GenerationpdfsShell extends AppShell
+	{
 		public $defaultParams = array(
 			'log' => false,
 			'logpath' => LOGS,
@@ -82,7 +82,7 @@
 			// A-t-on spécifié l'identifiant d'un utilisateur (obligatoire dans ce cas-ci) ?
 			$this->username = $this->_getNamedValue( 'username', 'string' );
 			if( empty( $this->username ) ) {
-				$this->err( "Veuillez spécifier l'identifiant d'un utilisateur pour pouvoir générer les impressions d'orientations." );
+				$this->err( "Veuillez spécifier l'identifiant d'un utilisateur qui sera utilisé pour la récupération d'informations lors de l'impression pour les impressions d'orientations (exemple: -username webrsa)." );
 				$this->_stop( 1 );
 			}
 
@@ -155,19 +155,16 @@
 			$this->out('Commandes:');
 			$this->out("\n\t{$this->shell}\n\t\tAffiche cette aide.");
 			$this->out("\n\t{$this->shell} relancenonrespectsanctionep93\n\t\tGénère les impressions des relances pour pour non respect et sanctions (CG 93).");
-			$this->out("\n\t{$this->shell} orientsstructs\n\t\tGénère les impressions des orientations.");
+			$this->out("\n\t{$this->shell} orientsstructs\n\t\tGénère les impressions des orientations (le paramètre -username est obligatoire, voir ci-dessous).");
 			$this->out("\n\t{$this->shell} help\n\t\tAffiche cette aide.");
 			$this->out();
 			$this->out('Paramètres:');
-			$this->out("\t-connection <connexion>\n\t\tLe nom d'une connexion PostgreSQL défini dans app/config/database.php\n\t\tPar défaut: ".$this->_defaultToString( 'connection' )."\n");
-// 			$this->out("\t-log <booléen>\n\t\tDoit-on journaliser la sortie du programme ?\n\t\tPar défaut: ".$this->_defaultToString( 'log' )."\n");
-// 			$this->out("\t-logpath <répertoire>\n\t\tLe répertoire dans lequel enregistrer les fichiers de journalisation.\n\t\tPar défaut: ".$this->_defaultToString( 'logpath' )."\n");
-			$this->out("\t-username <string>\n\t\tL'identifiant de l'utilisateur qui sera utilisé pour la récupération d'informations lors de l'impression (pour les orientations seulement).\n\t\tPar défaut: ".$this->_defaultToString( 'verbose' )."\n");
+			$this->out("\t-username <string>\n\t\tL'identifiant de l'utilisateur qui sera utilisé pour la récupération d'informations lors de l'impression (pour les orientations seulement).\n\t\tPar défaut: ".$this->_defaultToString( 'username' )."\n");
 			$this->out("\t-verbose <booléen>\n\t\tDoit-on afficher les étapes de lecture / écriture ?\n\t\tPar défaut: ".$this->_defaultToString( 'verbose' )."\n");
-			$this->out("\t-limit <entier>\n\t\tLimite sur le nombre de tables à traiter.\n\t\tPar défaut: ".$this->_defaultToString( 'limit' )."\n");
+			$this->out("\t-limit <entier>\n\t\tLimite sur le nombre d'enregistrements à traiter.\n\t\tPar défaut: ".$this->_defaultToString( 'limit' )."\n");
 			$this->out();
 
 			$this->_stop( 0 );
 		}
-    }
+	}
 ?>
