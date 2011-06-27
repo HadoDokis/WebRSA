ce000eca129ec04ee266a798cd57eaa52300675f
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-06-27 14:49:16 +0000

    Correction du bug où 2 utilisateurs pouvaient être connectés en même temps, après la fin du timeout, nettoyage du code.

--- app/controllers/recours_controller.php
+++ app/controllers/recours_controller.php
@@ -1,55 +1,66 @@
 <?php 
+	class RecoursController extends AppController
+	{
+		public $name = 'Recours';
+		public $uses = array( 'Infofinanciere', 'Option', 'Avispcgdroitrsa' );
 
-    class RecoursController extends AppController{
-        var $name = 'Recours';
-        var $uses = array( 'Infofinanciere', 'Option', 'Avispcgdroitrsa' );
+		/**
+		*
+		*/
 
-        function beforeFilter() {
-            parent::beforeFilter();
-            $this->set( 'commission', $this->Option->commission() );
-            $this->set( 'decisionrecours', $this->Option->decisionrecours() );
-            $this->set( 'motifrecours', $this->Option->motifrecours() );
-        }
+		public function beforeFilter() {
+			parent::beforeFilter();
+			$this->set( 'commission', $this->Option->commission() );
+			$this->set( 'decisionrecours', $this->Option->decisionrecours() );
+			$this->set( 'motifrecours', $this->Option->motifrecours() );
+		}
 
-        function gracieux( $dossier_id = null ){
+		/**
+		*
+		*/
 
-            $this->assert( valid_int( $dossier_id ), 'invalidParameter' );
+		public function gracieux( $dossier_id = null ){
 
-            $gracieux = $this->Avispcgdroitrsa->find(
-                'first',
-                array(
-                    'conditions' => array(
-                        'Avispcgdroitrsa.dossier_id' => $dossier_id
-                    ),
-                'recursive' => -1
-                )
-            );
+			$this->assert( valid_int( $dossier_id ), 'invalidParameter' );
 
-            $avispcg = $this->Avispcgdroitrsa->findByDossierId( $dossier_id );
+			$gracieux = $this->Avispcgdroitrsa->find(
+				'first',
+				array(
+					'conditions' => array(
+						'Avispcgdroitrsa.dossier_id' => $dossier_id
+					),
+				'recursive' => -1
+				)
+			);
+
+			$avispcg = $this->Avispcgdroitrsa->findByDossierId( $dossier_id );
 //             $this->assert( !empty( $gracieux ), 'error404' );
 
-            $this->set( 'dossier_id', $dossier_id );
-            $this->set( 'avispcg', $avispcg );
-            $this->set( 'gracieux', $gracieux );
-        }
+			$this->set( 'dossier_id', $dossier_id );
+			$this->set( 'avispcg', $avispcg );
+			$this->set( 'gracieux', $gracieux );
+		}
 
-        function contentieux( $dossier_id = null ){
+		/**
+		*
+		*/
 
-            $this->assert( valid_int( $dossier_id ), 'invalidParameter' );
+		public function contentieux( $dossier_id = null ){
+			$this->assert( valid_int( $dossier_id ), 'invalidParameter' );
 
-            $contentieux = $this->Avispcgdroitrsa->find(
-                'first',
-                array(
-                    'conditions' => array(
-                        'Avispcgdroitrsa.dossier_id' => $dossier_id
-                    ),
-                'recursive' => -1
-                )
-            );
+			$contentieux = $this->Avispcgdroitrsa->find(
+				'first',
+				array(
+					'conditions' => array(
+						'Avispcgdroitrsa.dossier_id' => $dossier_id
+					),
+				'recursive' => -1
+				)
+			);
 //             $this->assert( !empty( $contentieux ), 'error404' );
 
-            $this->set( 'dossier_id', $dossier_id );
-            $this->set( 'contentieux', $contentieux );
-        }
-    }
+			$this->set( 'dossier_id', $dossier_id );
+			$this->set( 'contentieux', $contentieux );
+		}
+	}
 ?>
