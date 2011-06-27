9086850504f70b576e4c2282e36a95be02b64aaf
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-06-27 09:53:22 +0000

    Correction: réparation et nettoyage de la méthode javascript dependantSelect ( possibilité d'avoir plus de 2 select dépendants, optimisations mineures)

--- trunk/app/webroot/js/dependantselect.js
+++ trunk/app/webroot/js/dependantselect.js
@@ -1,70 +1,75 @@
-    //
-    // INFO:
-    // * si on veut avoir les valeurs exactes des select, on peut voir
-    //   pour les enlever / remettre avec des classes
-    // * les textes qu'on met dans la BDD pour les selects ne peuvent
-    //   pas comprendre ' - ' ... ou alors faire une variable
-    //
-    // - http://codylindley.com/Webdev/315/ie-hiding-option-elements-with-css-and-dealing-with-innerhtml
-    // - http://bytes.com/forum/thread92041.html
-    // - http://www.javascriptfr.com/codes/GERER-OPTGROUP-LISTE-DEROULANTE_36855.aspx
-    // - http://www.highdots.com/forums/alt-html/optgroup-optgroup-display-none-style-264456.html
-    //
+//
+// INFO:
+// * si on veut avoir les valeurs exactes des select, on peut voir
+//   pour les enlever / remettre avec des classes
+// * les textes qu'on met dans la BDD pour les selects ne peuvent
+//   pas comprendre ' - ' ... ou alors faire une variable
+//
+// - http://codylindley.com/Webdev/315/ie-hiding-option-elements-with-css-and-dealing-with-innerhtml
+// - http://bytes.com/forum/thread92041.html
+// - http://www.javascriptfr.com/codes/GERER-OPTGROUP-LISTE-DEROULANTE_36855.aspx
+// - http://www.highdots.com/forums/alt-html/optgroup-optgroup-display-none-style-264456.html
+//
 
-    //*****************************************************************
+//*****************************************************************
 
-    // var selects = new Array(); // TODO dependantselects
-    function dependantSelect( select2Id, select1Id ) {
-        var value2 = $F( select2Id );
+function dependantSelect( select2Id, select1Id ) {
+	var selects = new Array();
+	var value2 = $F( select2Id );
 
-        // selects[select2Id] = new Array();
-        // Nettoyage du texte des options
-        $$('#' + select2Id + ' option').each( function ( option ) {
-            var data = $(option).innerHTML;
-            $(option).update( data.replace( new RegExp( '^.* - ', 'gi' ), '' ) );
-        } );
+	// Nettoyage du texte des options
+	$$('#' + select2Id + ' option').each( function ( option ) {
+		var data = $(option).innerHTML;
+		$(option).update( data.replace( new RegExp( '^.* - ', 'gi' ), '' ) );
+	} );
 
-        // Sauvegarde
-        var select2Values = new Array();
-        var select2Options = new Array();
-        $$('#' + select2Id + ' option').each( function ( option ) {
-            select2Values.push( option.value );
-            select2Options.push( option.innerHTML );
-        } );
+	// Sauvegarde
+	if( selects[select2Id] == undefined ) {
+		selects[select2Id] = new Array();
+		selects[select2Id]['values'] = new Array();
+		selects[select2Id]['options'] = new Array();
+	}
 
-        // Vidage de la liste
-        $$('#' + select2Id + ' option').each( function ( option ) {
-            if( ( $(option).value != '' ) && ( ( $(option).value != '' ) && ( $(option).value.match( new RegExp( '^' + $F( select1Id ) + '_', 'gi' ) ) == null ) ) )
-            $(option).remove();
-        } );
+	$$('#' + select2Id + ' option').each( function ( option ) {
+		selects[select2Id]['values'].push( option.value );
+		selects[select2Id]['options'].push( option.innerHTML );
+	} );
 
-        // Onchage event - Partie dynamique
-		Event.observe( select1Id, 'change', function(event) {
-            $$('#' + select2Id + ' option').each( function ( option ) {
-                $(option).remove();
-            } );
+	// Vidage de la liste
+	var select1ValueRegexp = new RegExp( '^' + $F( select1Id ).replace( new RegExp( '^[^_]+_', 'gi' ), '' ) + '_', 'gi' );
+	$$('#' + select2Id + ' option').each( function ( option ) {
+		if( ( $(option).value != '' ) && ( ( $(option).value != '' ) && ( $( option ).value.match( select1ValueRegexp ) == null ) ) )
+		$(option).remove();
+	} );
 
-            // INFO: pour les select dépendants en cascade
-            var select1IdValue = $(select1Id).value.replace( new RegExp( '^[^_]+_', 'gi' ), '' );
-
-            for( var i = 0 ; i < select2Values.length ; i++ ) {
-                if( select2Values[i] == '' || select2Values[i].match( new RegExp( '^' + select1IdValue + '_' ), "g" ) ) {
-                    $(select2Id).insert( new Element( 'option', { 'value': select2Values[i] } ).update( select2Options[i] ) );
-                }
-            }
-
-            var opt = $$('#' + select2Id + ' option');
-            $( opt ).each( function ( option ) {
-                if( $(option).value == value2 ) {
-                    $(option).selected = 'selected';
-                }
-            } );
-
-            try {
-                // INFO -> fonctionne quand même, mais génère une erreur
-                $( select2Id ).onchange();
-            }
-            catch(id) {
-            }
+	// Onchage event - Partie dynamique
+	Event.observe( select1Id, 'change', function( event ) {
+		$$('#' + select2Id + ' option').each( function ( option ) {
+			$(option).remove();
 		} );
-    }
+
+		// INFO: pour les select dépendants en cascade
+		var select1IdValue = $( select1Id ).value.replace( new RegExp( '^[^_]+_', 'gi' ), '' );
+		var select1IdRegexp = new RegExp( '^' + select1IdValue + '_' );
+
+		for( var i = 0 ; i < selects[select2Id]['values'].length ; i++ ) {
+			if( selects[select2Id]['values'][i] == '' || selects[select2Id]['values'][i].match( select1IdRegexp, "g" ) ) {
+				$(select2Id).insert( new Element( 'option', { 'value': selects[select2Id]['values'][i] } ).update( selects[select2Id]['options'][i] ) );
+			}
+		}
+
+		var opt = $$('#' + select2Id + ' option');
+		$( opt ).each( function ( option ) {
+			if( $(option).value == value2 ) {
+				$(option).selected = 'selected';
+			}
+		} );
+
+		// INFO: fonctionne quand même, mais génère une erreur
+		try {
+			$( select2Id ).onchange();
+		}
+		catch(id) {
+		}
+	} );
+}
