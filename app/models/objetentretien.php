6a96fb809ced76a4f3eb64cc8db89bfe63aec170
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-06-30 13:49:01 +0000

    ajotu d'une règle de validation pour avoir un message d'erreur au lieu d'une erreur sql

--- app/models/objetentretien.php
+++ app/models/objetentretien.php
@@ -1,32 +1,41 @@
 <?php
-    class Objetentretien extends AppModel
-    {
-        public $name = 'Objetentretien';
+	class Objetentretien extends AppModel
+	{
+		public $name = 'Objetentretien';
 
-        public $displayField = 'name';
+		public $displayField = 'name';
 
-        public $order = 'Objetentretien.id ASC';
+		public $order = 'Objetentretien.id ASC';
 
-        public $actsAs = array(
-            'Autovalidate',
-            'Formattable',
-            'Enumerable'
-        );
+		public $actsAs = array(
+			'Autovalidate',
+			'Formattable',
+			'Enumerable'
+		);
 
-        public $hasMany = array(
-            'Entretien' => array(
-                'className' => 'Entretien',
-                'foreignKey' => 'objetentretien_id',
-                'dependent' => true,
-                'conditions' => '',
-                'fields' => '',
-                'order' => '',
-                'limit' => '',
-                'offset' => '',
-                'exclusive' => '',
-                'finderQuery' => '',
-                'counterQuery' => ''
-            )
-        );
-    }
+		public $validate = array(
+			'name' => array(
+				array(
+						'rule' => 'isUnique',
+						'message' => 'Valeur déjà utilisée'
+				),
+			)
+		);
+
+		public $hasMany = array(
+			'Entretien' => array(
+				'className' => 'Entretien',
+				'foreignKey' => 'objetentretien_id',
+				'dependent' => true,
+				'conditions' => '',
+				'fields' => '',
+				'order' => '',
+				'limit' => '',
+				'offset' => '',
+				'exclusive' => '',
+				'finderQuery' => '',
+				'counterQuery' => ''
+			)
+		);
+	}
 ?>
