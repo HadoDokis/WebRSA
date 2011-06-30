ea9d3b3569d6abab202ee320c3630b7c51ee3e5a
gdumont <gdumont@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-06-30 13:10:00 +0000

    nettoyage de code et mise à jour de la vue avec le helper defaut2

--- trunk/app/controllers/originespdos_controller.php
+++ trunk/app/controllers/originespdos_controller.php
@@ -1,73 +1,73 @@
 <?php
 
-    class OriginespdosController extends AppController 
-    {
-        var $name = 'Originespdos';
-        var $uses = array( 'Originepdo', 'Propopdo', 'Option' );
-        var $helpers = array( 'Xform', 'Default', 'Theme' );
-        var $components = array( 'Default' );
-        
-		var $commeDroit = array(
+	class OriginespdosController extends AppController 
+	{
+		public $name = 'Originespdos';
+		public $uses = array( 'Originepdo', 'Propopdo', 'Option' );
+		public $helpers = array( 'Xform', 'Default', 'Default2', 'Theme' );
+		public $components = array( 'Default' );
+		
+		public $commeDroit = array(
 			'view' => 'Originespdos:index',
 			'add' => 'Originespdos:edit'
 		);
 
-        /**
-        *   Ajout à la suite de l'utilisation des nouveaux helpers
-        *   - default.php
-        *   - theme.php
-        */
+		/**
+		*   Ajout à la suite de l'utilisation des nouveaux helpers
+		*   - default.php
+		*   - theme.php
+		*/
 
-        public function index() {
-            $this->set(
-                Inflector::tableize( $this->modelClass ),
-                $this->paginate( $this->modelClass )
-            );
-        }
+		public function index() {
+			$this->set(
+				Inflector::tableize( $this->modelClass ),
+				$this->paginate( $this->modelClass )
+			);
+		}
 
-        /**
-        *
-        */
+		/**
+		*
+		*/
 
-        public function add() {
-            $args = func_get_args();
-            call_user_func_array( array( $this, '_add_edit' ), $args );
-        }
+		public function add() {
+			$args = func_get_args();
+			call_user_func_array( array( $this, '_add_edit' ), $args );
+		}
 
-        /**
-        *
-        */
+		/**
+		*
+		*/
 
-        public function edit() {
-            $args = func_get_args();
-            call_user_func_array( array( $this, '_add_edit' ), $args );
-        }
+		public function edit() {
+			$args = func_get_args();
+			call_user_func_array( array( $this, '_add_edit' ), $args );
+		}
 
-        /**
-        *
-        */
+		/**
+		*
+		*/
 
-        function _add_edit(){
-            $args = func_get_args();
-            $this->Default->{$this->action}( $args );
-        }
+		function _add_edit(){
+			$args = func_get_args();
+			$this->Default->{$this->action}( $args );
+		}
 
-        /**
-        *
-        */
+		/**
+		*
+		*/
 
-        public function delete( $id ) {
-            $this->Default->delete( $id );
-        }
+		public function delete( $id ) {
+			$this->Default->delete( $id );
+		}
 
-        /**
-        *
-        */
+		/**
+		*
+		*/
 
-        public function view( $id ) {
+		public function view( $id ) {
 
-            $this->Default->view( $id );
-        }
+			$this->Default->view( $id );
+		}
 
-    }
+	}
 ?>
--- trunk/app/views/originespdos/add_edit.ctp
+++ trunk/app/views/originespdos/add_edit.ctp
@@ -1,21 +1,21 @@
 <?php
-    echo $xhtml->tag(
-        'h1',
-        $this->pageTitle = __d( 'originepdo', "Originespdos::{$this->action}", true )
-    )
+	echo $xhtml->tag(
+		'h1',
+		$this->pageTitle = __d( 'originepdo', "Originespdos::{$this->action}", true )
+	)
 ?>
 <?php echo $xhtml->css( array( 'all.form' ), 'stylesheet', array( 'media' => 'all' ), false );?>
 
 <?php
-    echo $default->form(
-        array(
-            'Originepdo.libelle'
-        ),
-        array(
-            'actions' => array(
-                'Originepdo.save',
-                'Originepdo.cancel'
-            )
-        )
-    );
+	echo $default->form(
+		array(
+			'Originepdo.libelle'
+		),
+		array(
+			'actions' => array(
+				'Originepdo.save',
+				'Originepdo.cancel'
+			)
+		)
+	);
 ?>
