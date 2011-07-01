df0adf30aaaace3dc8d1c913f02b046fc982eedb
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2011-07-01 13:56:27 +0000

    Correction d'un patch SQL pour le CG 58

--- trunk/app/config/sql/patches/2.x/patch-2.0-rc16-datas-cg58.sql
+++ trunk/app/config/sql/patches/2.x/patch-2.0-rc16-datas-cg58.sql
@@ -11,34 +11,35 @@ SET default_with_oids = false;
 BEGIN;
 -- *****************************************************************************
 
-TRUNCATE informationspe CASCADE;
-TRUNCATE historiqueetatspe CASCADE;
-
-ALTER SEQUENCE informationspe_id_seq RESTART WITH 1;
-ALTER SEQUENCE historiqueetatspe_id_seq RESTART WITH 1;
-
--- *****************************************************************************
-
-INSERT INTO informationspe ( nir, nom, prenom, dtnai )
-	SELECT
-		NULL AS nir,
-		nom,
-		prenom,
-		dtnai
-	FROM personnes
-	WHERE personnes.id IN ( 15, 92, 48, 74, 117 )
-	ORDER BY personnes.id;
-
-INSERT INTO historiqueetatspe ( informationpe_id, identifiantpe, date, etat ) VALUES
-	( 1, '0611044290Y', '2011-01-01', 'radiation' ),
-	( 2, '0611717975P', '2011-01-01', 'radiation' ),
-	( 3, '0613061080L', '2011-01-01', 'radiation' ),
-	( 4, '0611309465G', '2011-01-01', 'radiation' ),
-	( 5, '0610905944X', '2011-01-01', 'radiation' )
-;
+-- INFO: pour les développeurs seulement
+-- TRUNCATE informationspe CASCADE;
+-- TRUNCATE historiqueetatspe CASCADE;
+-- 
+-- ALTER SEQUENCE informationspe_id_seq RESTART WITH 1;
+-- ALTER SEQUENCE historiqueetatspe_id_seq RESTART WITH 1;
+-- 
+-- -- *****************************************************************************
+-- 
+-- INSERT INTO informationspe ( nir, nom, prenom, dtnai )
+-- 	SELECT
+-- 		NULL AS nir,
+-- 		nom,
+-- 		prenom,
+-- 		dtnai
+-- 	FROM personnes
+-- 	WHERE personnes.id IN ( 15, 92, 48, 74, 117 )
+-- 	ORDER BY personnes.id;
+-- 
+-- INSERT INTO historiqueetatspe ( informationpe_id, identifiantpe, date, etat ) VALUES
+-- 	( 1, '0611044290Y', '2011-01-01', 'radiation' ),
+-- 	( 2, '0611717975P', '2011-01-01', 'radiation' ),
+-- 	( 3, '0613061080L', '2011-01-01', 'radiation' ),
+-- 	( 4, '0611309465G', '2011-01-01', 'radiation' ),
+-- 	( 5, '0610905944X', '2011-01-01', 'radiation' )
+-- ;
 
 TRUNCATE listesanctionseps58 CASCADE;
-SELECT pg_catalog.setval('listesanctionseps58_id_seq', ( SELECT COALESCE( max(listesanctionseps58.id) + 1, 1 ) FROM listesanctionseps58 ), false);
+ALTER SEQUENCE listesanctionseps58_id_seq RESTART WITH 1;
 INSERT INTO listesanctionseps58 (rang, sanction, duree) VALUES
     ('1', '-100€', '1'),
     ('2', '-50%', '4'),
