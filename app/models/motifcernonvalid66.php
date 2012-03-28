c47943bc60fb6eb28aadcb15dcdbcb7fb37b3066
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-03-28 09:58:47 +0000

    Amélioration: le courrier de fiche de liaison du CER 66 récupère bien les motifs de non validation du CER (CG66)

--- app/models/motifcernonvalid66.php
+++ app/models/motifcernonvalid66.php
@@ -29,5 +29,45 @@
 				'with' => 'Motifcernonvalid66Propodecisioncer66'
 			)
 		);
+		
+		/**
+		 * Retourne une sous-requête permettant d'obtenir la liste des pièces liées
+		 * à une aide APRE du 66. Les éléments de la liste sont triés et préfixés par une
+		 * chaîne de caractères.
+		 *
+		 * @param string $aideapre66Id
+		 * @param string $prefix
+		 * @return string
+		 */
+		public function vfListeMotifs( $propodecisioncer66Id = 'Propodecisioncer66.id', $prefix = '\\n\r-' ) {
+			$alias = Inflector::tableize( $this->alias );
+
+			$sq = $this->sq(
+				array(
+					'alias' => $alias,
+					'fields' => array(
+						"'{$prefix}' || \"{$alias}\".\"name\" AS \"{$alias}__name\""
+					),
+					'contain' => false,
+					'joins' => array(
+						array_words_replace(
+							$this->join( 'Motifcernonvalid66Propodecisioncer66', array( 'type' => 'INNER' ) ),
+							array(
+								'Motifcernonvalid66Propodecisioncer66' => 'motifscersnonvalids66_proposdecisionscers66',
+								'Motifcernonvalid66' => 'motifscersnonvalids66'
+							)
+						),
+					),
+					'conditions' => array(
+						"motifscersnonvalids66_proposdecisionscers66.propodecisioncer66_id = {$propodecisioncer66Id}"
+					),
+					'order' => array(
+						"{$alias}.name ASC"
+					)
+				)
+			);
+
+			return "ARRAY_TO_STRING( ARRAY( {$sq} ), '' )";
+		}
 	}
 ?>
