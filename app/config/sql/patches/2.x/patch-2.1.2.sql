ecb44b9958776885514f742287881d7cbee825c0
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-06-07 14:47:28 +0000

    Correction: pour la thématique d'EP "Maintien dans le social" du CG 58, on ne créait pas de nouvelle orientation lors d'un maintien de référent alors queon pouvait choisir un autre type d'orientation, une autre structure référente et un autre référent. Le patch SQL se charge d'ajouter les orientations manquantes pour cette raison et de mettre à jour le référent; à présent le référent est bien enregistré également lors de la création d'une orientation suite aux décisions.

--- app/config/sql/patches/2.x/patch-2.1.2.sql
+++ app/config/sql/patches/2.x/patch-2.1.2.sql
@@ -61,6 +61,135 @@ CREATE UNIQUE INDEX decisionsproposnonorientationsproscovs58_passagecov58_id_idx
 DROP INDEX IF EXISTS decisionsproposorientationscovs58_passagecov58_id_idx;
 CREATE UNIQUE INDEX decisionsproposorientationscovs58_passagecov58_id_idx ON decisionsproposorientationscovs58 (passagecov58_id);
 
+-- -----------------------------------------------------------------------------------------------------------
+-- 20120607: correction: une décision de maintienref pour la thématique nonorientationsproseps58
+-- entraîne tout de même la création d'une nouvelle orientation
+-- -----------------------------------------------------------------------------------------------------------
+
+DROP INDEX orientsstructs_personne_id_rgorient_idx;
+ALTER TABLE orientsstructs DROP CONSTRAINT orientsstructs_statut_orient_oriente_rgorient_not_null_chk;
+ALTER TABLE orientsstructs DROP CONSTRAINT orientsstructs_origine_check;
+
+UPDATE orientsstructs
+	SET statut_orient = 'Orienté'
+	WHERE
+		typeorient_id IS NOT NULL
+		AND structurereferente_id IS NOT NULL
+		AND date_valid IS NOT NULL;
+
+INSERT INTO orientsstructs ( personne_id, typeorient_id, structurereferente_id, referent_id, date_propo, date_valid, statut_orient, rgorient, etatorient, user_id, origine )
+	SELECT
+			dossierseps.personne_id AS personne_id,
+			decisionsnonorientationsproseps58.typeorient_id AS typeorient_id,
+			decisionsnonorientationsproseps58.structurereferente_id AS structurereferente_id,
+			decisionsnonorientationsproseps58.referent_id AS referent_id,
+			DATE_TRUNC( 'day', nonorientationsproseps58.created ) AS date_propo,
+			DATE_TRUNC( 'day', commissionseps.dateseance ) AS date_valid,
+			'Orienté' AS statut_orient,
+			NULL AS rgorient,
+			'decision' AS etatorient,
+			nonorientationsproseps58.user_id AS user_id,
+			'reorientation' AS origine
+		FROM decisionsnonorientationsproseps58
+			INNER JOIN passagescommissionseps ON ( decisionsnonorientationsproseps58.passagecommissionep_id  =passagescommissionseps.id )
+			INNER JOIN dossierseps ON ( passagescommissionseps.dossierep_id = dossierseps.id )
+			INNER JOIN nonorientationsproseps58 ON ( nonorientationsproseps58.dossierep_id = dossierseps.id )
+			INNER JOIN commissionseps ON ( passagescommissionseps.commissionep_id = commissionseps.id )
+		WHERE
+			commissionseps.etatcommissionep = 'traite'
+			AND passagescommissionseps.etatdossierep = 'traite'
+			AND decisionsnonorientationsproseps58.decision = 'maintienref';
+
+UPDATE orientsstructs SET rgorient = NULL;
+UPDATE orientsstructs
+	SET rgorient = (
+		SELECT ( COUNT(orientsstructspcd.id) + 1 )
+			FROM orientsstructs AS orientsstructspcd
+			WHERE orientsstructspcd.personne_id = orientsstructs.personne_id
+				AND orientsstructspcd.id <> orientsstructs.id
+				AND orientsstructs.date_valid IS NOT NULL
+				AND orientsstructspcd.date_valid IS NOT NULL
+				AND (
+					orientsstructspcd.date_valid < orientsstructs.date_valid
+					OR ( orientsstructspcd.date_valid = orientsstructs.date_valid AND orientsstructspcd.id < orientsstructs.id )
+				)
+				AND orientsstructs.statut_orient = 'Orienté'
+				AND orientsstructspcd.statut_orient = 'Orienté'
+	)
+	WHERE
+		orientsstructs.date_valid IS NOT NULL
+		AND orientsstructs.statut_orient = 'Orienté';
+
+UPDATE orientsstructs
+	SET origine = 'manuelle'
+	WHERE rgorient = 1 AND origine = 'reorientation';
+
+UPDATE orientsstructs
+	SET origine = 'reorientation'
+	WHERE rgorient > 1 AND origine <> 'reorientation';
+
+CREATE UNIQUE INDEX orientsstructs_personne_id_rgorient_idx ON orientsstructs( personne_id, rgorient ) WHERE rgorient IS NOT NULL;
+
+ALTER TABLE orientsstructs ADD CONSTRAINT orientsstructs_statut_orient_oriente_rgorient_not_null_chk CHECK (
+	( statut_orient <> 'Orienté' AND rgorient IS NULL )
+	OR ( statut_orient = 'Orienté' AND rgorient IS NOT NULL )
+);
+
+ALTER TABLE orientsstructs ADD CONSTRAINT orientsstructs_origine_check CHECK(
+	( origine IS NULL AND date_valid IS NULL )
+	OR (
+		( origine IS NOT NULL AND date_valid IS NOT NULL )
+		AND (
+			( rgorient = 1 AND origine IN ( 'manuelle', 'cohorte' ) )
+			OR ( rgorient > 1 AND origine = 'reorientation' )
+		)
+	)
+);
+
+-- On n'enregistrait pas le referent_id dans les orientsstructs
+CREATE OR REPLACE FUNCTION public.update_orientsstructs_decisionsnonorientationsproseps58() RETURNS VOID AS
+$$
+	DECLARE
+		v_row   record;
+		v_query text;
+	BEGIN
+		FOR v_row IN
+			SELECT
+					decisionsnonorientationsproseps58.referent_id AS referent_id,
+					orientsstructs.id AS orientstruct_id
+				FROM decisionsnonorientationsproseps58
+					INNER JOIN passagescommissionseps ON ( decisionsnonorientationsproseps58.passagecommissionep_id  =passagescommissionseps.id )
+					INNER JOIN dossierseps ON ( passagescommissionseps.dossierep_id = dossierseps.id )
+					INNER JOIN nonorientationsproseps58 ON ( nonorientationsproseps58.dossierep_id = dossierseps.id )
+					INNER JOIN commissionseps ON ( passagescommissionseps.commissionep_id = commissionseps.id )
+					INNER JOIN orientsstructs ON (
+						dossierseps.personne_id = orientsstructs.personne_id
+						AND decisionsnonorientationsproseps58.typeorient_id = orientsstructs.typeorient_id
+						AND decisionsnonorientationsproseps58.structurereferente_id = orientsstructs.structurereferente_id
+						AND decisionsnonorientationsproseps58.referent_id = orientsstructs.referent_id
+						AND DATE_TRUNC( 'day', nonorientationsproseps58.created ) = orientsstructs.date_propo
+						AND DATE_TRUNC( 'day', commissionseps.dateseance ) = orientsstructs.date_valid
+						AND orientsstructs.statut_orient = 'Orienté'
+						AND orientsstructs.etatorient = 'decision'
+					)
+				WHERE
+					commissionseps.etatcommissionep = 'traite'
+					AND passagescommissionseps.etatdossierep = 'traite'
+					AND decisionsnonorientationsproseps58.decision IN ( 'maintienref', 'reorientation' )
+					AND orientsstructs.referent_id IS NULL
+		LOOP
+			-- Mise à jour dans la table orientsstructs
+			v_query := 'UPDATE orientsstructs SET referent_id = ' || v_row.referent_id || ' WHERE id = ' || v_row.orientstruct_id || ';';
+			RAISE NOTICE  '%', v_query;
+			EXECUTE v_query;
+		END LOOP;
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT public.update_orientsstructs_decisionsnonorientationsproseps58();
+DROP FUNCTION public.update_orientsstructs_decisionsnonorientationsproseps58();
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
