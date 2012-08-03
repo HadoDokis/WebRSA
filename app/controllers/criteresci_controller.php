be2d91562a52aff6731634c25dd30bcca71a513a
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-08-03 07:43:20 +0000

    Modification: ajout de l'adresse complète de l'allocataire dans le tableau d'export CSV de la recherche par CER

--- app/controllers/criteresci_controller.php
+++ app/controllers/criteresci_controller.php
@@ -57,6 +57,8 @@
 			$this->set( 'forme_ci', $forme_ci );
 			
 			$this->set( 'etatdosrsa', $this->Option->etatdosrsa( $this->Situationdossierrsa->etatOuvert()) );
+			$this->set( 'typevoie', $this->Option->typevoie() );
+			$this->set( 'qual', $this->Option->qual() );
 			
 			$this->set(
 				'trancheage',
