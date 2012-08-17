655e81ff7a709ec5497258565d363ab4e7785fc8
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-08-17 09:16:09 +0000

    Amélioration: refactorisation de la classe XhtmlHelper, ajout de commentaires à la classe AppHelper

--- trunk/app/app_helper.php
+++ trunk/app/app_helper.php
@@ -1,11 +1,30 @@
 <?php
+	/**
+	 * Fichier source de la classe AppHelper.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package       app.View.Helper
+	 */
+	if( CAKE_BRANCH != '1.2' ) {
+		App::uses( 'Helper', 'View' );
+	}
+
+	/**
+	 * Class AppHelper
+	 * La classe parente de tous les helpers.
+	 *
+	 * @package       app.View.Helper
+	 */
 	class AppHelper extends Helper
 	{
 		/**
-		*
-		*/
-
-		protected function _cacheKey( $modelName ) {
+		 * Retourne une clé de cache pour un nom de modèle donné.
+		 *
+		 * @param string $modelName
+		 * @return string
+		 */
+		 protected function _cacheKey( $modelName ) {
 			$thisClassName = Inflector::underscore( get_class( $this ) );
 			$modelName = Inflector::tableize( $modelName );
 			return "{$thisClassName}_{$modelName}";
