2ba551cf3f8f0739545874b645f305e07e63d5a4
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-09-05 15:30:22 +0000

    Modification: suppression des recursive > -1 dans les modèles en possédant encore

--- trunk/app/models/calculdroitrsa.php
+++ trunk/app/models/calculdroitrsa.php
@@ -37,34 +37,22 @@
 		*
 		*/
 
-		public function dossierId( $ressource_id ) {
-			$this->unbindModelAll();
-			$this->bindModel(
-				array(
-					'hasOne' => array(
-						'Personne' => array(
-							'foreignKey' => false,
-							'conditions' => array( 'Personne.id = Calculdroitrsa.personne_id' )
-						),
-						'Foyer' => array(
-							'foreignKey' => false,
-							'conditions' => array( 'Foyer.id = Personne.foyer_id' )
-						)
-					)
-				)
-			);
-			$qd_ressource = array(
-				'conditions' => array(
-					'Ressource.id' => $ressource_id
+		public function dossierId( $calculdroitrsa_id ) {
+			$qd_calculdroitrsa = array(
+				'fields' => array( 'Foyer.dossier_id' ),
+				'joins' => array(
+					$this->join( 'Personne', array( 'type' => 'INNER' ) ),
+					$this->Personne->join( 'Foyer', array( 'type' => 'INNER' ) )
 				),
-				'fields' => null,
-				'order' => null,
-				'recursive' => 1
+				'conditions' => array(
+					'Calculdroitrsa.id' => $calculdroitrsa_id
+				),
+				'recursive' => -1
 			);
-			$ressource = $this->find('first', $qd_ressource);
+			$calculdroitrsa = $this->find('first', $qd_calculdroitrsa);
 
-			if( !empty( $ressource ) ) {
-				return $ressource['Foyer']['dossier_id'];
+			if( !empty( $calculdroitrsa ) ) {
+				return $calculdroitrsa['Foyer']['dossier_id'];
 			}
 			else {
 				return null;
