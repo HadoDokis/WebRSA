0e5e9bae77bbd8839911dca2ae834925144e88aa
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-10-08 17:38:07 +0000

    Passage en version 2.2.2

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1,177 +1,252 @@
 <?php
-/* SVN FILE: $Id: bootstrap.php 7945 2008-12-19 02:16:01Z gwoo $ */
-/**
- * Short description for file.
- *
- * Long description for file
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app.config
- * @since         CakePHP(tm) v 0.10.8.2117
- * @version       $Revision: 7945 $
- * @modifiedby    $LastChangedBy: gwoo $
- * @lastmodified  $Date: 2008-12-18 18:16:01 -0800 (Thu, 18 Dec 2008) $
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
- */
-/**
- *
- * This file is loaded automatically by the app/webroot/index.php file after the core bootstrap.php is loaded
- * This is an application wide file to load any function that is not used within a class define.
- * You can also use this to include or require any files in your application.
- *
- */
-/**
- * The settings below can be used to set additional paths to models, views and controllers.
- * This is related to Ticket #470 (https://trac.cakephp.org/ticket/470)
- *
- * $modelPaths = array('full path to models', 'second full path to models', 'etc...');
- * $viewPaths = array('this path to views', 'second full path to views', 'etc...');
- * $controllerPaths = array('this path to controllers', 'second full path to controllers', 'etc...');
- *
- */
+	/**
+	 * This file is loaded automatically by the app/webroot/index.php file after core.php
+	 *
+	 * This file should load/create any application wide configuration settings, such as
+	 * Caching, Logging, loading additional configuration files.
+	 *
+	 * You should also use this file to include any files that provide global functions/constants
+	 * that your application uses.
+	 *
+	 * PHP 5
+	 *
+	 * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+	 * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+	 * @link          http://cakephp.org CakePHP(tm) Project
+	 * @package       app.Config
+	 * @since         CakePHP(tm) v 0.10.8.2117
+	 * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+	 */
+	/**
+	 * Cache Engine Configuration
+	 * Default settings provided below
+	 *
+	 * File storage engine.
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'File', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
+	 * 		'prefix' => 'cake_', //[optional]  prefix every cache file with this string
+	 * 		'lock' => false, //[optional]  use file locking
+	 * 		'serialize' => true, // [optional]
+	 * 		'mask' => 0666, // [optional] permission mask to use when creating cache files
+	 * 	));
+	 *
+	 * APC (http://pecl.php.net/package/APC)
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'Apc', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+	 * 	));
+	 *
+	 * Xcache (http://xcache.lighttpd.net/)
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'Xcache', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional] prefix every cache file with this string
+	 * 		'user' => 'user', //user from xcache.admin.user settings
+	 * 		'password' => 'password', //plaintext password (xcache.admin.pass)
+	 * 	));
+	 *
+	 * Memcache (http://memcached.org/)
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'Memcache', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+	 * 		'servers' => array(
+	 * 			'127.0.0.1:11211' // localhost, default port 11211
+	 * 		), //[optional]
+	 * 		'persistent' => true, // [optional] set this to false for non-persistent connections
+	 * 		'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
+	 * 	));
+	 *
+	 *  Wincache (http://php.net/wincache)
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'Wincache', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+	 * 	));
+	 *
+	 * Redis (http://http://redis.io/)
+	 *
+	 * 	 Cache::config('default', array(
+	 * 		'engine' => 'Redis', //[required]
+	 * 		'duration'=> 3600, //[optional]
+	 * 		'probability'=> 100, //[optional]
+	 * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+	 * 		'server' => '127.0.0.1' // localhost
+	 * 		'port' => 6379 // default port 6379
+	 * 		'timeout' => 0 // timeout in seconds, 0 = unlimited
+	 * 		'persistent' => true, // [optional] set this to false for non-persistent connections
+	 * 	));
+	 */
+	Cache::config( 'default', array( 'engine' => 'File' ) );
 
-    //@set_time_limit( 0 );
-    @ini_set( 'memory_limit', '512M' );
+	/**
+	 * The settings below can be used to set additional paths to models, views and controllers.
+	 *
+	 * App::build(array(
+	 *     'Model'                     => array('/path/to/models', '/next/path/to/models'),
+	 *     'Model/Behavior'            => array('/path/to/behaviors', '/next/path/to/behaviors'),
+	 *     'Model/Datasource'          => array('/path/to/datasources', '/next/path/to/datasources'),
+	 *     'Model/Datasource/Database' => array('/path/to/databases', '/next/path/to/database'),
+	 *     'Model/Datasource/Session'  => array('/path/to/sessions', '/next/path/to/sessions'),
+	 *     'Controller'                => array('/path/to/controllers', '/next/path/to/controllers'),
+	 *     'Controller/Component'      => array('/path/to/components', '/next/path/to/components'),
+	 *     'Controller/Component/Auth' => array('/path/to/auths', '/next/path/to/auths'),
+	 *     'Controller/Component/Acl'  => array('/path/to/acls', '/next/path/to/acls'),
+	 *     'View'                      => array('/path/to/views', '/next/path/to/views'),
+	 *     'View/Helper'               => array('/path/to/helpers', '/next/path/to/helpers'),
+	 *     'Console'                   => array('/path/to/consoles', '/next/path/to/consoles'),
+	 *     'Console/Command'           => array('/path/to/commands', '/next/path/to/commands'),
+	 *     'Console/Command/Task'      => array('/path/to/tasks', '/next/path/to/tasks'),
+	 *     'Lib'                       => array('/path/to/libs', '/next/path/to/libs'),
+	 *     'Locale'                    => array('/path/to/locales', '/next/path/to/locales'),
+	 *     'Vendor'                    => array('/path/to/vendors', '/next/path/to/Vendor'),
+	 *     'Plugin'                    => array('/path/to/plugins', '/next/path/to/plugins'),
+	 * ));
+	 *
+	 */
+	/**
+	 * Custom Inflector rules, can be set to correctly pluralize or singularize table, model, controller names or whatever other
+	 * string is passed to the inflection functions
+	 *
+	 * Inflector::rules('singular', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
+	 * Inflector::rules('plural', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
+	 *
+	 */
+	require_once( dirname( __FILE__ ).DS.'inflections.php' );
+	Inflector::rules(
+		'plural',
+		array(
+			'irregular' => $irregularPlural,
+			'uninflected' => $uninflectedPlural
+		)
+	);
+
+	/**
+	 * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
+	 * Uncomment one of the lines below, as you need. make sure you read the documentation on CakePlugin to use more
+	 * advanced ways of loading plugins
+	 *
+	 * CakePlugin::loadAll(); // Loads all plugins at once
+	 * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
+	 *
+	 */
+	CakePlugin::loadAll();
+
+	/**
+	 * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
+	 *
+	 * - AssetDispatcher filter will serve your asset files (css, images, js, etc) from your themes and plugins
+	 * - CacheDispatcher filter will read the Cache.check configure variable and try to serve cached content generated from controllers
+	 *
+	 * Feel free to remove or add filters as you see fit for your application. A few examples:
+	 *
+	 * Configure::write('Dispatcher.filters', array(
+	 * 		'MyCacheFilter', //  will use MyCacheFilter class from the Routing/Filter package in your app.
+	 * 		'MyPlugin.MyFilter', // will use MyFilter class from the Routing/Filter package in MyPlugin plugin.
+	 * 		array('callable' => $aFunction, 'on' => 'before', 'priority' => 9), // A valid PHP callback type to be called on beforeDispatch
+	 * 		array('callable' => $anotherMethod, 'on' => 'after'), // A valid PHP callback type to be called on afterDispatch
+	 *
+	 * ));
+	 */
+	Configure::write(
+		'Dispatcher.filters',
+		array(
+			'AssetDispatcher',
+			'CacheDispatcher'
+		)
+	);
+
+	/**
+	 * Configures default file logging options
+	 */
+	App::uses( 'CakeLog', 'Log' );
+	CakeLog::config(
+		'debug',
+		array(
+			'engine' => 'FileLog',
+			'types' => array( 'notice', 'info', 'debug' ),
+			'file' => 'debug',
+		)
+	);
+	CakeLog::config(
+		'error',
+		array(
+			'engine' => 'FileLog',
+			'types' => array( 'warning', 'error', 'critical', 'alert', 'emergency' ),
+			'file' => 'error',
+		)
+	);
+
+	/**
+	 *
+	 */
+	Configure::write( 'Config.language', 'fre' );
+	setlocale( 'LC_ALL', 'fr_FR.utf8' );
 
 	// Branche de CakePHP que l'on utilise: 1.1, 1.2, 1.3
 	define( 'CAKE_BRANCH', preg_replace( '/^([0-9]+\.[0-9]+)(?![0-9]).*$/', '\1', Configure::version() ) );
 
 	define( 'REQUIRED_MARK', '<abbr class="required" title="Champ obligatoire">*</abbr>' );
 
-	define( 'MODELESODT_DIR', APP.'vendors'.DS.'modelesodt'.DS );
+	// Définition de certains répertoires
+	define( 'MODELESODT_DIR', APP.'Vendor'.DS.'modelesodt'.DS );
+	define( 'CONFIGS', APP.DS.'Config'.DS );
+	define( 'ABSTRACTMODELS', APP.DS.'Model'.DS.'Abstractclass'.DS );
 
-    // Messages en français
-    App::import( 'l10n' );
-    $this->L10n = new L10n();
-    $this->L10n->get( 'fre' );
+	require_once( CONFIGS.'webrsa.inc' );
+	require_once( APPLIBS.'basics.php' );
+	require_once( APPLIBS.'xset.php' );
+	require_once( APPLIBS.'xinflector.php' );
+	require_once( APP.DS.'Vendor'.DS.'money_format.php' );
+	require_once( APP.'Lib'.DS.'Error'.DS.'rsa_exceptions.php' );
 
-    // Dates, nombres, ... en français
-    $locales = array( 'fr_FR', 'fr_FR@euro', 'fr', 'fr_FR.UTF8' );
-    setlocale( LC_ALL, $locales );
-
-    mb_internal_encoding( Configure::read( 'App.encoding' ) );
-    mb_regex_encoding( Configure::read( 'App.encoding' ) );
-
-	require_once( APP.DS.'vendors'.DS.'money_format.php' );
-
-	// TODO: créer répertoire APP.'libs' + bouger les fichiers
-	if( CAKE_BRANCH == '1.2' ) {
-		// backport architecture et constantes 1.3
-		define( 'APPLIBS', APP.'libs/' );
+	/**
+	 * Retourne la version de CakePHP utilisée.
+	 *
+	 * @todo basics.php
+	 *
+	 * @return string
+	 */
+	function core_version() {
+		$versionData = explode( "\n", file_get_contents( ROOT.DS.'lib'.DS.'Cake'.DS.'VERSION.txt' ) );
+		$version = explode( '.', $versionData[count( $versionData ) - 1] );
+		return implode( '.', $version );
 	}
 
-    /** ************************************************************************
-    *
-    *** ***********************************************************************/
-
-    require_once( CONFIGS.'webrsa.inc' );
-    require_once( APPLIBS.'basics.php' );
-    require_once( APPLIBS.'xset.php' );
-    require_once( APPLIBS.'xinflector.php' );
-
-    /** ************************************************************************
-    *
-    * Versioning
-    *
-    ** ************************************************************************/
-
-    function core_version() {
-        $versionData = explode( "\n", file_get_contents( ROOT.DS.'cake'.DS.'VERSION.txt' ) );
-        $version = explode( '.', $versionData[count( $versionData) - 1] );
-        return implode( '.', $version );
-    }
-
-    function app_version() {
-        $versionData = explode( "\n", file_get_contents( ROOT.DS.'app'.DS.'VERSION.txt' ) );
-        $version = explode( '.', $versionData[count( $versionData) - 1] );
-        return implode( '.', $version );
-    }
-
-    /**
-    * @param array $array
-    * @param array $replacements
-    * @return array
-    */
-
-    /*function recursive_key_value_preg_replace( array $array, array $replacements ) {
-        $newArray = array();
-        foreach( $array as $key => $value ) {
-            foreach( $replacements as $pattern => $replacement ) {
-                $key = preg_replace( $pattern, $replacement, $key );
-            }
-
-            if( is_array( $value ) ) {
-                $value = recursive_key_value_preg_replace( $value, $replacements );
-            }
-            else {
-                foreach( $replacements as $pattern => $replacement ) {
-                    $value = preg_replace( $pattern, $replacement, $value );
-                }
-            }
-            $newArray[$key] = $value;
-        }
-        return $newArray;
-    }*/
-
-    /**
-    * INFO: http://fr.php.net/manual/fr/function.strpos.php#49739
-    */
-
-    /*function strallpos( $pajar, $aguja, $offset = 0, &$count = null ) {
-        if( $offset > strlen( $pajar ) ) {
-            trigger_error("strallpos(): Offset not contained in string.", E_USER_WARNING);
-        }
-        $match = array();
-        for( $count = 0; ( ( $pos = strpos( $pajar, $aguja, $offset ) ) !== false ); $count++ ) {
-            $match[] = $pos;
-            $offset = $pos + strlen( $aguja );
-        }
-        return $match;
-    }*/
-
-	//require_once( CONFIGS.'app.enumerable.php' );
-	/// TODO: options par défaut pour dans les vues -> Configure::read( 'View.display.phone' );
-
-	Configure::write( 'Typeable.phone', array( 'country' => 'fr', 'maxlength' => 14/*, 'rule' => 'phoneFr'*/ ) );
-	Configure::write( 'Typeable.amount', array( 'currency' => 'EUR' ) );
-	Configure::write( 'Typeable.date', array( 'dateFormat' => 'DMY' ) );
-	Configure::write( 'Typeable.enum::presence',
-		array(
-			'type' => 'enum',
-			'domain' => 'default',
-			'options' => array( 'absent', 'present', 'excuse', 'remplace' )
-		)
-	);
-	Configure::write( 'Typeable.email', array( 'rule' => 'email' ) );
-//EOF
-
-	// Un shell ? On va charger la classe parente
-	if( defined( 'CAKEPHP_SHELL' ) && CAKEPHP_SHELL && file_exists( APP.'/app_shell.php' ) ) {
-		require_once( APP.'/../cake/console/libs/shell.php' );
-		require_once( APP.'/app_shell.php' );
+	/**
+	 * Retourne la version de WebRSA utilisée.
+	 *
+	 * @todo basics.php
+	 *
+	 * @return string
+	 */
+	function app_version() {
+		$versionData = explode( "\n", file_get_contents( ROOT.DS.'app'.DS.'VERSION.txt' ) );
+		$version = explode( '.', $versionData[count( $versionData ) - 1] );
+		return implode( '.', $version );
 	}
 
-	// -------------------------------------------------------------------------
-	// Ajout de répertoires pour les classes abstraites
-	// -------------------------------------------------------------------------
-	define( 'ABSTRACTMODELS', MODELS.'abstractclasses/' );
-
-
-	// -----------------
-	// Paramétrage iconv
-	// -----------------
-
+	/**
+	 * Paramétrage iconv.
+	 */
 	iconv_set_encoding("input_encoding", "UTF-8");
 	iconv_set_encoding("output_encoding", "UTF-8");
 	iconv_set_encoding("internal_encoding", "UTF-8");
--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,40 +1,45 @@
 <?php
-/* SVN FILE: $Id: routes.php 7945 2008-12-19 02:16:01Z gwoo $ */
-/**
- * Short description for file.
- *
- * In this file, you set up routes to your controllers and their actions.
- * Routes are very important mechanism that allows you to freely connect
- * different urls to chosen controllers and their actions (functions).
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.app.config
- * @since         CakePHP(tm) v 0.2.9
- * @version       $Revision: 7945 $
- * @modifiedby    $LastChangedBy: gwoo $
- * @lastmodified  $Date: 2008-12-18 18:16:01 -0800 (Thu, 18 Dec 2008) $
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
- */
-/**
- * Here, we are connecting '/' (base path) to controller called 'Pages',
- * its action called 'display', and we pass a param to select the view file
- * to use (in this case, /app/views/pages/home.ctp)...
- */
+	/**
+	 * Routes configuration
+	 *
+	 * In this file, you set up routes to your controllers and their actions.
+	 * Routes are very important mechanism that allows you to freely connect
+	 * different urls to chosen controllers and their actions (functions).
+	 *
+	 * PHP 5
+	 *
+	 * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+	 * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+	 * @link          http://cakephp.org CakePHP(tm) Project
+	 * @package       app.Config
+	 * @since         CakePHP(tm) v 0.2.9
+	 * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+	 */
+	/**
+	 * Here, we are connecting '/' (base path) to controller called 'Pages',
+	 * its action called 'display', and we pass a param to select the view file
+	 * to use (in this case, /app/View/Pages/home.ctp)...
+	 */
 	Router::connect( '/', array( 'controller' => 'dossiers', 'action' => 'index' ) );
+	/**
+	 * ...and connect the rest of 'Pages' controller's urls.
+	 */
+	Router::connect( '/pages/*', array( 'controller' => 'pages', 'action' => 'display' ) );
 
-/**
- * ...and connect the rest of 'Pages' controller's urls.
- */
-	/*Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));*/
+	/**
+	 * Load all plugin routes.  See the CakePlugin documentation on
+	 * how to customize the loading of plugin routes.
+	 */
+	CakePlugin::routes();
+
+	/**
+	 * Load the CakePHP default routes. Remove this if you do not want to use
+	 * the built-in default routes.
+	 */
+	require CAKE.'Config'.DS.'routes.php';
 ?>
