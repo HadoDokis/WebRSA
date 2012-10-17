88742e4b1a97e810c205213b237fa15c15555e53
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-10-17 12:24:50 +0000

    Ajout de la première partie du code du nouveau CER pour le CG 93.

---
+++ app/Test/Fixture/Diplomecer93Fixture.php
@@ -0,0 +1,37 @@
+<?php
+	/**
+	 * Code source de la classe Diplomecer93Fixture.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package app.Test.Fixture
+	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+	 */
+
+	/**
+	 * Classe Diplomecer93Fixture.
+	 *
+	 * @package app.Test.Fixture
+	 */
+	class Diplomecer93Fixture extends CakeTestFixture
+	{
+		/**
+		 * On importe la définition de la table, pas les enregistrements.
+		 *
+		 * @var array
+		 */
+		public $import = array(
+			'model' => 'Diplomecer93',
+			'records' => false
+		);
+
+		/**
+		 * Définition des enregistrements.
+		 *
+		 * @var array
+		 */
+		public $records = array(
+		);
+
+	}
+?>
