b6b4b50bab6e94f74dcbbce8c33604441eebe3b0
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-10-25 05:34:17 +0000

    Nettoyage des sources liées aux tests unitaires.

--- app/Test/Fixture/cake_app_test_fixture.php
+++ app/Test/Fixture/cake_app_test_fixture.php
@@ -1,5 +1,20 @@
 <?php
-	class CakeAppTestFixture extends CakeTestFixture
+	/**
+	 * Code source de la classe CakeAppTestFixture.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package app.Test.Fixture
+	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+	 */
+
+	/**
+	 * Cette classe sert de classe parente à toutes les fixtures de app, en tous
+	 * cas pour les tables qui contiennent des types enum en PostgreSQL.
+	 *
+	 * @package app.Test.Fixture
+	 */
+	abstract class CakeAppTestFixture extends CakeTestFixture
 	{
 		public $masterDb = null;
 		public $masterDbPrefix = null;
@@ -8,6 +23,12 @@
 		public $_pgsqlEnumTypes = false;
 		public $_masterTableTypes = array();
 
+		/**
+		 * Retourne le nom du driver utilisé (postgres, mysql, ...)
+		 *
+		 * @param object $ds
+		 * @return string
+		 */
 		protected function _driver( $ds ) {
 			if( isset( $ds->config['datasource'] ) ) {
 				$driver = $ds->config['datasource'];
@@ -20,11 +41,12 @@
 		}
 
 		/**
-		* À partir du nom du type, on vérifie s'il existe déjà dans le base de test ou non.
-		* S'il existe ou s'il n'a aucunes valeurs la fonction se termine.
-		* S'il n'existe pas, on va rechercher ses valeurs et on le crée.
-		*/
-
+		 * À partir du nom du type, on vérifie s'il existe déjà dans le base de test ou non.
+		 * S'il existe ou s'il n'a aucunes valeurs la fonction se termine.
+		 * S'il n'existe pas, on va rechercher ses valeurs et on le crée.
+		 *
+		 * @param string $typeName
+		 */
 		protected function _createTypeIfNotExists( $typeName ) {
 			// On contourne le cache mémoire du DataSource de CakePHP avec microtime()
 			$sql = "SELECT count(*), '{$this->testDbPrefix}{$typeName}".microtime()."' FROM pg_catalog.pg_type where typname = '{$this->testDbPrefix}{$typeName}';";
@@ -48,9 +70,11 @@
 		}
 
 		/**
-		* Met à null la valeur par défaut du champ passé en paramètre et le cast avec ce type.
-		*/
-
+		 * Met à null la valeur par défaut du champ passé en paramètre et le cast avec ce type.
+		 *
+		 * @param string $typeName
+		 * @param string $columnName
+		 */
 		protected function _alterColumns( $typeName, $columnName ) {
 			$queries = array();
 
@@ -77,9 +101,10 @@
 		}
 
 		/**
-		* Si la table est al dernière de la base à utiliser le type passé en paramètre, on supprime le type
-		*/
-
+		 * Si la table est al dernière de la base à utiliser le type passé en paramètre, on supprime le type
+		 *
+		 * @param string $typeName
+		 */
 		protected function _dropTypeIfLastTable( $typeName ) {
 			$sql = "SELECT COUNT( DISTINCT(table_name) ) FROM information_schema.columns WHERE data_type = 'USER-DEFINED' AND udt_name = '{$this->testDbPrefix}{$typeName}' AND table_name <> '{$this->testDbPrefix}{$this->table}';";
 			$nbTableHaveType = $this->testDb->query( $sql );
@@ -94,10 +119,12 @@
 		}
 
 		/**
-		* Retourne les types ainsi que les champs pour une table particulière. + valeurs par défaut
-		* ex. pour la table users: array( 'type_no' => array( 'isgestionnaire', 'sensibilite' ) )
-		*/
-
+		 * Retourne les types ainsi que les champs pour une table particulière. + valeurs par défaut
+		 * ex. pour la table users: array( 'type_no' => array( 'isgestionnaire', 'sensibilite' ) )
+		 *
+		 * @param string $tableName
+		 * @return array
+		 */
 		protected function _masterTableTypes( $tableName ) {
 			$results = $this->masterDb->query( "SELECT column_name, udt_name FROM information_schema.columns WHERE table_name = '{$this->masterDbPrefix}{$this->table}' AND data_type = 'USER-DEFINED';" );
 
@@ -111,79 +138,85 @@
 			return $return;
 		}
 
-
 		/**
-		* Création des champs "Enumerable" pour le modèle User
-		*
-		* @see http://www.tig12.net/downloads/apidocs/cakephp/cake/tests/lib/CakeTestFixture.class.html
-		*/
-
+		 * Création de la table.
+		 *
+		 * @see http://www.tig12.net/downloads/apidocs/cakephp/cake/tests/lib/CakeTestFixture.class.html
+		 *
+		 * @param object $db
+		 */
 		public function create( $db ) {
 			//$this->log( "CREATE TABLE {$this->table};", LOG_DEBUG);
 			$return = parent::create( $db );
-			$this->_initEnum( $db );
-			return $return;
-		}
 
-		/**
-		* Initialisation des enums
-		*/
-		protected function _initEnum( $db ) {
 			if( $this->_driver( $db ) == 'postgres' ) {
-				////$this->log( "----- _initEnum {$this->table}", LOG_DEBUG );
 				$this->testDb = $db;
 				$this->testDbPrefix = $db->config['prefix'];
 
 				$this->masterDb = ConnectionManager::getDataSource( 'default' );
 				$this->masterDbPrefix = $this->masterDb->config['prefix'];
 
-				$masterTableTypes = $this->_masterTableTypes( $this->table );
-				if ( !empty( $masterTableTypes ) ) {
-					foreach( $masterTableTypes as $type => $fields) {
-						$this->_createTypeIfNotExists( $type );
-						foreach( $fields as $field ) {
-							//$this->log("inittype=> '$type'", LOG_DEBUG);
-							$this->_alterColumns( $type, $field );
-						}
+				$this->_initPostgresqlEnums( $db );
+			}
+
+			return $return;
+		}
+
+		/**
+		 * Initialisation des enums
+		 *
+		 * @param object $db
+		 */
+		protected function _initPostgresqlEnums( $db ) {
+			////$this->log( "----- _initPostgresqlEnums {$this->table}", LOG_DEBUG );
+			$masterTableTypes = $this->_masterTableTypes( $this->table );
+			if ( !empty( $masterTableTypes ) ) {
+				foreach( $masterTableTypes as $type => $fields) {
+					$this->_createTypeIfNotExists( $type );
+					foreach( $fields as $field ) {
+						//$this->log("inittype=> '$type'", LOG_DEBUG);
+						$this->_alterColumns( $type, $field );
 					}
 				}
 			}
 		}
 
 		/**
-		*
-		*/
-
+		 * Destruction de la table.
+		 *
+		 * @param object $db
+		 */
 		public function drop( $db ) {
 			//$this->log( "DROP TABLE {$this->table};", LOG_DEBUG);
 			$return = parent::drop( $db );
-			$this->_dropEnum( $db );
-			return $return;
-		}
 
-		/**
-		* Suppression des enums
-		*/
-
-		protected function _dropEnum( $db ) {
 			if( $this->_driver( $db ) == 'postgres' ) {
-				////$this->log( "----- _dropEnum {$this->table}", LOG_DEBUG );
 				$this->testDb = $db;
 				$this->testDbPrefix = $db->config['prefix'];
 
 				$this->masterDb = ConnectionManager::getDataSource( 'default' );
 				$this->masterDbPrefix = $this->masterDb->config['prefix'];
 
-				$masterTableTypes = $this->_masterTableTypes( $this->table );
-				if ( !empty( $masterTableTypes ) ) {
-					foreach( $masterTableTypes as $type => $fields) {
-						//$this->log("droptype=> '$type'", LOG_DEBUG);
-						$this->_dropTypeIfLastTable( $type );
-					}
+				$this->_dropPostgresqlEnums( $db );
+			}
+
+			return $return;
+		}
+
+		/**
+		 * Suppression des enums
+		 *
+		 * @param object $db
+		 */
+		protected function _dropPostgresqlEnums( $db ) {
+			$masterTableTypes = $this->_masterTableTypes( $this->table );
+			if ( !empty( $masterTableTypes ) ) {
+				foreach( $masterTableTypes as $type => $fields) {
+					//$this->log("droptype=> '$type'", LOG_DEBUG);
+					$this->_dropTypeIfLastTable( $type );
 				}
 			}
 		}
-
 	}
 
 ?>
