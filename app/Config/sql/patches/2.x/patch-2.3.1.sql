cf6371e8c39d86a6af40c65e642ff1727d62929f
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2012-12-03 16:22:27 +0000

    Mise à jour du patch 2.3.1 suite à l'ajout des données DOM + modification du module CER Particulier (CG66)

--- app/Config/sql/patches/2.x/patch-2.3.1.sql
+++ app/Config/sql/patches/2.x/patch-2.3.1.sql
@@ -18,6 +18,39 @@ CREATE UNIQUE INDEX bilansparcours66_nvcontratinsertion_id_idx ON bilansparcours
 
 SELECT public.alter_enumtype ( 'TYPE_PROPOSITIONBILANPARCOURS', ARRAY['audition','parcours','traitement','auditionpe','parcourspe','aucun'] );
 
+-------------------------------------------------------------------------------------------------------------
+-- 20121130: Intégration des données DOM manquantes en base
+-------------------------------------------------------------------------------------------------------------
+
+--Dans les flux Bénéficiaires : ajouter la table aviscgssdompersonnes
+			
+DROP TABLE IF EXISTS aviscgssdompersonnes CASCADE;
+CREATE TABLE aviscgssdompersonnes (
+	id 					SERIAL NOT NULL PRIMARY KEY,
+	personne_id        INTEGER NOT NULL REFERENCES personnes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	jusactdom			CHAR(1) NOT NULL,
+	resujusactdom		CHAR(1)
+);
+COMMENT ON TABLE aviscgssdompersonnes IS 'Regroupement des décisions de la CGSS (Caisse Générale Sécurité Sociale) liées à la personne (Mayotte)';
+
+DROP INDEX IF EXISTS aviscgssdompersonnes_personne_id_idx;
+CREATE UNIQUE INDEX aviscgssdompersonnes_personne_id_idx ON aviscgssdompersonnes( personne_id );
+
+ALTER TABLE aviscgssdompersonnes ADD CONSTRAINT aviscgssdompersonnes_jusactdom_in_list_chk CHECK ( cakephp_validate_in_list( jusactdom, ARRAY['A', 'D', 'F', 'J', 'M'] ) );
+ALTER TABLE aviscgssdompersonnes ADD CONSTRAINT aviscgssdompersonnes_resujusactdom_in_list_chk CHECK ( cakephp_validate_in_list( resujusactdom, ARRAY['N', 'T'] ) );
+
+
+--- Ajouter les 5 champs suivants à la table detailsdroitsrsa
+SELECT add_missing_table_field('public', 'detailsdroitsrsa', 'surfagridom', 'NUMERIC(5,2)' );
+SELECT add_missing_table_field('public', 'detailsdroitsrsa', 'ddsurfagridom', 'DATE' );
+SELECT add_missing_table_field('public', 'detailsdroitsrsa', 'surfagridompla', 'NUMERIC(5,2)' );
+SELECT add_missing_table_field('public', 'detailsdroitsrsa', 'nbtotaidefamsurfdom', 'INTEGER' );
+SELECT add_missing_table_field('public', 'detailsdroitsrsa', 'nbtotpersmajosurfdom', 'INTEGER' );
+
+--------------------------------------------------------------------------------
+-- Ajout d'une valeur finale pour la déicison du CER Particulier CG66
+--------------------------------------------------------------------------------
+SELECT add_missing_table_field( 'public', 'proposdecisionscers66', 'decisionfinale', 'TYPE_NO' );
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
