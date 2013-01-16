53b27d0346dc9165b783dc8f6421af3e3f8f0ffa
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-01-16 13:28:01 +0000

    Correction du bug #6235 (courrier orientation) : la fonction d'impression() pointe désormais sur un getdefaultpdf du modèle Transfertpdv93.php et imprime un courrier non fourni pour le moment par le CG93

--- app/Controller/Cohortestransfertspdvs93Controller.php
+++ app/Controller/Cohortestransfertspdvs93Controller.php
@@ -255,9 +255,11 @@
 		 * @return void
 		 */
 		public function impression( $id = null ) {
-			$pdf = $this->Transfertpdv93->VxOrientstruct->Personne->Orientstruct->getStoredPdf( $id, 'date_impression' );
-			$pdf = ( isset( $pdf['Pdf']['document'] ) ? $pdf['Pdf']['document'] : null );
+// 			$pdf = $this->Transfertpdv93->VxOrientstruct->Personne->Orientstruct->getStoredPdf( $id, 'date_impression' );
+// 			$pdf = ( isset( $pdf['Pdf']['document'] ) ? $pdf['Pdf']['document'] : null );
 
+			$pdf = $this->Transfertpdv93->getDefaultPdf( $id, $this->Session->read( 'Auth.User.id' ) );
+			
 			if( !empty( $pdf ) ) {
 				$this->Gedooo->sendPdfContentToClient( $pdf, sprintf( 'transfertspdvs93_%d-%s.pdf', $id, date( 'Y-m-d' ) ) );
 			}
