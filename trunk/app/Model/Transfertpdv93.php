4920b5f98baada4568da2739cceb9b4501b15d3a
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-01-16 13:28:01 +0000

    Correction du bug #6235 (courrier orientation) : la fonction d'impression() pointe désormais sur un getdefaultpdf du modèle Transfertpdv93.php et imprime un courrier non fourni pour le moment par le CG93

--- trunk/app/Model/Transfertpdv93.php
+++ trunk/app/Model/Transfertpdv93.php
@@ -31,6 +31,7 @@
 				)
 			),
 			'Validation.Autovalidate',
+			'Gedooo.Gedooo'
 		);
 
 		/**
@@ -85,5 +86,120 @@
 				'counterCache' => null
 			),
 		);
+		
+		/**
+		 * Retourne le chemin relatif du modèle de document à utiliser pour
+		 * l'enregistrement du PDF.
+		 *
+		 * @param array $data
+		 * @return string
+		 */
+		public function modeleOdt( $data ) {
+			return "Transfertpdv93/transfert.odt";
+		}
+
+		/**
+		 * Récupère les données pour le PDF.
+		 *
+		 * @param integer $nvorientstruct_id
+		 * @param integer $user_id
+		 * @return array
+		 */
+		public function getDataForPdf( $nvorientstruct_id, $user_id ) {
+
+			$joins = array(
+				$this->join( 'User', array( 'type' => 'INNER' ) ),
+				$this->join( 'NvOrientstruct', array( 'type' => 'INNER' ) ),
+				$this->join( 'VxOrientstruct', array( 'type' => 'INNER' ) ),
+				$this->NvOrientstruct->join( 'Personne', array( 'type' => 'INNER' )),
+				array_words_replace(
+					$this->NvOrientstruct->join( 'Structurereferente', array( 'type' => 'INNER' )),
+					array(
+						'Structurereferente' => 'NvStructurereferente'
+					)
+				),
+				array_words_replace(
+					$this->VxOrientstruct->join( 'Structurereferente', array( 'type' => 'INNER' )),
+					array(
+						'Structurereferente' => 'VxStructurereferente'
+					)
+				),
+				$this->NvOrientstruct->Personne->join( 'Foyer', array( 'type' => 'INNER' )),
+				$this->NvOrientstruct->Personne->join( 'Prestation', array( 'type' => 'LEFT OUTER'  )),
+				$this->NvOrientstruct->Personne->Foyer->join( 'Adressefoyer', array( 'type' => 'LEFT OUTER' ) ),
+				$this->NvOrientstruct->Personne->Foyer->Adressefoyer->join( 'Adresse', array( 'type' => 'LEFT OUTER' ) ),
+				$this->NvOrientstruct->Personne->Foyer->join( 'Dossier', array( 'type' => 'INNER' ) )
+			);
+
+			$queryData = array(
+				'fields' => array_merge(
+					$this->fields(),
+					$this->User->fields(),
+					$this->NvOrientstruct->fields(),
+					$this->VxOrientstruct->fields(),
+					array_words_replace(
+						$this->NvOrientstruct->Structurereferente->fields(),
+						array(
+							'Structurereferente' => 'NvStructurereferente'
+						)
+					),
+					array_words_replace(
+						$this->VxOrientstruct->Structurereferente->fields(),
+						array(
+							'Structurereferente' => 'VxStructurereferente'
+						)
+					),
+					$this->NvOrientstruct->Personne->fields(),
+					$this->NvOrientstruct->Personne->Prestation->fields(),
+					$this->NvOrientstruct->Personne->Foyer->fields(),
+					$this->NvOrientstruct->Personne->Foyer->Adressefoyer->Adresse->fields(),
+					$this->NvOrientstruct->Personne->Foyer->Dossier->fields()
+				),
+				'joins' => $joins,
+				'conditions' => array(
+					'Transfertpdv93.nv_orientstruct_id' => $nvorientstruct_id,
+					array(
+						'OR' => array(
+							'Adressefoyer.id IS NULL',
+							'Adressefoyer.id IN ( '.$this->NvOrientstruct->Personne->Foyer->Adressefoyer->sqDerniereRgadr01( 'Foyer.id' ).' )'
+						)
+					)
+				),
+				'contain' => false
+			);
+			
+			$data = $this->find( 'first', $queryData );
+// debug($data);
+// die();
+			return $data;
+		}
+		
+		/**
+		 * Retourne le PDF par défaut, stocké, ou généré par les appels aux méthodes getDataForPdf, modeleOdt et
+		 * à la méthode ged du behavior Gedooo et le stocke,
+		 *
+		 * @param integer $id Id du Trasnfert réalisé
+		 * @param integer $user_id Id de l'utilisateur connecté
+		 * @return string
+		 */
+		public function getDefaultPdf( $nvorientstruct_id, $user_id ) {
+			$data = $this->getDataForPdf( $nvorientstruct_id, $user_id );
+			$modeleodt = $this->modeleOdt( $data );
+
+			$Option = ClassRegistry::init( 'Option' );
+			$options =  Set::merge(
+				array(
+					'Personne' => array(
+						'qual' => $Option->qual()
+					),
+					'Adresse' => array(
+						'typevoie' => $Option->typevoie()
+					)
+				)
+			);
+// debug($data);
+// die();
+			return $this->ged( $data, $modeleodt, false, $options );
+		}
 	}
 ?>
