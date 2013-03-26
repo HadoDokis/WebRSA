87ad6587869a17e88492de24902fa7ef764dfcb1
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-03-26 17:01:24 +0000

    Modification du patch 2.4.3

--- trunk/app/Config/sql/patches/2.x/patch-2.4.3.sql
+++ trunk/app/Config/sql/patches/2.x/patch-2.4.3.sql
@@ -28,13 +28,14 @@ ALTER TABLE decisionsdossierspcgs66 ALTER COLUMN etatdossierpcg TYPE VARCHAR(30)
 DROP TYPE IF EXISTS TYPE_ETATDOSSIERPCG CASCADE;
 
 SELECT alter_table_drop_constraint_if_exists( 'public', 'dossierspcgs66', 'dossierspcgs66_etatdossierpcg_in_list_chk' );
-ALTER TABLE dossierspcgs66 ADD CONSTRAINT dossierspcgs66_etatdossierpcg_in_list_chk CHECK ( cakephp_validate_in_list( etatdossierpcg, ARRAY['attaffect','attinstr','instrencours','attavistech','attval','dossiertraite','decisionvalid','decisionnonvalid','decisionnonvalidretouravis','decisionvalidretouravis','attpj','transmisop','atttransmisop','annule'] ) );
+ALTER TABLE dossierspcgs66 ADD CONSTRAINT dossierspcgs66_etatdossierpcg_in_list_chk CHECK ( cakephp_validate_in_list( etatdossierpcg, ARRAY['attaffect','attinstr','instrencours','attavistech','attval','dossiertraite','decisionvalid','decisionnonvalid','decisionnonvalidretouravis','decisionvalidretouravis','transmisop','atttransmisop','annule'] ) );
 
 SELECT alter_table_drop_constraint_if_exists( 'public', 'decisionsdossierspcgs66', 'decisionsdossierspcgs66_etatdossierpcg_in_list_chk' );
-ALTER TABLE decisionsdossierspcgs66 ADD CONSTRAINT decisionsdossierspcgs66_etatdossierpcg_in_list_chk CHECK ( cakephp_validate_in_list( etatdossierpcg, ARRAY['attaffect','attinstr','instrencours','attavistech','attval','dossiertraite','decisionvalid','decisionnonvalid','decisionnonvalidretouravis','decisionvalidretouravis','attpj','transmisop','atttransmisop','annule'] ) );
+ALTER TABLE decisionsdossierspcgs66 ADD CONSTRAINT decisionsdossierspcgs66_etatdossierpcg_in_list_chk CHECK ( cakephp_validate_in_list( etatdossierpcg, ARRAY['attaffect','attinstr','instrencours','attavistech','attval','dossiertraite','decisionvalid','decisionnonvalid','decisionnonvalidretouravis','decisionvalidretouravis','transmisop','atttransmisop','annule'] ) );
 
 
 SELECT add_missing_table_field( 'public', 'actionscandidats_personnes', 'poursuitesuivicg', 'CHAR(1)' );
+SELECT alter_table_drop_constraint_if_exists( 'public', 'actionscandidats_personnes', 'actionscandidats_personnes_poursuitesuivicg_in_list_chk' );
 ALTER TABLE actionscandidats_personnes ADD CONSTRAINT actionscandidats_personnes_poursuitesuivicg_in_list_chk CHECK ( cakephp_validate_in_list( poursuitesuivicg, ARRAY['0','1'] ) );
 
 -- *****************************************************************************
