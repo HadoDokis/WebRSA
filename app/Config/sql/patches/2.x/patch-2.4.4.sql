706000b8d3e8126726fde0cdfcff261f86e080f1
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-05-07 08:59:17 +0000

    Suppression des champs obligatoires sur l'employeur dans la table cuis

--- app/Config/sql/patches/2.x/patch-2.4.4.sql
+++ app/Config/sql/patches/2.x/patch-2.4.4.sql
@@ -19,6 +19,12 @@ ALTER TABLE expsproscers93 ALTER COLUMN nbduree TYPE FLOAT;
 ALTER TABLE cers93 DROP CONSTRAINT cers93_pointparcours_in_list_chk;
 ALTER TABLE cers93 ADD CONSTRAINT cers93_pointparcours_in_list_chk CHECK ( cakephp_validate_in_list( pointparcours, ARRAY['aladate','alafin','encours'] ) );
 
+
+ALTER TABLE cuis ALTER COLUMN typevoieemployeur DROP NOT NULL;
+ALTER TABLE cuis ALTER COLUMN nomvoieemployeur DROP NOT NULL;
+ALTER TABLE cuis ALTER COLUMN codepostalemployeur DROP NOT NULL;
+ALTER TABLE cuis ALTER COLUMN villeemployeur DROP NOT NULL;
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
