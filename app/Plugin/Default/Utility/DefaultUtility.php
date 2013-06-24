5d12e0db4ccc9e136d8289c86ace95e491dad5da
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-06-24 15:21:50 +0000

    Améliorations du plugin Default.

--- app/Plugin/Default/Utility/DefaultUtility.php
+++ app/Plugin/Default/Utility/DefaultUtility.php
@@ -8,6 +8,7 @@
 	 * @subpackage Utility
 	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
 	 */
+	App::uses('Router', 'Routing');
 
 	/**
 	 * La classe DefaultUtility ...
@@ -72,6 +73,132 @@
 		 * @return array
 		 */
 		public static function linkParams( $path, array $htmlAttributes, array $data = array() ) {
+ 			if( !empty( $data ) ) { // FIXME: il faudrait le faire après, mais ne pas se tromper avec les #content
+				$url = self::toArray( self::evaluate( $data, $path ) );;
+			}
+			else {
+				$url = self::toArray( $path );
+			}
+
+			$controller = Hash::get( $url, 'controller' );
+			$action = Hash::get( $url, 'action' );
+			$plugin = Hash::get( $url, 'plugin' );
+			$prefix = Hash::get( $url, 'prefix' );
+
+			// -----------------------------------------------------------------
+
+			$domain = ( isset( $htmlAttributes['domain'] ) ? $htmlAttributes['domain'] : Inflector::underscore( $controller ) );
+
+			$msgid = '/'
+				.implode(
+					'.',
+					Hash::filter(
+						array(
+							Inflector::camelize( $plugin ),
+							Inflector::camelize( $controller )
+						)
+					)
+				)
+				.'/'.( !empty( $prefix ) ? "{$prefix}_" : null ).$action;
+
+			if( isset( $htmlAttributes['title'] ) && $htmlAttributes['title'] === true ) {
+				$htmlAttributes['title'] = __d( $domain, $path );
+			}
+			if( isset( $htmlAttributes['confirm'] ) && $htmlAttributes['confirm'] === true ) {
+				$htmlAttributes['confirm'] = __d( $domain, "{$path} ?" );
+			}
+
+			$htmlAttributes = self::evaluate( $data, $htmlAttributes );
+
+			return array(
+				__d( $domain, $msgid ),
+				$url,
+				$htmlAttributes
+			);
+		}
+
+		/**
+		 * Retourne une URL "canonique" à partir d'une URL sous forme de string ou
+		 * d'array.
+		 *
+		 * Exemples d'URLs "canoniques":
+		 * <pre>
+		 * /Users/index
+		 * /Test.Users/admin_index/666/Saerch__active:1
+		 * </pre>
+		 *
+		 * @param mixed $url
+		 * @return string
+		 */
+		public function toString( $url ) {
+			if( !is_array( $url ) ) {
+				$parsed = Router::parse( $url );
+			}
+			else {
+				if( !isset( $this->request ) ) {
+					$request = new CakeRequest();
+				}
+				else {
+					$request = $this->request;
+				}
+
+				$parsed = $url;
+				$parsed['plugin'] = isset( $parsed['plugin'] ) ? $parsed['plugin'] : $request->params['plugin'];
+				$parsed['controller'] = isset( $parsed['controller'] ) ? $parsed['controller'] : $request->params['controller'];
+				if( isset( $request->params['prefix'] ) ) {
+					$parsed['prefix'] = isset( $parsed['prefix'] ) ? $parsed['prefix'] : $request->params['prefix'];
+				}
+				$parsed['action'] = isset( $parsed['action'] ) ? $parsed['action'] : $request->params['action'];
+
+				// TODO: compléter les autres paramètres si besoin
+				$named = array();
+				$pass = array();
+				foreach( $parsed as $key => $value ) {
+					if( !is_string( $key ) || !in_array( $key, array( 'controller', 'action', 'prefix', 'plugin' ) ) ) {
+						if( is_integer( $key ) ) {
+							$pass[] = $value;
+							unset( $parsed[$key] );
+						}
+						else if( !isset( $parsed['prefix'] ) || $parsed['prefix'] != $key ) {
+							$named[$key] = $value;
+							unset( $parsed[$key] );
+						}
+					}
+				}
+				$parsed['named'] = $named;
+				$parsed['pass'] = $pass;
+			}
+
+			if( empty( $parsed ) ) {
+				return $url;
+			}
+
+			$parsed['controller'] = Inflector::camelize( $parsed['controller'] );
+			if( !is_null( $parsed['plugin'] ) ) {
+				$parsed['controller'] = Inflector::camelize( $parsed['plugin'] ).'.'.$parsed['controller'];
+			}
+			if( isset( $parsed['prefix'] ) && !empty( $parsed['prefix'] ) && !empty( $parsed[$parsed['prefix']] ) ) {
+				$parsed['action'] = $parsed['prefix'].'_'.$parsed['action'];
+			}
+
+			$return = "/{$parsed['controller']}/{$parsed['action']}";
+
+			if( !empty( $parsed['pass'] ) ) {
+				$return .= '/'.implode( '/', $parsed['pass'] );
+			}
+
+			if( !empty( $parsed['named'] ) ) {
+				$return .= '/'.str_replace( '=', ':', http_build_query( $parsed['named'], null, '/' ) );
+			}
+
+			return $return;
+		}
+
+		/**
+		 *
+		 * @param string $url
+		 */
+		public function toArray( $path ) {
 			$tokens = explode( '/', $path );
 			$plugin = null;
 			if( strpos( $tokens[1], '.' ) !== false ) {
@@ -102,46 +229,20 @@
 				}
 			}
 
-			if( !empty( $data ) ) {
-				$url = self::evaluate( $data, $url );
-			}
-
-			// Traitement d'un hash dans les valeurs, ex: array( 0 => '6#content' ) => array( 0 => '6', #' => 'content' )
 			foreach( $url as $key => $value ) {
-				if( ( strpos( $value, '#' ) !== false ) && preg_match( '/^([^#]*)#(.*)$/', $value, $matches ) ) {
+				// CakePHP named parameters
+				if( is_numeric( $key ) && ( strpos( $value, ':' ) !== false ) && preg_match( '/^([^:]*):(.*)$/', $value, $matches ) ) {
+					unset( $url[$key] );
+					$url[$matches[1]] = $matches[2];
+				}
+				// Traitement d'un hash dans les valeurs, ex: array( 0 => '6#content' ) => array( 0 => '6', #' => 'content' )
+				if( ( strpos( $value, '#' ) !== false ) && preg_match( '/^([^#]*)#([^#]*)$/', $value, $matches ) ) {
 					$url[$key] = $matches[1];
 					$url['#'] = $matches[2];
 				}
 			}
 
-			$domain = ( isset( $htmlAttributes['domain'] ) ? $htmlAttributes['domain'] : Inflector::underscore( $controller ) );
-
-			$msgid = '/'
-				.implode(
-					'.',
-					Hash::filter(
-						array(
-							Inflector::camelize( $plugin ),
-							Inflector::camelize( $controller )
-						)
-					)
-				)
-				."/{$action}";
-
-			if( isset( $htmlAttributes['title'] ) && $htmlAttributes['title'] === true ) {
-				$htmlAttributes['title'] = __d( $domain, $path );
-			}
-			if( isset( $htmlAttributes['confirm'] ) && $htmlAttributes['confirm'] === true ) {
-				$htmlAttributes['confirm'] = __d( $domain, "{$path} ?" );
-			}
-
-			$htmlAttributes = self::evaluate( $data, $htmlAttributes );
-
-			return array(
-				__d( $domain, $msgid ),
-				$url,
-				$htmlAttributes
-			);
+			return $url;
 		}
 	}
 ?>
