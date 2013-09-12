23e3bed987fc310cfc03f55d358cbd7e33e68ab3
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-09-12 22:08:45 +0000

    Amélioration du plugin Search: ajout de la classe SearchFormHelper et des tests unitaires associés.

---
+++ app/Plugin/Search/Test/Case/View/Helper/SearchFormHelperTest.php
@@ -0,0 +1,91 @@
+<?php
+	/**
+	 * Code source de la classe SearchFormHelperTest.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package Search
+	 * @subpackage Test.Case.View.Helper
+	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+	 */
+	App::uses( 'Controller', 'Controller' );
+	App::uses( 'View', 'View' );
+	App::uses( 'AppHelper', 'View/Helper' );
+	App::uses( 'SearchFormHelper', 'Search.View/Helper' );
+
+	/**
+	 * La classe SearchFormHelperTest ...
+	 *
+	 * @package Search
+	 * @subpackage Test.Case.View.Helper
+	 */
+	class SearchFormHelperTest extends CakeTestCase
+	{
+		/**
+		 * Le contrôleur utilisé pour les tests.
+		 *
+		 * @var Controller
+		 */
+		public $Controller = null;
+
+		/**
+		 * Le contrôleur utilisé pour les tests.
+		 *
+		 * @var View
+		 */
+		public $View = null;
+
+		/**
+		 * Le helper à tester.
+		 *
+		 * @var SearchForm
+		 */
+		public $SearchForm = null;
+
+		/**
+		 * Préparation du test.
+		 */
+		public function setUp() {
+			parent::setUp();
+			$Request = new CakeRequest();
+			$this->Controller = new Controller( $Request );
+			$this->View = new View( $this->Controller );
+			$this->SearchForm = new SearchFormHelper( $this->View );
+		}
+
+		/**
+		 * Nettoyage postérieur au test.
+		 */
+		public function tearDown() {
+			unset( $this->Controller, $this->View, $this->SearchForm );
+			parent::tearDown();
+		}
+
+		/**
+		 * Test de la méthode SearchFormHelper::dependantCheckboxes()
+		 */
+		public function testDependantCheckboxes() {
+			$this->Controller->request->addParams(
+				array(
+					'controller' => 'users',
+					'action' => 'index',
+					'pass' => array( ),
+					'named' => array( )
+				)
+			);
+			$options = array( '2' => 'Ouvert', '6' => 'Clos' );
+
+			$result = $this->SearchForm->dependantCheckboxes( 'Search.Dossier.etatdosrsa', $options );
+			$expected = '<div class="input checkbox"><input type="hidden" name="data[Search][Dossier][etatdosrsa_choice]" id="SearchDossierEtatdosrsaChoice_" value="0"/><input type="checkbox" name="data[Search][Dossier][etatdosrsa_choice]"  value="1" id="SearchDossierEtatdosrsaChoice"/><label for="SearchDossierEtatdosrsaChoice">Search.Dossier.etatdosrsa_choice</label></div><fieldset id="SearchDossierEtatdosrsa"><legend>Search.Dossier.etatdosrsa</legend><div class="input select"><input type="hidden" name="data[Search][Dossier][etatdosrsa]" value="" id="SearchDossierEtatdosrsa"/>
+
+<div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="2" id="SearchDossierEtatdosrsa2" /><label for="SearchDossierEtatdosrsa2">Ouvert</label></div>
+<div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="6" id="SearchDossierEtatdosrsa6" /><label for="SearchDossierEtatdosrsa6">Clos</label></div>
+</div></fieldset><script type="text/javascript">
+//<![CDATA[
+document.observe( \'dom:loaded\', function() { observeDisableFieldsetOnCheckbox( \'SearchDossierEtatdosrsaChoice\', $( \'SearchDossierEtatdosrsa\' ), false ); } );
+//]]>
+</script>';
+			$this->assertEquals( $result, $expected, var_export( $result, true ) );
+		}
+	}
+?>
---
+++ app/Plugin/Search/View/Helper/SearchFormHelper.php
@@ -0,0 +1,95 @@
+<?php
+	/**
+	 * Code source de la classe SearchFormHelper.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package Search
+	 * @subpackage View.Helper
+	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+	 */
+
+	/**
+	 * La classe SearchFormHelper ...
+	 *
+	 * @package Search
+	 * @subpackage View.Helper
+	 */
+	class SearchFormHelper extends AppHelper
+	{
+		/**
+		 * Helpers utilisés.
+		 *
+		 * @var array
+		 */
+		public $helpers = array( 'Form', 'Html' );
+
+		/**
+		 * Retourne le code javascript permettant d'activer ou de désactiver le fieldset contentant les cases à
+		 * cocher (états du dossier, natures de la prestation, ...) suivant la valeur de la case à cocher
+		 * "parente" ("choice").
+		 *
+		 * @param string $observeId
+		 * @param string $updateId
+		 * @param boolean $up
+		 * @return string
+		 */
+		protected function _constuctObserve( $observeId, $updateId, $up = true ) {
+			$up = $up ? ".up( 'fieldset' )" : '';
+			$out = "document.observe( 'dom:loaded', function() { observeDisableFieldsetOnCheckbox( '{$observeId}', $( '{$updateId}' ){$up}, false ); } );";
+
+			return $this->Html->scriptBlock( $out );
+		}
+
+		/**
+		 * Méthode générique permettant de retourner un ensemble de cases à cocher au sein d'un
+		 * fieldset, activées ou désactivées par une autre case à cocher située au-dessus du fieldset.
+		 *
+		 * Les traductions - "{$path}_choice" pour la case à cocher d'activation/désactivation et
+		 * $path pour le fieldset sont faites dans le fichier de traduction correspondant au nom
+		 * du contrôleur.
+		 *
+		 * Remplacements possibles:
+		 * //			echo $this->Search->etatdosrsa($etatdosrsa);
+		 * echo $this->SearchForm->dependantCheckboxes( 'Situationdossierrsa.etatdosrsa', $etatdosrsa );
+		 * @see SearchHelper
+		 *
+		 * @param $path string Le chemin au sens formulaire CakePHP
+		 * @param $options array Les différentes valeurs pour les cases à cocher.
+		 * @return string
+		 */
+		public function dependantCheckboxes( $path, $options ) {
+			$domain = Inflector::underscore( $this->request->params['controller'] );
+			$fieldsetId = $this->domId( $path );
+			$choicePath = "{$path}_choice";
+
+			$input = $this->Form->input(
+				$choicePath,
+				array(
+					'label' => __d( $domain, $choicePath ),
+					'type' => 'checkbox'
+				)
+			);
+
+			$input .= $this->Html->tag(
+				'fieldset',
+				$this->Html->tag( 'legend', __d( $domain, $path ) )
+				.$this->Form->input(
+					$path,
+					array(
+						'label' => false,
+						'type' => 'select',
+						'multiple' => 'checkbox',
+						'options' => $options,
+						'fieldset' => false
+					)
+				),
+				array( 'id' => $fieldsetId )
+			);
+
+			$script = $this->_constuctObserve( $this->domId( $choicePath ), $fieldsetId, false );
+
+			return $input.$script;
+		}
+	}
+?>
