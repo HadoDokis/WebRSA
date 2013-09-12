2b9369df131ba4cbb28406f19c811b43c0f75f70
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-09-12 21:49:29 +0000

    Amélioration du template de tests unitaires de helpers.

--- trunk/app/Vendor/Netbeans/Templates/Test/CakePHP Helper Test Case.php
+++ trunk/app/Vendor/Netbeans/Templates/Test/CakePHP Helper Test Case.php
@@ -10,6 +10,7 @@
 	 * @package app.Test.Case.View.Helper
 	 * @license ${license}
 	 */
+	App::uses( 'Controller', 'Controller' );
 	App::uses( 'View', 'View' );
 	App::uses( 'AppHelper', 'View/Helper' );
 	App::uses( '${class_name(name)?replace("Test", "", "r")}', 'View/Helper' );
@@ -22,20 +23,41 @@
 	class ${name} extends CakeTestCase
 	{
 		/**
-		 * Fixtures utilisés.
+		 * Fixtures utilisées pour les tests.
 		 *
 		 * @var array
 		 */
-		public $fixtures = array(
-		);
+		public $fixtures = array();
+
+		/**
+		 * Le contrôleur utilisé pour les tests.
+		 *
+		 * @var Controller
+		 */
+		public $Controller = null;
+
+		/**
+		 * Le contrôleur utilisé pour les tests.
+		 *
+		 * @var View
+		 */
+		public $View = null;
+
+		/**
+		 * Le helper à tester.
+		 *
+		 * @var ${class_name(name)?replace("HelperTest", "", "r")}
+		 */
+		public $${class_name(name)?replace("HelperTest", "", "r")} = null;
 
 		/**
 		 * Préparation du test.
 		 */
 		public function setUp() {
 			parent::setUp();
-			$controller = null;
-			$this->View = new View( $controller );
+			$Request = new CakeRequest();
+			$this->Controller = new Controller( $Request );
+			$this->View = new View( $this->Controller );
 			$this->${class_name(name)?replace("HelperTest", "", "r")} = new ${class_name(name)?replace("Test", "", "r")}( $this->View );
 		}
 
@@ -44,13 +66,22 @@
 		 */
 		public function tearDown() {
 			parent::tearDown();
-			unset( $this->View, $this->${class_name(name)?replace("HelperTest", "", "r")} );
+			unset( $this->Controller, $this->View, $this->${class_name(name)?replace("HelperTest", "", "r")} );
 		}
 
 		/**
 		 * Test de la méthode ${class_name(name)?replace("Test", "", "r")}::method()
 		 */
 		public function testMethod() {
+			$this->Controller->request->addParams(
+				array(
+					'controller' => 'users',
+					'action' => 'index',
+					'pass' => array( ),
+					'named' => array( )
+				)
+			);
+
 			$this->markTestIncomplete( 'Ce test n\'a pas encoré été implémenté.' );
 		}
 	}
