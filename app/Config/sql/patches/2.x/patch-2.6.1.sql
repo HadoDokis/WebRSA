27decebc001f1e9e818d2ff3301236fbb8ca13b5
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-10-30 13:47:49 +0000

    Ajout de la récupération des formulaires D2 automatisables à partir des données existant en base pour le module FSE (CG 93).

--- app/Config/sql/patches/2.x/patch-2.6.1.sql
+++ app/Config/sql/patches/2.x/patch-2.6.1.sql
@@ -89,7 +89,7 @@ CREATE TABLE questionnairesd2pdvs93 (
     structurereferente_id			INTEGER NOT NULL REFERENCES structuresreferentes(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	situationaccompagnement			VARCHAR(25) NOT NULL,
 	sortieaccompagnementd2pdv93_id	INTEGER DEFAULT NULL REFERENCES sortiesaccompagnementsd2pdvs93(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	chgmentsituationadmin			VARCHAR(25) DEFAULT NULL, -- FIXME
+	chgmentsituationadmin			VARCHAR(25) DEFAULT NULL,
     created							TIMESTAMP WITHOUT TIME ZONE,
     modified						TIMESTAMP WITHOUT TIME ZONE
 );
@@ -139,6 +139,136 @@ ALTER TABLE tableauxsuivispdvs93 ADD CONSTRAINT tableauxsuivispdvs93_name_in_lis
 -- 20131028 : Ajout de la date d'impression de la tacite reconduction (CG66)
 ---------------------------------------------------------------------------------------
 SELECT add_missing_table_field ( 'public', 'contratsinsertion', 'datetacitereconduction', 'DATE' );
+
+--------------------------------------------------------------------------------
+-- 20131030 : Ajout des sorties de l'accompagnement des enregistrements se
+-- trouvant déjà en base pour le module FSE (CG93)
+--------------------------------------------------------------------------------
+
+INSERT INTO questionnairesd2pdvs93 ( personne_id, questionnaired1pdv93_id, structurereferente_id, situationaccompagnement, chgmentsituationadmin, created, modified )
+	SELECT
+			personnes.id AS personne_id,
+			questionnairesd1pdvs93.id AS questionnaired1pdv93_id,
+			rendezvous.structurereferente_id AS structurereferente_id,
+			( CASE
+				WHEN ( adresses.id IS NOT NULL OR transfertspdvs93.id IS NOT NULL ) THEN 'changement_situation'
+				WHEN ( decisionsreorientationseps93.id IS NOT NULL OR decisionsnonorientationsproseps93.id IS NOT NULL ) THEN 'reorientation'
+				WHEN ( decisionssignalementseps93.id IS NOT NULL ) THEN 'abandon'
+				ELSE NULL
+			END ) AS situationaccompagnement,
+			( CASE
+				WHEN ( adresses.id IS NOT NULL ) THEN 'modif_departement'
+				WHEN ( transfertspdvs93.id IS NOT NULL ) THEN 'modif_commune'
+				ELSE NULL
+			END ) AS chgmentsituationadmin,
+			( CASE
+				WHEN ( adresses.id IS NOT NULL ) THEN adressesfoyers.dtemm
+				WHEN ( transfertspdvs93.id IS NOT NULL ) THEN transfertspdvs93.created
+				ELSE commissionseps.dateseance
+			END ) AS created,
+			( CASE
+				WHEN ( adresses.id IS NOT NULL ) THEN adressesfoyers.dtemm
+				WHEN ( transfertspdvs93.id IS NOT NULL ) THEN transfertspdvs93.created
+				ELSE commissionseps.dateseance
+			END ) AS modified
+		FROM questionnairesd1pdvs93
+			INNER JOIN rendezvous ON ( questionnairesd1pdvs93.rendezvous_id = rendezvous.id )
+			INNER JOIN personnes ON ( questionnairesd1pdvs93.personne_id = personnes.id )
+			INNER JOIN foyers ON ( personnes.foyer_id = foyers.id )
+			LEFT OUTER JOIN adressesfoyers ON (
+				adressesfoyers.foyer_id = foyers.id
+				AND adressesfoyers.rgadr = '01'
+				AND adressesfoyers.id IN ( SELECT "dernieresadressesfoyers"."id" FROM adressesfoyers AS "dernieresadressesfoyers" WHERE "dernieresadressesfoyers"."foyer_id" = foyers."id" AND "dernieresadressesfoyers"."rgadr" = '01' ORDER BY "dernieresadressesfoyers"."dtemm" DESC LIMIT 1 )
+				AND adressesfoyers.dtemm > rendezvous.daterdv
+			)
+			LEFT OUTER JOIN adresses ON (
+				adressesfoyers.adresse_id = adresses.id
+				AND adresses.numcomptt NOT LIKE '93%'
+			)
+			LEFT OUTER JOIN dossierseps ON (
+				dossierseps.personne_id = personnes.id
+				AND dossierseps.themeep IN ( 'reorientationseps93', 'nonorientationsproseps93', 'nonrespectssanctionseps93' )
+			)
+			LEFT OUTER JOIN passagescommissionseps ON (
+				passagescommissionseps.dossierep_id = dossierseps.id
+				AND passagescommissionseps.id IN (
+					SELECT dernierspassagescommissionseps.id
+						FROM passagescommissionseps AS dernierspassagescommissionseps
+							INNER JOIN commissionseps AS dernierescommissionseps ON ( dernierescommissionseps.id = dernierspassagescommissionseps.commissionep_id )
+						WHERE dernierspassagescommissionseps.dossierep_id = passagescommissionseps.dossierep_id
+						ORDER BY dernierescommissionseps.dateseance DESC
+						LIMIT 1
+				)
+				AND passagescommissionseps.etatdossierep = 'traite'
+			)
+			-- Décision d'EP réorientation
+			LEFT OUTER JOIN decisionsreorientationseps93 ON (
+				decisionsreorientationseps93.passagecommissionep_id = passagescommissionseps.id
+				AND decisionsreorientationseps93.etape = 'cg'
+				AND decisionsreorientationseps93.decision = 'accepte'
+			)
+			-- Décision d'EP réorientation
+			LEFT OUTER JOIN decisionsnonorientationsproseps93 ON (
+				decisionsnonorientationsproseps93.passagecommissionep_id = passagescommissionseps.id
+				AND decisionsnonorientationsproseps93.etape = 'cg'
+				AND decisionsnonorientationsproseps93.decision = 'reorientation'
+			)
+			-- Décision d'EP sanction
+			LEFT OUTER JOIN decisionssignalementseps93 ON (
+				decisionssignalementseps93.passagecommissionep_id = passagescommissionseps.id
+				AND decisionssignalementseps93.etape = 'cg'
+				AND decisionssignalementseps93.decision IN ( '1reduction', '2suspensiontotale', '2suspensionpartielle' )
+			)
+			-- Décision d'EP sanction
+			LEFT OUTER JOIN commissionseps ON (
+				commissionseps.id = passagescommissionseps.commissionep_id
+			)
+			-- Transfert de PDV ?
+			LEFT OUTER JOIN orientsstructs AS vxorientsstructs ON (
+				vxorientsstructs.personne_id = personnes.id
+				AND vxorientsstructs.statut_orient = 'Orienté'
+			)
+			LEFT OUTER JOIN orientsstructs AS nvorientsstructs ON (
+				nvorientsstructs.personne_id = personnes.id
+				AND nvorientsstructs.statut_orient = 'Orienté'
+			)
+			LEFT OUTER JOIN transfertspdvs93 ON (
+				vxorientsstructs.id = transfertspdvs93.vx_orientstruct_id
+				AND nvorientsstructs.id = transfertspdvs93.nv_orientstruct_id
+			)
+		WHERE
+			-- Possédant un D1 sans D2
+			questionnairesd1pdvs93.id NOT IN (
+				SELECT questionnairesd2pdvs93.questionnaired1pdv93_id
+					FROM questionnairesd2pdvs93
+					WHERE
+						questionnairesd2pdvs93.personne_id = questionnairesd1pdvs93.personne_id
+						AND questionnairesd2pdvs93.structurereferente_id = rendezvous.structurereferente_id
+						AND EXTRACT( 'YEAR' FROM rendezvous.daterdv ) = EXTRACT( 'YEAR' FROM questionnairesd2pdvs93.created )
+			)
+			AND (
+				-- Changement de département après D1
+				(
+					adresses.numcomptt NOT LIKE '93%'
+					AND adressesfoyers.dtemm > rendezvous.daterdv
+				)
+				OR
+				-- Transfert PDV après D1
+				(
+					transfertspdvs93.id IS NOT NULL
+					AND (
+						vxorientsstructs.typeorient_id <> nvorientsstructs.typeorient_id
+						OR vxorientsstructs.structurereferente_id <> nvorientsstructs.structurereferente_id
+					)
+					AND DATE_TRUNC( 'DAY', transfertspdvs93.created ) > rendezvous.daterdv
+				)
+				OR
+				-- Décision d'EP réorientation
+				(
+					DATE_TRUNC( 'DAY', commissionseps.dateseance ) > rendezvous.daterdv
+				)
+			);
+
 -- *****************************************************************************
 COMMIT;
 -- *****************************************************************************
