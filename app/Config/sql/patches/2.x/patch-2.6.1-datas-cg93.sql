32852ca1b1c839f0eca34e94d984d053c16a95c6
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2013-11-07 10:49:13 +0000

    Corrections: pour le tableau de résultats D2, à présent les intitulés de sortie de l'accompagnement devraient être corrects, ajout de la ligne du total des participants, correction des pourcentages, correction des libellés "Abandon" et "Réorientation".

--- app/Config/sql/patches/2.x/patch-2.6.1-datas-cg93.sql
+++ app/Config/sql/patches/2.x/patch-2.6.1-datas-cg93.sql
@@ -15,23 +15,23 @@ BEGIN;
 INSERT INTO sortiesaccompagnementsd2pdvs93 ( name, parent_id, created, modified ) VALUES
 	( 'Sortie "Emploi formation"', NULL, NOW(), NOW() ),
 	( 'Sortie "Autres droits"', NULL, NOW(), NOW() ),
-	-- Motifs de sortie "Autres droits" du formulaire/tableau D2
-	( 'Création d''activité', 2, NOW(), NOW() ),
-	( 'Accès à un emploi temporaire ou saisonnier (< ou = à 6 mois)', 2, NOW(), NOW() ),
-	( 'Accès à un contrat aidé', 2, NOW(), NOW() ),
-	( 'Accès à un emploi durable (plus de 6 mois)', 2, NOW(), NOW() ),
-	( 'Accès à une formation qualifiante rémunérée', 2, NOW(), NOW() ),
-	( 'Accès à une formation certifiée rémunérée', 2, NOW(), NOW() ),
-	( 'Accès à une procédure VAE', 2, NOW(), NOW() ),
-	( 'Retour en formation scolaire (après une rupture)', 2, NOW(), NOW() ),
-	( 'Accès à un emploi salarié SIAE (hors contrat aidé)', 2, NOW(), NOW() ),
-	( 'Maintien ou développement de l''emploi ou de l''activité', 2, NOW(), NOW() ),
 	-- Motifs de sortie "Emploi formation" du formulaire/tableau D2
-	( 'Indemnisation Pôle Emploi', 1, NOW(), NOW() ),
-	( 'Allocation Adulte Handicapé', 1, NOW(), NOW() ),
-	( 'Autre invalidité', 1, NOW(), NOW() ),
-	( 'Retraites', 1, NOW(), NOW() ),
-	( 'Autres', 1, NOW(), NOW() );
+	( 'Création d''activité', 1, NOW(), NOW() ),
+	( 'Accès à un emploi temporaire ou saisonnier (< ou = à 6 mois)', 1, NOW(), NOW() ),
+	( 'Accès à un contrat aidé', 1, NOW(), NOW() ),
+	( 'Accès à un emploi durable (plus de 6 mois)', 1, NOW(), NOW() ),
+	( 'Accès à une formation qualifiante rémunérée', 1, NOW(), NOW() ),
+	( 'Accès à une formation certifiée rémunérée', 1, NOW(), NOW() ),
+	( 'Accès à une procédure VAE', 1, NOW(), NOW() ),
+	( 'Retour en formation scolaire (après une rupture)', 1, NOW(), NOW() ),
+	( 'Accès à un emploi salarié SIAE (hors contrat aidé)', 1, NOW(), NOW() ),
+	( 'Maintien ou développement de l''emploi ou de l''activité', 1, NOW(), NOW() ),
+	-- Motifs de sortie "Autres droits" du formulaire/tableau D2
+	( 'Indemnisation Pôle Emploi', 2, NOW(), NOW() ),
+	( 'Allocation Adulte Handicapé', 2, NOW(), NOW() ),
+	( 'Autre invalidité', 2, NOW(), NOW() ),
+	( 'Retraites', 2, NOW(), NOW() ),
+	( 'Autres', 2, NOW(), NOW() );
 
 -- *****************************************************************************
 COMMIT;
