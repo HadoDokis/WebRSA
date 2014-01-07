d6b9fa19032736f665b393c0baae5fbd19e020d0
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-01-07 17:23:26 +0000

    Ajout du début du patch 2.7.0 avec les fonction Pl/PgSQL permettant de transformer les champs ENUM d'un table en champs VARCHAR avec des contraintes de type cakephp_validate_in_list().

---
+++ app/Config/sql/patches/2.x/patch-2.7.0.sql
@@ -0,0 +1,134 @@
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+-- SET client_min_messages = warning;
+SET client_min_messages = notice;
+SET escape_string_warning = off;
+SET search_path = public, pg_catalog;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+-- *****************************************************************************
+BEGIN;
+-- *****************************************************************************
+
+CREATE OR REPLACE FUNCTION public.enumtype_to_validate_in_list( p_schema TEXT, p_table TEXT, p_field TEXT ) RETURNS VOID AS
+$$
+	DECLARE
+		v_row_field RECORD;
+		v_select_query TEXT;
+		v_count_query TEXT;
+		v_row_count RECORD;
+		v_enum_query TEXT;
+		v_enum_row RECORD;
+		v_enum_value TEXT;
+		v_enum_i INTEGER;
+
+		v_altercolumn_query TEXT;
+		v_droptype_query TEXT;
+		v_inlist_query TEXT;
+		v_inlist_maxlength INTEGER;
+	BEGIN
+		v_select_query := 'SELECT isc.table_schema, isc.table_name, isc.column_name, isc.udt_name
+			FROM information_schema.columns AS isc
+			WHERE
+				isc.data_type = ''USER-DEFINED''
+				AND isc.udt_name IN (
+					SELECT isc2.udt_name
+						FROM information_schema.columns AS isc2
+						WHERE
+							isc2.data_type = ''USER-DEFINED''
+							AND isc2.table_schema = ''' || p_schema || '''
+							AND isc2.table_name = ''' || p_table || '''
+							AND isc2.column_name = ''' || p_field || '''
+			)';
+
+		v_count_query := 'SELECT COUNT(tables.*) AS count FROM ( ' || v_select_query || ' ) AS tables';
+		EXECUTE v_count_query INTO v_row_count;
+
+		-- FIXME: pas d'erreur en-dehors des tests
+		IF v_row_count.count = 0 THEN
+			RAISE  EXCEPTION 'Le champ %.%.% n''est pas de type ENUM', p_schema, p_table, p_field;
+		END IF;
+
+		-- Transformation de la requête pour ne concerner que le champ demandé
+		v_select_query := v_select_query
+			|| 'AND isc.table_schema = ''' || p_schema || '''
+			AND isc.table_name = ''' || p_table || '''
+			AND isc.column_name = ''' || p_field || ''';';
+
+		FOR v_row_field IN EXECUTE v_select_query
+		LOOP
+			-- 1°) Récupération des valeurs
+			v_enum_query := 'SELECT enum_range( null::' || v_row_field.udt_name || ' )::TEXT[] AS enum;';
+			EXECUTE v_enum_query INTO v_enum_row;
+
+			-- Recherche de la longueur maximale du champ
+			v_inlist_maxlength := 0;
+			FOR v_enum_value IN SELECT unnest( v_enum_row.enum )
+			LOOP
+				IF LENGTH( v_enum_value ) > v_inlist_maxlength THEN
+					v_inlist_maxlength := LENGTH( v_enum_value );
+				END IF;
+			END LOOP;
+
+			-- 2°) Transformation de la colonne
+ 			v_altercolumn_query := 'ALTER TABLE ' || p_schema || '.' || p_table || ' ALTER COLUMN ' || p_field || ' TYPE VARCHAR(' || v_inlist_maxlength || ') USING CAST(' || p_field || ' AS VARCHAR(' || v_inlist_maxlength || '));';
+			RAISE NOTICE  '%', v_altercolumn_query;
+			EXECUTE v_altercolumn_query;
+
+			-- 3°) Ajout de la contrainte cakephp_calidate_in_list()
+			v_inlist_query := 'ALTER TABLE ' || p_schema || '.' || p_table || ' ADD CONSTRAINT ' || p_table || '_' || p_field || '_in_list_chk CHECK ( cakephp_validate_in_list( ' || p_field || ', ARRAY[\'' || ARRAY_TO_STRING( v_enum_row.enum, '\', \'' ) || '\'] ) );';
+			RAISE NOTICE  '%', v_inlist_query;
+			EXECUTE v_inlist_query;
+
+			-- 3°) Suppression de l'ENUM si on est le seul à l'utiliser
+			IF v_row_count.count = 1 THEN
+				v_droptype_query := 'DROP TYPE IF EXISTS ' || v_row_field.udt_name || ';';
+				RAISE NOTICE  '%', v_droptype_query;
+				EXECUTE v_droptype_query;
+			END IF;
+		END LOOP;
+	END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+COMMENT ON FUNCTION public.enumtype_to_validate_in_list( p_schema TEXT, p_table TEXT, p_field TEXT ) IS
+	'Permet de transformer un champ de type ENUM en un champ de type VARCHAR, avec ajout d''une contrainte cakephp_validate_in_list() et la suppression du type s''il n''est plus utilisé.';
+
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION public.table_enumtypes_to_validate_in_list( p_schema TEXT, p_table TEXT ) RETURNS VOID AS
+$$
+	DECLARE
+		v_row_field RECORD;
+		v_select_query TEXT;
+		v_function_query TEXT;
+	BEGIN
+		v_select_query := 'SELECT isc.table_schema, isc.table_name, isc.column_name
+			FROM information_schema.columns AS isc
+			WHERE
+				isc.data_type = ''USER-DEFINED''
+				AND isc.table_schema = ''' || p_schema || '''
+				AND isc.table_name = ''' || p_table || ''';';
+
+		FOR v_row_field IN EXECUTE v_select_query
+		LOOP
+			EXECUTE public.enumtype_to_validate_in_list( v_row_field.table_schema, v_row_field.table_name, v_row_field.column_name );
+		END LOOP;
+	END;
+$$
+LANGUAGE plpgsql VOLATILE;
+
+COMMENT ON FUNCTION public.table_enumtypes_to_validate_in_list( p_schema TEXT, p_table TEXT ) IS
+	'Permet de transformer tous les champs de type ENUM d''une table en champs de type VARCHAR, avec ajout d''une contrainte cakephp_validate_in_list() et la suppression du type s''il n''est plus utilisé.';
+
+--------------------------------------------------------------------------------
+
+-- SELECT public.enumtype_to_validate_in_list( 'public', 'cuis', 'secteur' );
+-- SELECT public.table_enumtypes_to_validate_in_list( 'public', 'cuis' );
+-- TODO: 'attavismne'::type_positioncui66, ... -> valeurs par défaut lorsque le type est encore utilisé ailleurs
+
+-- *****************************************************************************
+COMMIT;
+-- *****************************************************************************
