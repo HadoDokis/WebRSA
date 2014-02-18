392ae1fc6c1a6808724111db86f394c44221fa74
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-02-18 13:03:03 +0000

    Ajout des méthodes SearchFormHelper::dateRange() et SearchFormHelper::observeDisableFormOnSubmit() ainsi que des tests unitaires associés.

--- app/Plugin/Search/Test/Case/View/Helper/SearchFormHelperTest.php
+++ app/Plugin/Search/Test/Case/View/Helper/SearchFormHelperTest.php
@@ -43,6 +43,15 @@
 		public $SearchForm = null;
 
 		/**
+		 * Fixtures utilisées.
+		 *
+		 * @var array
+		 */
+		public $fixtures = array(
+			'core.Apple'
+		);
+
+		/**
 		 * Préparation du test.
 		 */
 		public function setUp() {
@@ -87,6 +96,150 @@
 document.observe( \'dom:loaded\', function() { observeDisableFieldsetOnCheckbox( \'SearchDossierEtatdosrsaChoice\', $( \'SearchDossierEtatdosrsaFieldset\' ), false ); } );
 //]]>
 </script>';
+			$this->assertEquals( $result, $expected, var_export( $result, true ) );
+		}
+
+		/**
+		 * Test de la méthode SearchFormHelper::dateRange()
+		 */
+		public function testDateRange() {
+			$result = $this->SearchForm->dateRange( 'Search.Apple.date' );
+
+			// On peut factoriser
+			$thisYear = date( 'Y' );
+
+			$years = range( $thisYear, $thisYear - 120, -1 );
+			foreach( $years as $i => $year ) {
+				$selected = ( ( $year == $thisYear ) ? ' selected="selected"' : '' );
+				$years[$i] = "<option value=\"{$year}\"{$selected}>{$year}</option>";
+			}
+			$yearsFrom = implode( "\n", $years );
+
+			$years = range( $thisYear + 5, $thisYear - 120, -1 );
+			foreach( $years as $i => $year ) {
+				$selected = ( ( $year == $thisYear ) ? ' selected="selected"' : '' );
+				$years[$i] = "<option value=\"{$year}\"{$selected}>{$year}</option>";
+			}
+			$yearsTo = implode( "\n", $years );
+
+			$expected = '<script type="text/javascript">
+//<![CDATA[
+document.observe( \'dom:loaded\', function() { observeDisableFieldsetOnCheckbox( \'SearchAppleDate\', $( \'SearchAppleDate_from_to\' ), false ); } );
+//]]>
+</script><div class="input checkbox"><input type="hidden" name="data[Search][Apple][date]" id="SearchAppleDate_" value="0"/><input type="checkbox" name="data[Search][Apple][date]"  value="1" id="SearchAppleDate"/><label for="SearchAppleDate">Filtrer par apple.date</label></div><fieldset id="SearchAppleDate_from_to"><legend>Apple.date</legend><div class="input date"><label for="SearchAppleDateFromDay">Du (inclus)</label><select name="data[Search][Apple][date_from][day]" id="SearchAppleDateFromDay">
+<option value="01">1</option>
+<option value="02">2</option>
+<option value="03">3</option>
+<option value="04">4</option>
+<option value="05">5</option>
+<option value="06">6</option>
+<option value="07">7</option>
+<option value="08">8</option>
+<option value="09">9</option>
+<option value="10">10</option>
+<option value="11" selected="selected">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>
+</select>-<select name="data[Search][Apple][date_from][month]" id="SearchAppleDateFromMonth">
+<option value="01">janvier</option>
+<option value="02" selected="selected">février</option>
+<option value="03">mars</option>
+<option value="04">avril</option>
+<option value="05">mai</option>
+<option value="06">juin</option>
+<option value="07">juillet</option>
+<option value="08">août</option>
+<option value="09">septembre</option>
+<option value="10">octobre</option>
+<option value="11">novembre</option>
+<option value="12">décembre</option>
+</select>-<select name="data[Search][Apple][date_from][year]" id="SearchAppleDateFromYear">
+'.$yearsFrom.'
+</select></div><div class="input date"><label for="SearchAppleDateToDay">Au (inclus)</label><select name="data[Search][Apple][date_to][day]" id="SearchAppleDateToDay">
+<option value="01">1</option>
+<option value="02">2</option>
+<option value="03">3</option>
+<option value="04">4</option>
+<option value="05">5</option>
+<option value="06">6</option>
+<option value="07">7</option>
+<option value="08">8</option>
+<option value="09">9</option>
+<option value="10">10</option>
+<option value="11">11</option>
+<option value="12">12</option>
+<option value="13">13</option>
+<option value="14">14</option>
+<option value="15">15</option>
+<option value="16">16</option>
+<option value="17">17</option>
+<option value="18" selected="selected">18</option>
+<option value="19">19</option>
+<option value="20">20</option>
+<option value="21">21</option>
+<option value="22">22</option>
+<option value="23">23</option>
+<option value="24">24</option>
+<option value="25">25</option>
+<option value="26">26</option>
+<option value="27">27</option>
+<option value="28">28</option>
+<option value="29">29</option>
+<option value="30">30</option>
+<option value="31">31</option>
+</select>-<select name="data[Search][Apple][date_to][month]" id="SearchAppleDateToMonth">
+<option value="01">janvier</option>
+<option value="02" selected="selected">février</option>
+<option value="03">mars</option>
+<option value="04">avril</option>
+<option value="05">mai</option>
+<option value="06">juin</option>
+<option value="07">juillet</option>
+<option value="08">août</option>
+<option value="09">septembre</option>
+<option value="10">octobre</option>
+<option value="11">novembre</option>
+<option value="12">décembre</option>
+</select>-<select name="data[Search][Apple][date_to][year]" id="SearchAppleDateToYear">
+'.$yearsTo.'
+</select></div></fieldset>';
+			$this->assertEquals( $result, $expected, var_export( $result, true ) );
+		}
+
+		/**
+		 * Test de la méthode SearchForm::observeDisableFormOnSubmit()
+		 */
+		public function testObserveDisableFormOnSubmit() {
+			// Sans message à l'utilisateur
+			$result = $this->SearchForm->observeDisableFormOnSubmit( 'UsersEditForm' );
+			$expected = '<script type=\'text/javascript\'>document.observe( \'dom:loaded\', function() {
+					observeDisableFormOnSubmit( \'UsersEditForm\' );
+				} );</script>';
+			$this->assertEquals( $result, $expected, var_export( $result, true ) );
+
+			// Avec message à l'utilisateur
+			$result = $this->SearchForm->observeDisableFormOnSubmit( 'UsersEditForm', 'Merci de patienter' );
+			$expected = '<script type=\'text/javascript\'>document.observe( \'dom:loaded\', function() {
+					observeDisableFormOnSubmit( \'UsersEditForm\', \'Merci de patienter\' );
+				} );</script>';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
 	}
