a491898fdf75bb6a3ad99f6b7ddb8028ffb25f13
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-02-25 22:30:52 +0000

    Ajout du début de la classe DefaultCsvHelper du plugin Default, ajout du début de l'export CSV des fiches de prescription (CG 93).

--- app/Plugin/Default/View/Helper/DefaultActionHelper.php
+++ app/Plugin/Default/View/Helper/DefaultActionHelper.php
@@ -4,7 +4,8 @@
 	 *
 	 * PHP 5.3
 	 *
-	 * @package app.View.Helper
+	 * @package Default
+	 * @subpackage View.Helper
 	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
 	 */
 	App::uses( 'DefaultUrl', 'Default.Utility' );
@@ -13,7 +14,8 @@
 	 * La classe DefaultActionHelper fournit des actions à utiliser avec la méthode
 	 * DefaultDefaultHelper::actions().
 	 *
-	 * @package       app.View.Helper
+	 * @package Default
+	 * @subpackage View.Helper
 	 */
 	class DefaultActionHelper extends AppHelper
 	{
