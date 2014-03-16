b4b75434d184e113c46e30381ae5bf67eecf8f83
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-03-16 11:46:34 +0000

    Amélioration du gabarit Netbeans de la classe modèle.

--- trunk/app/Vendor/Netbeans/Templates/Test/CakePHP Model Test Case.php
+++ trunk/app/Vendor/Netbeans/Templates/Test/CakePHP Model Test Case.php
@@ -13,7 +13,7 @@
 	App::uses( '${class_name(name)?replace("Test", "", "r")}', 'Model' );
 
 	/**
-	 * La classe ${name} ...
+	 * La classe ${name} réalise les tests unitaires de la classe ${class_name(name)?replace("Test", "", "r")}.
 	 *
 	 * @package app.Test.Case.Model
 	 */
@@ -25,19 +25,29 @@
 		 * @var array
 		 */
 		public $fixtures = array(
+			'app.${class_name(name)?replace("Test", "", "r")}',
 		);
+
+		/**
+		 * Le modèle à tester.
+		 *
+		 * @var ${class_name(name)?replace("Test", "", "r")}
+		 */
+		public $${class_name(name)?replace("Test", "", "r")} = null;
 
 		/**
 		 * Préparation du test.
 		 */
 		public function setUp() {
 			parent::setUp();
+			$this->${class_name(name)?replace("Test", "", "r")} = ClassRegistry::init( '${class_name(name)?replace("Test", "", "r")}' );
 		}
 
 		/**
 		 * Nettoyage postérieur au test.
 		 */
 		public function tearDown() {
+			unset( $this->${class_name(name)?replace("Test", "", "r")} );
 			parent::tearDown();
 		}
 
@@ -46,6 +56,10 @@
 		 */
 		public function testMethod() {
 			$this->markTestIncomplete( 'Ce test n\'a pas encoré été implémenté.' );
+
+			$result = $this->${class_name(name)?replace("Test", "", "r")}->method();
+			$expected = null;
+			$this->assertEqual( $result, $expected, var_export( $result, true ) );
 		}
 	}
 ?>
