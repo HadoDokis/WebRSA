75fd75fe22f3163208809ce4246293466246018b
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-04-21 10:51:21 +0000

    Ajout de la possibilité de supprimer une action si elle n'est pas utilisée. Refactorisation de certaines parties du code de la fiche de prescription (CG 93).

--- app/Model/Behavior/Cataloguepdifp93Behavior.php
+++ app/Model/Behavior/Cataloguepdifp93Behavior.php
@@ -17,18 +17,86 @@
 	 */
 	class Cataloguepdifp93Behavior extends ModelBehavior
 	{
+		public function searchQuery( Model $Model, array $query = array() ) {
+			// TODO: cache
+//			$cacheKey = sprintf( '%s_%s_%s_%s_%s', $Model->useDbConfig, __CLASS__, $Model->alias, __FUNCTION__, md5( serialize( $query ) ) );
+//			debug( $cacheKey );
+
+			$query += array(
+				'fields' => array(),
+				'joins' => array(),
+				'conditions' => array(),
+				'order' => array()
+			);
+
+			if( !empty( $Model->belongsTo ) ) {
+				foreach( $Model->belongsTo as $alias => $params ) {
+					$Parent = $Model->{$alias};
+
+					$query = $this->searchQuery( $Parent, $query );
+
+					array_unshift( $query['joins'], $Model->join( $alias, array( 'type' => 'INNER' ) ) );
+				}
+			}
+
+			$query['fields'][] = "{$Model->alias}.{$Model->primaryKey}";
+			$query['fields'][] = "{$Model->alias}.{$Model->displayField}";
+
+			/*$query['fields'] = array_merge(
+				$query['fields'],
+				array(
+					"{$Model->alias}.{$Model->primaryKey}",
+					"{$Model->alias}.{$Model->displayField}"
+				)
+			);*/
+
+			if( $Model->alias !== 'Prestatairefp93' ) {
+				if( !in_array( 'Thematiquefp93.type', $query['fields'] ) ) {
+					array_unshift( $query['fields'], 'Thematiquefp93.type' );
+				}
+
+				if( !in_array( 'Thematiquefp93.type DESC', $query['order'] ) ) {
+					array_unshift( $query['order'], 'Thematiquefp93.type DESC' );
+				}
+			}
+
+			// TODO: pdi, thématique, catégorie, ...
+			// array_unshift( $query['order'], $Model->order );
+			$query['order'][] = $Model->order;
+
+			return $query;
+		}
+
+		public function setup( Model $model, $config = array( ) ) {
+			parent::setup( $model, $config );
+
+			if( $model->order === null ) {
+				$model->order = "{$model->alias}.name ASC";
+			}
+
+			/*$query = $this->searchQuery( $model );
+			$results = $model->find( 'all', $query );
+
+			debug( array(
+				'model' => $model->alias,
+				'joins' => (array)Hash::extract( $query, 'joins.{n}.alias' ),
+				'list' => Hash::combine( $results, "{n}.{$model->alias}.{$model->primaryKey}", "{n}.{$model->alias}.{$model->displayField}" ),
+				'query' => $query,
+				'results' => $results,
+			) );*/
+		}
+
 		/**
 		 * Recherche l'enregistrement répondant aux conditions. Si celui-ci existe,
 		 * sa clé primaire est renvoyée; sinon, on tente d'enregistrer les données.
 		 *
-		 * En cas de succès, la clé primaire du nouvel enregistrement est retournée,
-		 * sinon on peuple l'attribut $_validationErrors
+		 * En cas de succès, la clé primaire du nouvel enregistrement est retournée.
 		 *
-		 * @param AppModel $Model
+		 * @param Model $Model
 		 * @param array $conditions
 		 * @return integer
 		 */
-		public function createOrUpdate( AppModel $Model, array $conditions ) {
+		public function createOrUpdate( Model $Model, array $conditions ) {
 			$conditions = Hash::flatten( $Model->doFormatting( Hash::expand( $conditions ) ) );
 
 			$primaryKeyField = "{$Model->alias}.{$Model->primaryKey}";
@@ -38,6 +106,7 @@
 				'conditions' => $conditions
 			);
 
+			// On cherche un intitulé approchant à la casse et aux accents près
 			foreach( $query['conditions'] as $path => $value ) {
 				if( !is_numeric( $value ) ) {
 					unset( $query['conditions'][$path] );
@@ -53,12 +122,6 @@
 				$Model->create( $record );
 
 				if( !$Model->save() ) {
-					// FIXME
-					/*$this->_validationErrors = Hash::merge(
-						$this->_validationErrors,
-						Hash::flatten( array( $Model->alias => $Model->validationErrors ) )
-					);*/
-
 					return null;
 				}
 				else {
@@ -69,5 +132,6 @@
 				return Hash::get( $record, $primaryKeyField );
 			}
 		}
+
 	}
 ?>
