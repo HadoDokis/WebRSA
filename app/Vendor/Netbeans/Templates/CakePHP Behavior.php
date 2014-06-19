9837935969bf159da1f4aaa84909b4471164d1fd
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-06-19 22:48:40 +0000

    Qualité du code: mise en confirmité de la template Netbeans du Behavior CakePHP avec la signature des versions >= 2.2.x.

--- app/Vendor/Netbeans/Templates/CakePHP Behavior.php
+++ app/Vendor/Netbeans/Templates/CakePHP Behavior.php
@@ -19,16 +19,20 @@
 	class ${name} extends ModelBehavior
 	{
 		/**
-		 * Contient une configuration à utiliser, par alias du modèle.
+		 * Contains configuration settings for use with individual model objects. This
+		 * is used because if multiple models use this Behavior, each will use the same
+		 * object instance. Individual model settings should be stored as an
+		 * associative array, keyed off of the model name.
 		 *
 		 * @var array
+		 * @see Model::$alias
 		 */
 		public $settings = array();
 
 		/**
 		 * Allows the mapping of preg-compatible regular expressions to public or
 		 * private methods in this class, where the array key is a /-delimited regular
-		 * expression, and the value is a class method.  Similar to the functionality of
+		 * expression, and the value is a class method. Similar to the functionality of
 		 * the findBy* / findAllBy* magic methods.
 		 *
 		 * @var array
@@ -36,21 +40,22 @@
 		public $mapMethods = array();
 
 		/**
-		 * Configuration du behavior.
+		 * Setup this behavior with the specified configuration settings.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
-		 * @param array $config La configuration à appliquer
+		 * @param Model $Model Model using this behavior
+		 * @param array $config Configuration settings for $model
+		 * @return void
 		 */
 		public function setup( Model $Model, $config = array() ) {
 			parent::setup( $Model, $config );
 		}
 
 		/**
-		 * Clean up any initialization this behavior has done on a model.
-		 * Called when a behavior is dynamically detached from a model using
-		 * Model::detach().
+		 * Clean up any initialization this behavior has done on a model. Called when a behavior is dynamically
+		 * detached from a model using Model::detach().
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
+		 * @return void
 		 * @see BehaviorCollection::detach()
 		 */
 		public function cleanup( Model $Model ) {
@@ -59,10 +64,10 @@
 
 		/**
 		 * beforeFind can be used to cancel find operations, or modify the query that will be executed.
-		 * By returning null/false you can abort a find.  By returning an array you can modify/replace the query
+		 * By returning null/false you can abort a find. By returning an array you can modify/replace the query
 		 * that is going to be run.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @param array $query Data used to execute this query, i.e. conditions, order, etc.
 		 * @return boolean|array False or null will abort the operation. You can return an array to replace the
 		 *   $query that will be eventually run.
@@ -74,32 +79,34 @@
 		/**
 		 * After find callback. Can be used to modify any results returned by find.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @param mixed $results The results of the find operation
 		 * @param boolean $primary Whether this model is being queried directly (vs. being queried as an association)
 		 * @return mixed An array value will replace the value of $results - any other value will be ignored.
 		 */
-		public function afterFind( Model $Model, $results, $primary ) {
+		public function afterFind( Model $Model, $results, $primary = false ) {
 			return parent::afterFind( $Model, $results, $primary );
 		}
 
 		/**
 		 * beforeValidate is called before a model is validated, you can use this callback to
-		 * add behavior validation rules into a models validate array.  Returning false
+		 * add behavior validation rules into a models validate array. Returning false
 		 * will allow you to make the validation fail.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
+		 * @param array $options Options passed from Model::save().
 		 * @return mixed False or null will abort the operation. Any other result will continue.
+		 * @see Model::save()
 		 */
-		public function beforeValidate( Model $Model ) {
-			return parent::beforeValidate( $Model );
+		public function beforeValidate( Model $Model, $options = array() ) {
+			return parent::beforeValidate( $Model, $options );
 		}
 
 		/**
 		 * afterValidate is called just after model data was validated, you can use this callback
 		 * to perform any data cleanup or preparation if needed
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @return mixed False will stop this event from being passed to other behaviors
 		 */
 		public function afterValidate( Model $Model ) {
@@ -107,32 +114,36 @@
 		}
 
 		/**
-		 * beforeSave is called before a model is saved.  Returning false from a beforeSave callback
+		 * beforeSave is called before a model is saved. Returning false from a beforeSave callback
 		 * will abort the save operation.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
+		 * @param array $options Options passed from Model::save().
 		 * @return mixed False if the operation should abort. Any other result will continue.
+		 * @see Model::save()
 		 */
-		public function beforeSave( Model $Model ) {
-			return parent::beforeSave( $Model );
+		public function beforeSave( Model $Model, $options = array( ) ) {
+			return parent::beforeSave( $Model, $options );
 		}
 
 		/**
 		 * afterSave is called after a model is saved.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @param boolean $created True if this save created a new record
+		 * @param array $options Options passed from Model::save().
 		 * @return boolean
+		 * @see Model::save()
 		 */
-		public function afterSave( Model $Model, $created ) {
-			return parent::afterSave( $Model, $created );
+		public function afterSave( Model $Model, $created, $options = array() ) {
+			return parent::afterSave( $Model, $created, $options );
 		}
 
 		/**
 		 * Before delete is called before any delete occurs on the attached model, but after the model's
-		 * beforeDelete is called.  Returning false from a beforeDelete will abort the delete.
+		 * beforeDelete is called. Returning false from a beforeDelete will abort the delete.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @param boolean $cascade If true records that depend on this record will also be deleted
 		 * @return mixed False if the operation should abort. Any other result will continue.
 		 */
@@ -143,7 +154,8 @@
 		/**
 		 * After delete is called after any delete occurs on the attached model.
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
+		 * @return void
 		 */
 		public function afterDelete( Model $Model ) {
 			parent::afterDelete( $Model );
@@ -152,8 +164,9 @@
 		/**
 		 * DataSource error callback
 		 *
-		 * @param Model $Model Le modèle qui utilise ce behavior
+		 * @param Model $Model Model using this behavior
 		 * @param string $error Error generated in DataSource
+		 * @return void
 		 */
 		public function onError( Model $Model, $error ) {
 			parent::onError( $Model, $error );
