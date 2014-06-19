4e56455c2ecc816e8ea4ada3704b657abe16b832
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-06-19 22:52:58 +0000

    Qualité du code: amélioration de la couverture de code des tests unitaires du module "fiche de prescription" (CG 93).

--- trunk/app/Test/Case/Controller/Component/AjaxFichesprescriptions93ComponentTest.php
+++ trunk/app/Test/Case/Controller/Component/AjaxFichesprescriptions93ComponentTest.php
@@ -138,6 +138,8 @@
 
 		/**
 		 * Test de la méthode AjaxFichesprescriptions93Component::ajaxOnChange()
+		 *
+		 * @medium
 		 */
 		public function testAjaxOnChange() {
 			// 1. En changeant le type de thématique
@@ -284,6 +286,10 @@
 									'id' => 1,
 									'name' => 'Action de test',
 								),
+								array(
+									'id' => 2,
+									'name' => 'Action de test',
+								),
 							),
 						),
 					)
@@ -295,7 +301,58 @@
 
 			$this->assertEqual( $result, $expected, var_export( $result, true ) );
 
-			// 4. En changeant le prestataire
+			// 4. En changeant la filière lors d'un add
+			$data = array(
+				'prefix' => null,
+				'Ficheprescription93' => array(
+					'typethematiquefp93_id' => 'pdi',
+					'thematiquefp93_id' => '1',
+					'categoriefp93_id' => '1',
+					'filierefp93_id' => '1',
+					'action' => 'add'
+				),
+				'Target' => array(
+					'name' => 'data[Ficheprescription93][filierefp93_id]'
+				)
+			);
+			$result = $this->Controller->AjaxFichesprescriptions93->ajaxOnChange( $data );
+
+			$expected = Hash::merge(
+				$this->emptyJson['onChange'],
+				array(
+					'fields' => array(
+						'Ficheprescription93.prestatairefp93_id' => array(
+							'id' => 'Ficheprescription93Prestatairefp93Id',
+							'value' => NULL,
+							'type' => 'select',
+							'options' => array(
+								array(
+									'id' => 1,
+									'name' => 'Association LE PRISME',
+								),
+							),
+						),
+						'Ficheprescription93.actionfp93_id' => array(
+							'id' => 'Ficheprescription93Actionfp93Id',
+							'value' => NULL,
+							'type' => 'select',
+							'options' => array(
+								array(
+									'id' => 2,
+									'name' => 'Action de test',
+								),
+							),
+						),
+					)
+				)
+			);
+			unset( $expected['fields']['Ficheprescription93.thematiquefp93_id'] );
+			unset( $expected['fields']['Ficheprescription93.categoriefp93_id'] );
+			unset( $expected['fields']['Ficheprescription93.filierefp93_id'] );
+
+			$this->assertEqual( $result, $expected, var_export( $result, true ) );
+
+			// 5. En changeant le prestataire
 			$data = array(
 				'prefix' => null,
 				'Ficheprescription93' => array(
@@ -324,6 +381,10 @@
 									'id' => 1,
 									'name' => 'Action de test',
 								),
+								array(
+									'id' => 2,
+									'name' => 'Action de test',
+								),
 							),
 						),
 						'Ficheprescription93.adresseprestatairefp93_id' => array(
@@ -347,7 +408,60 @@
 
 			$this->assertEqual( $result, $expected, var_export( $result, true ) );
 
-			// 5. En changeant l'action
+			// 6. En changeant le prestataire lors d'un add
+			$data = array(
+				'prefix' => null,
+				'Ficheprescription93' => array(
+					'typethematiquefp93_id' => 'pdi',
+					'thematiquefp93_id' => '1',
+					'categoriefp93_id' => '1',
+					'filierefp93_id' => '1',
+					'prestatairefp93_id' => '1',
+					'action' => 'add',
+				),
+				'Target' => array(
+					'name' => 'data[Ficheprescription93][prestatairefp93_id]'
+				)
+			);
+			$result = $this->Controller->AjaxFichesprescriptions93->ajaxOnChange( $data );
+
+			$expected = Hash::merge(
+				$this->emptyJson['onChange'],
+				array(
+					'fields' => array(
+						'Ficheprescription93.actionfp93_id' => array(
+							'id' => 'Ficheprescription93Actionfp93Id',
+							'value' => NULL,
+							'type' => 'select',
+							'options' => array(
+								array(
+									'id' => 2,
+									'name' => 'Action de test',
+								),
+							),
+						),
+						'Ficheprescription93.adresseprestatairefp93_id' => array(
+								'id' => 'Ficheprescription93Adresseprestatairefp93Id',
+								'value' => 1,
+								'type' => 'select',
+								'options' => array(
+									array(
+										'id' => 1,
+										'name' => 'Av. de la république, 93000 Bobigny',
+									),
+								),
+							)
+					)
+				)
+			);
+			unset( $expected['fields']['Ficheprescription93.thematiquefp93_id'] );
+			unset( $expected['fields']['Ficheprescription93.categoriefp93_id'] );
+			unset( $expected['fields']['Ficheprescription93.filierefp93_id'] );
+			unset( $expected['fields']['Ficheprescription93.prestatairefp93_id'] );
+
+			$this->assertEqual( $result, $expected, var_export( $result, true ) );
+
+			// 7. En changeant l'action
 			$data = array(
 				'prefix' => null,
 				'Ficheprescription93' => array(
@@ -364,31 +478,12 @@
 			);
 			$result = $this->Controller->AjaxFichesprescriptions93->ajaxOnChange( $data );
 
-			$expected = Hash::merge(
-				$this->emptyJson['onChange'],
-				array(
-					'fields' => array(
-						'Ficheprescription93.numconvention' => array(
-							'id' => 'Ficheprescription93Numconvention',
-							'value' => '93XXX1400001',
-							'type' => 'text',
-							'options' => array( ),
-						),
-						'Ficheprescription93.prestatairefp93_id' => array(
-							'value' => 1,
-						),
-						'Ficheprescription93.duree_action' => array(
-							'value' => NULL,
-						),
-					)
-				)
-			);
 			$expected = array(
 				'success' => true,
 				'fields' => array(
 					'Ficheprescription93.numconvention' => array(
 						'id' => 'Ficheprescription93Numconvention',
-						'value' => '93XXX1400001',
+						'value' => '93XXX1300001',
 						'type' => 'text',
 						'options' => array( ),
 					),
@@ -399,6 +494,30 @@
 						'value' => NULL,
 					),
 				),
+			);
+			$this->assertEqual( $result, $expected, var_export( $result, true ) );
+
+			// 8. En changeant l'adresse du prestataire
+			$data = array(
+				'prefix' => null,
+				'Ficheprescription93' => array(
+					'typethematiquefp93_id' => 'pdi',
+					'thematiquefp93_id' => '1',
+					'categoriefp93_id' => '1',
+					'filierefp93_id' => '1',
+					'prestatairefp93_id' => '1',
+					'actionfp93_id' => '1',
+					'adresseprestatairefp93_id' => '1',
+				),
+				'Target' => array(
+					'name' => 'data[Ficheprescription93][adresseprestatairefp93_id]'
+				)
+			);
+			$result = $this->Controller->AjaxFichesprescriptions93->ajaxOnChange( $data );
+
+			$expected = array(
+				'success' => true,
+				'fields' => array(),
 			);
 			$this->assertEqual( $result, $expected, var_export( $result, true ) );
 		}
