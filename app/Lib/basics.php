d0a99c6c8a5ff9bdd0768fd6e8a7f7a3d81d4adc
arnauz <arnauz@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-07-03 16:45:15 +0000

    Modification: suite aux évolutions du formulaire CUI, des traductions ont été mises en place pour la gestion des droits + pour les boutons nouvellement ajoutés, tel que l'envoi de mail à l'employeur. Une vue a notamment été ajoutée pour cet envoi de mail à l'employeur afin de pouvoir saisir la date du jour où le mail est envoyé. NB: Une fonction permettant de calculer la différence en nombre de jours entre deux dates a été mise en place dans app/Lib/basics.php afin de calculer la différence entre la date de demande RSA et la date du jour (au format 0 an, 2 mois, 3 semaines et 10 jours)

--- app/Lib/basics.php
+++ app/Lib/basics.php
@@ -1124,4 +1124,27 @@
 	function preg_test( $pattern ) {
 		return ( @preg_match( $pattern, '' ) !== false );
 	}
+    
+    
+    $now   = time();
+    $date2 = strtotime('2012-08-14 16:01:05');
+ 
+    function dateDiff($date1, $date2){
+        $diff = abs($date1 - $date2); // abs pour avoir la valeur absolute, ainsi éviter d'avoir une différence négative
+        $retour = array();
+
+        $tmp = $diff;
+        $retour['second'] = $tmp % 60;
+
+        $tmp = floor( ($tmp - $retour['second']) /60 );
+        $retour['minute'] = $tmp % 60;
+
+        $tmp = floor( ($tmp - $retour['minute'])/60 );
+        $retour['hour'] = $tmp % 24;
+
+        $tmp = floor( ($tmp - $retour['hour'])  /24 );
+        $retour['day'] = $tmp;
+
+        return $retour;
+    }
 ?>
