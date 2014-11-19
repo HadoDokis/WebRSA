debf405af2253a14ea73e741248f488d5927440c
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2014-11-19 14:37:41 +0000

    Correction de ceertains tests unitaires qui ne passaient plus suite à des modifications du code.

--- app/Test/Case/Model/Criteredossierpcg66Test.php
+++ app/Test/Case/Model/Criteredossierpcg66Test.php
@@ -52,143 +52,152 @@
 		protected $_querydatas = array(
 			'searchDossier' => array(
 				'fields' => array(
-					'Dossierpcg66.id',
-					'Dossierpcg66.foyer_id',
-					'Dossierpcg66.datereceptionpdo',
-					'Dossierpcg66.typepdo_id',
-					'Dossierpcg66.etatdossierpcg',
-					'Dossierpcg66.datetransmission',
-					'Dossierpcg66.dateaffectation',
-					'Dossierpcg66.poledossierpcg66_id',
-					'Decisiondossierpcg66.datetransmissionop',
-					'Decisiondossierpcg66.datevalidation',
-					'Decisiondossierpcg66.useravistechnique_id',
-					'Decisiondossierpcg66.userproposition_id',
-					'Decisiondossierpcg66.etatop',
-					'Dossierpcg66.originepdo_id',
-					'Dossierpcg66.user_id',
-					'Dossier.id',
-					'Dossier.numdemrsa',
-					'Dossier.dtdemrsa',
-					'Dossier.matricule',
-					'Personne.id',
-					'Personne.nom',
-					'Personne.prenom',
-					'Personne.dtnai',
-					'Personne.nir',
-					'Personne.qual',
-					'Personne.nomcomnai',
-					'Adresse.nomcom',
-					'Adresse.codepos',
-					'Adresse.numcom',
-					'Situationdossierrsa.etatdosrsa',
-					'( ( SELECT COUNT(*) FROM decisionsdossierspcgs66 WHERE decisionsdossierspcgs66.dossierpcg66_id = "Dossierpcg66"."id" ) ) AS "Dossierpcg66__nbpropositions"',
-					'( SELECT COUNT("fichiermodule"."id") FROM "fichiersmodules" AS "fichiermodule" WHERE "fichiermodule"."modele" = \'Foyer\' AND "fichiermodule"."fk_value" = "Foyer"."id" ) AS "Fichiermodule__nb_fichiers_lies"',
-					'Decisionpdo.libelle',
-					'( ( "User"."nom" || \' \' || "User"."prenom" ) ) AS "User__nom_complet"',
-					'Poledossierpcg66.name',
-					'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Situationpdo"."libelle" || \'\' AS "Situationpdo__libelle" FROM "personnespcgs66" AS "Personnepcg66" LEFT OUTER JOIN "public"."personnespcgs66_situationspdos" AS "Personnepcg66Situationpdo" ON ("Personnepcg66Situationpdo"."personnepcg66_id" = "Personnepcg66"."id") LEFT OUTER JOIN "public"."situationspdos" AS "Situationpdo" ON ("Personnepcg66Situationpdo"."situationpdo_id" = "Situationpdo"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Personnepcg66__listemotifs"',
-					'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Statutpdo"."libelle" || \'\' AS "Statutpdo__libelle" FROM "personnespcgs66" AS "Personnepcg66" LEFT OUTER JOIN "public"."personnespcgs66_statutspdos" AS "Personnepcg66Statutpdo" ON ("Personnepcg66Statutpdo"."personnepcg66_id" = "Personnepcg66"."id") LEFT OUTER JOIN "public"."statutspdos" AS "Statutpdo" ON ("Personnepcg66Statutpdo"."statutpdo_id" = "Statutpdo"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Personnepcg66__listestatuts"',
-					'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Traitementpcg66"."typetraitement" || \'\' AS "Traitementpcg66__typetraitement" FROM "traitementspcgs66" AS "Traitementpcg66" INNER JOIN "public"."personnespcgs66" AS "Personnepcg66" ON ("Traitementpcg66"."personnepcg66_id" = "Personnepcg66"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Dossierpcg66__listetraitements"',
-					'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Traitementpcg66"."dateecheance" || \'\' AS "Traitementpcg66__dateecheance" FROM "traitementspcgs66" AS "Traitementpcg66" INNER JOIN "public"."personnespcgs66" AS "Personnepcg66" ON ("Traitementpcg66"."personnepcg66_id" = "Personnepcg66"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Dossierpcg66__dateecheance"',
-					'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Orgtransmisdossierpcg66"."name" || \'\' AS "Orgtransmisdossierpcg66__name" FROM "decisionsdossierspcgs66" AS "Decisiondossierpcg66" LEFT JOIN "public"."decsdospcgs66_orgsdospcgs66" AS "Decdospcg66Orgdospcg66" ON ("Decdospcg66Orgdospcg66"."decisiondossierpcg66_id" = "Decisiondossierpcg66"."id") LEFT JOIN "public"."orgstransmisdossierspcgs66" AS "Orgtransmisdossierpcg66" ON ("Decdospcg66Orgdospcg66"."orgtransmisdossierpcg66_id" = "Orgtransmisdossierpcg66"."id") WHERE "Decisiondossierpcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ORDER BY "Decisiondossierpcg66"."created" DESC ), \'\' ) ) ) AS "Orgtransmisdossierpcg66__listorgs"',
-					'PersonneReferent.referent_id',
-					'( ( "Referentparcours"."qual" || \' \' || "Referentparcours"."nom" || \' \' || "Referentparcours"."prenom" ) ) AS "Referentparcours__nom_complet"',
-					'Structurereferenteparcours.lib_struc',
+					0 => 'Dossierpcg66.id',
+					1 => 'Dossierpcg66.foyer_id',
+					2 => 'Dossierpcg66.datereceptionpdo',
+					3 => 'Dossierpcg66.typepdo_id',
+					4 => 'Dossierpcg66.etatdossierpcg',
+					5 => 'Dossierpcg66.datetransmission',
+					6 => 'Dossierpcg66.dateaffectation',
+					7 => 'Dossierpcg66.poledossierpcg66_id',
+					8 => 'Decisiondossierpcg66.datetransmissionop',
+					9 => 'Decisiondossierpcg66.datevalidation',
+					10 => 'Decisiondossierpcg66.useravistechnique_id',
+					11 => 'Decisiondossierpcg66.userproposition_id',
+					12 => 'Decisiondossierpcg66.etatop',
+					13 => 'Dossierpcg66.originepdo_id',
+					14 => 'Dossierpcg66.user_id',
+					15 => 'Dossier.id',
+					16 => 'Dossier.numdemrsa',
+					17 => 'Dossier.dtdemrsa',
+					18 => 'Dossier.matricule',
+					19 => 'Personne.id',
+					20 => 'Personne.nom',
+					21 => 'Personne.prenom',
+					22 => 'Personne.dtnai',
+					23 => 'Personne.nir',
+					24 => 'Personne.qual',
+					25 => 'Personne.nomcomnai',
+					26 => 'Adresse.nomcom',
+					27 => 'Adresse.codepos',
+					28 => 'Adresse.numcom',
+					29 => 'Situationdossierrsa.etatdosrsa',
+					30 => '( ( SELECT COUNT(*) FROM decisionsdossierspcgs66 WHERE decisionsdossierspcgs66.dossierpcg66_id = "Dossierpcg66"."id" ) ) AS "Dossierpcg66__nbpropositions"',
+					31 => '( SELECT COUNT("fichiermodule"."id") FROM "fichiersmodules" AS "fichiermodule" WHERE "fichiermodule"."modele" = \'Foyer\' AND "fichiermodule"."fk_value" = "Foyer"."id" ) AS "Fichiermodule__nb_fichiers_lies"',
+					32 => 'Decisionpdo.libelle',
+					33 => '( ( "User"."nom" || \' \' || "User"."prenom" ) ) AS "User__nom_complet"',
+					34 => 'Poledossierpcg66.name',
+					35 => 'Traitementpcg66.id',
+					36 => 'Traitementpcg66.datereception',
+					37 => 'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Situationpdo"."libelle" || \'\' AS "Situationpdo__libelle" FROM "personnespcgs66" AS "Personnepcg66" LEFT OUTER JOIN "public"."personnespcgs66_situationspdos" AS "Personnepcg66Situationpdo" ON ("Personnepcg66Situationpdo"."personnepcg66_id" = "Personnepcg66"."id") LEFT OUTER JOIN "public"."situationspdos" AS "Situationpdo" ON ("Personnepcg66Situationpdo"."situationpdo_id" = "Situationpdo"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Personnepcg66__listemotifs"',
+					38 => 'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Statutpdo"."libelle" || \'\' AS "Statutpdo__libelle" FROM "personnespcgs66" AS "Personnepcg66" LEFT OUTER JOIN "public"."personnespcgs66_statutspdos" AS "Personnepcg66Statutpdo" ON ("Personnepcg66Statutpdo"."personnepcg66_id" = "Personnepcg66"."id") LEFT OUTER JOIN "public"."statutspdos" AS "Statutpdo" ON ("Personnepcg66Statutpdo"."statutpdo_id" = "Statutpdo"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Personnepcg66__listestatuts"',
+					39 => 'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Traitementpcg66"."typetraitement" || \'\' AS "Traitementpcg66__typetraitement" FROM "traitementspcgs66" AS "Traitementpcg66" INNER JOIN "public"."personnespcgs66" AS "Personnepcg66" ON ("Traitementpcg66"."personnepcg66_id" = "Personnepcg66"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Dossierpcg66__listetraitements"',
+					40 => 'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Traitementpcg66"."dateecheance" || \'\' AS "Traitementpcg66__dateecheance" FROM "traitementspcgs66" AS "Traitementpcg66" INNER JOIN "public"."personnespcgs66" AS "Personnepcg66" ON ("Traitementpcg66"."personnepcg66_id" = "Personnepcg66"."id") WHERE "Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ), \'\' ) ) ) AS "Dossierpcg66__dateecheance"',
+					41 => 'TRIM( BOTH \' \' FROM TRIM( TRAILING \'\' FROM ARRAY_TO_STRING( ARRAY( SELECT \'\\n\\r-\' || "Orgtransmisdossierpcg66"."name" || \'\' AS "Orgtransmisdossierpcg66__name" FROM "decisionsdossierspcgs66" AS "Decisiondossierpcg66" LEFT JOIN "public"."decsdospcgs66_orgsdospcgs66" AS "Decdospcg66Orgdospcg66" ON ("Decdospcg66Orgdospcg66"."decisiondossierpcg66_id" = "Decisiondossierpcg66"."id") LEFT JOIN "public"."orgstransmisdossierspcgs66" AS "Orgtransmisdossierpcg66" ON ("Decdospcg66Orgdospcg66"."orgtransmisdossierpcg66_id" = "Orgtransmisdossierpcg66"."id") WHERE "Decisiondossierpcg66"."dossierpcg66_id" = "Dossierpcg66"."id" ORDER BY "Decisiondossierpcg66"."created" DESC ), \'\' ) ) ) AS "Orgtransmisdossierpcg66__listorgs"',
+					42 => 'PersonneReferent.referent_id',
+					43 => '( ( "Referentparcours"."qual" || \' \' || "Referentparcours"."nom" || \' \' || "Referentparcours"."prenom" ) ) AS "Referentparcours__nom_complet"',
+					44 => 'Structurereferenteparcours.lib_struc',
 				),
 				'recursive' => '-1',
 				'joins' => array(
-					array(
+					0 => array(
 						'table' => '"foyers"',
 						'alias' => 'Foyer',
 						'type' => 'INNER',
 						'conditions' => '"Dossierpcg66"."foyer_id" = "Foyer"."id"',
 					),
-					array(
+					1 => array(
 						'table' => '"users"',
 						'alias' => 'User',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Dossierpcg66"."user_id" = "User"."id"',
 					),
-					array(
+					2 => array(
 						'table' => '"personnes"',
 						'alias' => 'Personne',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Personne"."foyer_id" = "Foyer"."id" AND "Personne"."id" IN ( SELECT "personnes"."id" AS personnes__id FROM personnes AS personnes LEFT JOIN "public"."prestations" AS prestations ON ("prestations"."personne_id" = "personnes"."id" AND "prestations"."natprest" = \'RSA\') WHERE "personnes"."foyer_id" = "Foyer"."id" AND "prestations"."rolepers" = \'DEM\' LIMIT 1 )',
 					),
-					array(
+					3 => array(
 						'table' => '"personnespcgs66"',
 						'alias' => 'Personnepcg66',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Personnepcg66"."dossierpcg66_id" = "Dossierpcg66"."id" AND "Personnepcg66"."personne_id" = "Personne"."id"',
 					),
-					array(
+					4 => array(
+						'table' => '"traitementspcgs66"',
+						'alias' => 'Traitementpcg66',
+						'type' => 'LEFT OUTER',
+						'conditions' => '"Traitementpcg66"."personnepcg66_id" = "Personnepcg66"."id" AND ( "Traitementpcg66"."id" IS NULL OR "Traitementpcg66"."id" IN ( SELECT "traitementspcgs66"."id" AS traitementspcgs66__id FROM traitementspcgs66 AS traitementspcgs66 WHERE "traitementspcgs66"."personnepcg66_id" = "Personnepcg66"."id" ORDER BY "traitementspcgs66"."created" DESC LIMIT 1 ) ) AND "Traitementpcg66"."typetraitement" = \'documentarrive\' AND "Traitementpcg66"."datereception" IS NOT NULL AND "Dossierpcg66"."etatdossierpcg" = \'attinstrdocarrive\'',
+					),
+					5 => array(
 						'table' => '"adressesfoyers"',
 						'alias' => 'Adressefoyer',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Adressefoyer"."foyer_id" = "Foyer"."id"',
 					),
-					array(
+					6 => array(
 						'table' => '"adresses"',
 						'alias' => 'Adresse',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Adressefoyer"."adresse_id" = "Adresse"."id"',
 					),
-					array(
+					7 => array(
 						'table' => '"dossiers"',
 						'alias' => 'Dossier',
 						'type' => 'INNER',
 						'conditions' => '"Foyer"."dossier_id" = "Dossier"."id"',
 					),
-					array(
+					8 => array(
 						'table' => '"situationsdossiersrsa"',
 						'alias' => 'Situationdossierrsa',
 						'type' => 'INNER',
 						'conditions' => '"Situationdossierrsa"."dossier_id" = "Dossier"."id"',
 					),
-					array(
+					9 => array(
 						'table' => '"decisionsdossierspcgs66"',
 						'alias' => 'Decisiondossierpcg66',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Decisiondossierpcg66"."dossierpcg66_id" = "Dossierpcg66"."id"',
 					),
+					10 =>
 					array(
 						'table' => '"decisionspdos"',
 						'alias' => 'Decisionpdo',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Decisiondossierpcg66"."decisionpdo_id" = "Decisionpdo"."id"',
 					),
-					array(
+					11 => array(
 						'table' => '"detailsdroitsrsa"',
 						'alias' => 'Detaildroitrsa',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Detaildroitrsa"."dossier_id" = "Dossier"."id"',
 					),
-					array(
+					12 => array(
 						'table' => '"detailscalculsdroitsrsa"',
 						'alias' => 'Detailcalculdroitrsa',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Detailcalculdroitrsa"."detaildroitrsa_id" = "Detaildroitrsa"."id"',
 					),
-					array(
+					13 => array(
 						'table' => '"polesdossierspcgs66"',
 						'alias' => 'Poledossierpcg66',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"Dossierpcg66"."poledossierpcg66_id" = "Poledossierpcg66"."id"',
 					),
-					array(
+					14 => array(
 						'table' => '"personnes_referents"',
 						'alias' => 'PersonneReferent',
 						'type' => 'LEFT OUTER',
-						'conditions' => '"PersonneReferent"."personne_id" = "Personne"."id" AND (("PersonneReferent"."id" IS NULL) OR ("PersonneReferent"."id" IN ( SELECT "personnes_referents"."id" FROM personnes_referents WHERE "personnes_referents"."personne_id" = "Personne"."id" AND "personnes_referents"."dfdesignation" IS NULL ORDER BY "personnes_referents"."dddesignation" DESC LIMIT 1 )))'
+						'conditions' => '"PersonneReferent"."personne_id" = "Personne"."id" AND (("PersonneReferent"."id" IS NULL) OR ("PersonneReferent"."id" IN ( SELECT "personnes_referents"."id" FROM personnes_referents WHERE "personnes_referents"."personne_id" = "Personne"."id" AND "personnes_referents"."dfdesignation" IS NULL ORDER BY "personnes_referents"."dddesignation" DESC LIMIT 1 )))',
 					),
-					array(
+					15 => array(
 						'table' => '"referents"',
 						'alias' => 'Referentparcours',
 						'type' => 'LEFT OUTER',
 						'conditions' => '"PersonneReferent"."referent_id" = "Referentparcours"."id"',
 					),
-					array(
+					16 => array(
 						'table' => '"structuresreferentes"',
 						'alias' => 'Structurereferenteparcours',
 						'type' => 'LEFT OUTER',
@@ -197,27 +206,27 @@
 				),
 				'limit' => '10',
 				'conditions' => array(
-					array(
-						array(
+					0 => array(
+						0 => array(
 							'OR' => array(
-								'Adressefoyer.id IS NULL',
-								'Adressefoyer.id IN ( SELECT "adressesfoyers"."id" AS "adressesfoyers__id" FROM "adressesfoyers" AS "adressesfoyers" WHERE "adressesfoyers"."foyer_id" = "Foyer"."id" AND "adressesfoyers"."rgadr" = \'01\' ORDER BY "adressesfoyers"."dtemm" DESC LIMIT 1 )'
+								0 => 'Adressefoyer.id IS NULL',
+								1 => 'Adressefoyer.id IN ( SELECT "adressesfoyers"."id" AS "adressesfoyers__id" FROM "adressesfoyers" AS "adressesfoyers" WHERE "adressesfoyers"."foyer_id" = "Foyer"."id" AND "adressesfoyers"."rgadr" = \'01\' ORDER BY "adressesfoyers"."dtemm" DESC LIMIT 1 )',
 							),
 						),
-						array(
+						1 => array(
 							'OR' => array(
-								'Detailcalculdroitrsa.id IS NULL',
-								'Detailcalculdroitrsa.id IN ( SELECT "detailscalculsdroitsrsa"."id" AS "detailscalculsdroitsrsa__id" FROM "detailscalculsdroitsrsa" AS "detailscalculsdroitsrsa" WHERE "detailscalculsdroitsrsa"."detaildroitrsa_id" = "Detaildroitrsa"."id" ORDER BY "detailscalculsdroitsrsa"."ddnatdro" DESC LIMIT 1 )',
+								0 => 'Detailcalculdroitrsa.id IS NULL',
+								1 => 'Detailcalculdroitrsa.id IN ( SELECT "detailscalculsdroitsrsa"."id" AS "detailscalculsdroitsrsa__id" FROM "detailscalculsdroitsrsa" AS "detailscalculsdroitsrsa" WHERE "detailscalculsdroitsrsa"."detaildroitrsa_id" = "Detaildroitrsa"."id" ORDER BY "detailscalculsdroitsrsa"."ddnatdro" DESC LIMIT 1 )',
 							),
 						),
-						array(
+						2 => array(
 							'OR' => array(
-								'Decisiondossierpcg66.id IS NULL',
-								'Decisiondossierpcg66.id IN ( SELECT decisionsdossierspcgs66.id FROM decisionsdossierspcgs66 WHERE decisionsdossierspcgs66.dossierpcg66_id = Dossierpcg66.id ORDER BY decisionsdossierspcgs66.created DESC LIMIT 1 )',
+								0 => 'Decisiondossierpcg66.id IS NULL',
+								1 => 'Decisiondossierpcg66.id IN ( SELECT decisionsdossierspcgs66.id FROM decisionsdossierspcgs66 WHERE decisionsdossierspcgs66.dossierpcg66_id = Dossierpcg66.id ORDER BY decisionsdossierspcgs66.created DESC LIMIT 1 )',
 							),
 						),
 					),
-					''
+					1 => '',
 				),
 			),
 			'searchGestionnaire' => array(
