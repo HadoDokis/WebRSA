c1bc64bb831fb0c096cb92b4dc3fbee2811d2492
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-07-03 15:08:05 +0000

    Amélioration; à présent, les tests du plugin MultiDomainsTranslator fonctionnent indistinctement dans les répertoires app/Plugin et pugins.

--- app/Plugin/MultiDomainsTranslator/Test/Case/Utility/MultiDomainsTranslator/MultiDomainsTranslatorTest.php
+++ app/Plugin/MultiDomainsTranslator/Test/Case/Utility/MultiDomainsTranslator/MultiDomainsTranslatorTest.php
@@ -19,7 +19,7 @@
 	{
 		/**
 		 * Defini une url fictive
-		 * 
+		 *
 		 * @param array $requestParams
 		 */
 		protected function _setRequest( array $requestParams = array() ) {
@@ -35,10 +35,10 @@
 			$request = new CakeRequest();
 
 			$request->addParams( $requestParams );
-			
+
 			Router::setRequestInfo( $request );
 		}
-		
+
 		/**
 		 * setUp method
 		 *
@@ -48,103 +48,103 @@
 			parent::setUp();
 
 			$this->_setRequest( array('controller' => 'tests', 'action' => 'index') );
-			App::build( array('locales' => APP.'Plugin/MultiDomainsTranslator/Test/Locale/') );
+			App::build( array( 'locales' => CakePlugin::path( 'MultiDomainsTranslator' ).'Test'.DS.'Locale'.DS ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::translate();
 		 */
 		public function testTranslate() {
 			Configure::write( 'Cg.departement', 66 ); // Juste pour l'exemple d'utilisation
 			Configure::write( 'MultiDomainsTranslator.prefix', 'cd'.Configure::read( 'Cg.departement' ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Traduction pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test.n2', 1 );
 			$expected = '1 Traduction pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'Test.test' );
 			$expected = 'Traduction pour model CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator.prefix', 'cd93' );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Traduction pour controller CD93';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'Test.test' );
 			$expected = 'Traduction pour model CD93';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test2' );
 			$expected = 'Controller par defaut';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'Test.test2' );
 			$expected = 'Model par defaut';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'Foo.bar' ); // Not found
 			$expected = 'Foo.bar';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'Autremodeltest.test' );
 			$expected = 'Selection d\'un autre model';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'monprefix', 'separator' => '%%' ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Changement de prefix et de séparateur';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'eng', 'separator' => '_' ) );
 			$_SESSION['Config']['language'] = 'eng';
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'English traduction\'s file';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::translatePlural();
 		 */
 		public function testTranslatePlural() {
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'cd66', 'separator' => '_' ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'test', 'tests', 1 );
 			$expected = 'Traduction pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'test', 'test_plural', 2 );
 			$expected = 'Traductions pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'Test.test2', 'Test.test2_plural', 2 );
 			$expected = 'Models par defaut';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'test.n2', 'tests.n2', 1, 1 );
 			$expected = '1 Traduction pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'test.n2', 'test_plural.n2', 2, 2 );
 			$expected = '2 Traductions pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::translatePlural( 'foo', 'foos', 2 ); // Not found
 			$expected = 'foos';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::language();
 		 */
@@ -153,57 +153,57 @@
 			$result = MultiDomainsTranslator::language();
 			$expected = 'eng';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$_SESSION['Config']['language'] = 'fre';
 			$result = MultiDomainsTranslator::language();
 			$expected = 'fre';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::reset();
 		 */
 		public function testReset() {
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'cd66', 'separator' => '_' ) );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Traduction pour controller CD66';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			Configure::write( 'MultiDomainsTranslator.prefix', 'cd93' );
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Traduction pour controller CD66'; // Pas de changement malgrès le changement du prefix
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			MultiDomainsTranslator::reset();
-			
+
 			$result = MultiDomainsTranslator::translate( 'test' );
 			$expected = 'Traduction pour controller CD93';
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::urlDomains();
 		 */
 		public function testUrlDomains() {
 			MultiDomainsTranslator::reset();
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'cd66', 'separator' => '_' ) );
-			
+
 			$result = MultiDomainsTranslator::urlDomains();
 			$expected = array( 'cd66_tests_index', 'tests_index', 'cd66_tests', 'tests' );
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			MultiDomainsTranslator::reset();
 			$this->_setRequest( array('controller' => 'foos', 'action' => 'bar') );
 			Configure::write( 'MultiDomainsTranslator', array( 'prefix' => 'cd93', 'separator' => '#' ) );
-			
+
 			$result = MultiDomainsTranslator::urlDomains();
 			$expected = array( 'cd93#foos#bar', 'foos#bar', 'cd93#foos', 'foos' );
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
 		}
-		
+
 		/**
 		 * Test de la méthode MultiDomainsTranslator::model_field();
 		 */
@@ -211,11 +211,11 @@
 			$result = MultiDomainsTranslator::model_field( 'Testmodel.test' );
 			$expected = array( 'Testmodel', 'test' );
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::model_field( 'Testmodel.0.test' );
 			$expected = array( 'Testmodel', 'test' );
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
-			
+
 			$result = MultiDomainsTranslator::model_field( 'Testmodel' );
 			$expected = null;
 			$this->assertEquals( $result, $expected, var_export( $result, true ) );
