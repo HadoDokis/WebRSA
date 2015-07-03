6e294390c159db5308012ad6ff302a0f63126a1e
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-07-03 14:57:09 +0000

    Correction: à présent, il n'y a plus de message d'erreur dans la fonction svnbackup du webrsa.sh lorsque des fichiers sont déplacés.

--- app/webrsa.sh
+++ app/webrsa.sh
@@ -296,7 +296,7 @@ function __svnbackup() {
 		local SAVEIFS=$IFS
 		IFS=$(echo -en "\n\b")
 
-		status="`svn status . | grep -v "^\(\!\|D\)" | sed 's/^\(.\{8\}\)\(.*\)$/\2/'`";
+		status="`svn status . | grep -v "\(^\(\!\|D\)\|>\)" | sed 's/^\(.\{8\}\)\(.*\)$/\2/'`";
 		for file in `echo "$status"`; do
 			dir="`dirname "$file" | sed "s@^\./@$PWD@"`"
 			if [ "$dir" != '.' ] ; then
