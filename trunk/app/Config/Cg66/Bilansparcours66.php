b6de28db30599d025827a396f0e1d267dc34b9e1
ludodev <ludodev@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-07-30 09:50:02 +0000

    Ajout : nouveau moteur de recherche par bilan de parcours

---
+++ trunk/app/Config/Cg66/Bilansparcours66.php
@@ -0,0 +1,56 @@
+<?php
+	Configure::write(
+		'Filtresdefaut.Bilansparcours66_search',
+		array(
+			'Search' => array(
+				'Pagination' => array(
+					'nombre_total' => 0
+				),
+			)
+		)
+	);
+	
+	Configure::write(
+		'Bilansparcours66',
+		array(
+			'search' => array(
+				'fields' => array (
+					'Dossier.numdemrsa',
+					'Bilanparcours66.datebilan',
+					'Personne.nom_complet',
+					'Structurereferente.lib_struc',
+					'Referent.nom_complet',
+					'Bilanparcours66.proposition',
+					'Bilanparcours66.positionbilan',
+					'Bilanparcours66.choixparcours',
+					'Bilanparcours66.examenaudition',
+					'Bilanparcours66.examenauditionpe',
+					'Dossierep.themeep',
+					'/Bilansparcours66/index/#Bilanparcours66.personne_id#' => array( 'class' => 'view' ),
+				),
+				'innerTable' => array(
+					'Adresse.numcom',
+					'Adresse.nomcom',
+					'Structurereferenteparcours.lib_struc',
+					'Referentparcours.nom_complet'
+				)
+			),
+			'exportcsv' => array(
+				'Bilanparcours66.datebilan',
+				'Personne.nom_complet',
+				'Dossier.matricule',
+				'Structurereferente.lib_struc',
+				'Referent.nom_complet',
+				'Bilanparcours66.proposition',
+				'Bilanparcours66.positionbilan',
+				'Bilanparcours66.choixparcours',
+				'Bilanparcours66.examenaudition',
+				'Bilanparcours66.examenauditionpe',
+				'Adresse.numcom',
+				'Adresse.nomcom',
+				'Structurereferenteparcours.lib_struc',
+				'Referentparcours.nom_complet'
+			)
+		)
+	);
+?>
