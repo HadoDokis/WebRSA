caa085e33477a9ba49dd12c961df161129ef10b1
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-09-21 12:18:14 +0000

    Améliorations (plugin Graphviz): à présent, le shell Graphviz.graphviz_mpd2 retourne un schéma ressemblant beaucoup plus à un MDP, le type de champ reflète mieux le type de champ en BDD PostgreSQL.

--- app/Plugin/Graphviz/Console/Command/Task/GraphvizTablesTask.php
+++ app/Plugin/Graphviz/Console/Command/Task/GraphvizTablesTask.php
@@ -48,14 +48,58 @@
 			return $tables;
 		}
 
+		protected $_typemap = array(
+			'character varying' => 'varchar',
+			'USER-DEFINED' => 'text'
+		);
+
+		// INFO: ne fonctionne qu'avec PostgreSQL
 		public function getFields( $tableName ) {
-			$schema = $this->Dbo->describe( $tableName );
-			return array_combine( array_keys( $schema ), Hash::extract( $schema, '{s}.type' ) );
+			/*$schema = $this->Dbo->describe( $tableName );
+			return $schema;*/
+			$result = array();
+			$sql = "SELECT
+							column_name AS \"name\",
+							data_type AS \"type\",
+							is_nullable AS \"null\",
+							column_default AS \"default\",
+							character_maximum_length AS \"length\"
+						FROM information_schema.columns
+						WHERE
+							table_name = 'personnes'
+							AND table_schema = 'public'
+						ORDER BY ordinal_position;";
+			foreach( $this->Dbo->query( $sql ) as $row ) {
+				$result[$row[0]['name']] = array(
+					'type' => strtoupper( isset( $this->_typemap[$row[0]['type']] ) ? $this->_typemap[$row[0]['type']] : $row[0]['type'] ),
+					'null' => ( $row[0]['null'] === 'YES' ? true : false ),
+					'default' => $row[0]['default'],
+					'length' => $row[0]['length']
+				);
+
+				if( strpos( $result[$row[0]['name']]['default'], 'nextval(' ) !== false ) {
+					$result[$row[0]['name']]['default'] = null;
+				}
+				else if( $result[$row[0]['name']]['default'] !== null ) {
+					$result[$row[0]['name']]['default'] = preg_replace( '/^\'([^\']+)\'.*$/', '\1', $result[$row[0]['name']]['default'] );
+					$result[$row[0]['name']]['default'] = preg_replace( '/^([^\:]+).*$/', '\1', $result[$row[0]['name']]['default'] );
+					if( strtolower( $result[$row[0]['name']]['default'] ) === 'null' ) {
+						$result[$row[0]['name']]['default'] = null;
+					}
+				}
+			}
+
+			return $result;
 		}
 
 		public function getRelations( $tableName ) {
 			$foreignKeys = $this->Dbo->getPostgresForeignKeys(
-				array( "\"From\".\"table_name\" = '{$tableName}'" )
+				array(
+					"\"From\".\"table_name\" = '{$tableName}'",
+					"\"From\".table_schema = '{$this->Dbo->config['schema']}'",
+					"\"To\".\"table_name\" IN ( '".implode("', '", $this->getNames())."' )",
+					"\"To\".table_schema = '{$this->Dbo->config['schema']}'",
+				)
 			);
 
 			return $foreignKeys;
