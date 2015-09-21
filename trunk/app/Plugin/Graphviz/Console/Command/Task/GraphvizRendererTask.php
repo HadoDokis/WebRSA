64e149564a7929692864d6188a6e804f4b183016
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-09-21 12:18:14 +0000

    Améliorations (plugin Graphviz): à présent, le shell Graphviz.graphviz_mpd2 retourne un schéma ressemblant beaucoup plus à un MDP, le type de champ reflète mieux le type de champ en BDD PostgreSQL.

--- trunk/app/Plugin/Graphviz/Console/Command/Task/GraphvizRendererTask.php
+++ trunk/app/Plugin/Graphviz/Console/Command/Task/GraphvizRendererTask.php
@@ -10,7 +10,7 @@
 	 */
 
 	/**
-	 * La classe GraphvizRendererTask ...
+	 * La classe GraphvizRendererTask s'occupe du rendu au format MPD
 	 *
 	 * @package Graphviz
 	 * @subpackage Console.Command.Task
@@ -22,9 +22,24 @@
 		public function renderTable( $summary ) {
 			$label = $summary['Table']['name'];
 
+			$primaryKey = Hash::get( $summary, 'Table.indexes.PRIMARY.column' );
+			$foreignKeys = Hash::extract( $summary, 'Table.relations.{n}.From.column' );
+
 			if( !empty( $this->params['fields'] ) ) {
 				$fields = array();
-				foreach( $summary['Table']['fields'] as $fieldName => $fieldType ) {
+				foreach( $summary['Table']['fields'] as $fieldName => $details ) {
+					if( $fieldName === $primaryKey ) {
+						$fieldName = "{$fieldName} &lt;PK&gt;";
+					}
+					else if( in_array( $fieldName, $foreignKeys ) ) {
+						$fieldName = "{$fieldName} &lt;FK&gt;";
+					}
+
+					$fieldType = $details['type'];
+					if( $details['length'] !== null ) {
+						$fieldType = "{$fieldType}({$details['length']})";
+					}
+
 					$fields[] = "{$fieldName}: {$fieldType}";
 				}
 				$label = '{'.$label.'|'.implode( '\l', $fields ).'\l}';
@@ -39,7 +54,31 @@
 			foreach( $summary['Table']['relations'] as $relation ) {
 				if( in_array( $relation['To']['table'], $this->tables ) ) {
 					$taillabel = $headlabel = null;
-					$return .= "\t\"{$summary['Table']['name']}\" -> \"{$relation['To']['table']}\" [dir=\"forward\", taillabel=\"{$taillabel}\", headlabel=\"{$headlabel}\", label=\"{$relation['Foreignkey']['name']}\", arrowhead=\"normal\"];\n";
+
+					// ex.: personnes, foyers
+					if($relation['From']['nullable'] === false && $relation['From']['unique'] === false) {
+						$headlabel = '0,n';
+						$taillabel = '1,1';
+					}
+					// TODO: nonorientationsproscovs58.dossiercov58_id -> NOT NULL && UNIQUE nonorientationsproscovs58_dossiercov58_id_idx
+					else if($relation['From']['nullable'] === false && $relation['From']['unique'] === true) {
+						$headlabel = '0,1';
+						$taillabel = '1,1';
+					}
+					// ex.: accompagnementscuis66, immersionscuis66
+					else if($relation['From']['nullable'] === true && $relation['From']['unique'] === false) {
+						$headlabel = '0,n';
+						$taillabel = '0,1';
+					}
+					else if($relation['From']['nullable'] === true && $relation['From']['unique'] === true) {
+						$headlabel = '0,1';
+						$taillabel = '0,1';
+					}
+
+					$return .= "\t\"{$relation['Foreignkey']['name']}\" [label=\"{$relation['Foreignkey']['name']}\", shape=ellipse];\n";
+					$return .= "\t\"{$summary['Table']['name']}\" -> \"{$relation['Foreignkey']['name']}\" [dir=\"forward\", taillabel=\"{$taillabel}\", headlabel=\"\", arrowhead=\"none\"];\n";
+					$return .= "\t\"{$relation['Foreignkey']['name']}\" -> \"{$relation['To']['table']}\" [dir=\"forward\", taillabel=\"\", headlabel=\"{$headlabel}\", arrowhead=\"none\"];\n";
+
 				}
 			}
 
