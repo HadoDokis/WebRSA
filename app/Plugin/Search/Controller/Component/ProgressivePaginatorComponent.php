b56b004f98de3aa435badab290f58e3d72d9c7e3
ludodev <ludodev@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-10-14 08:35:58 +0000

    Amélioration : la pagination non progressive dispose maintenant des mêmes aventages que la progressive

--- app/Plugin/Search/Controller/Component/ProgressivePaginatorComponent.php
+++ app/Plugin/Search/Controller/Component/ProgressivePaginatorComponent.php
@@ -9,7 +9,7 @@
 	 * @package Search
 	 * @subpackage Controller.Component
 	 */
-	App::uses( 'PaginatorComponent', 'Controller/Component' );
+	App::uses( 'BasicPaginatorComponent', 'Search.Controller/Component' );
 
 	/**
 	 * La classe Search.ProgressivePaginatorComponent permet d'optimiser la pagination en ne comptant pas le
@@ -18,69 +18,8 @@
 	 * @package Search
 	 * @subpackage Controller.Component
 	 */
-	class ProgressivePaginatorComponent extends PaginatorComponent
+	class ProgressivePaginatorComponent extends BasicPaginatorComponent
 	{
-
-		/**
-		 * Surcharge de la méthode validateSort pour que les champs virtuels soient pris en compte pour le tri
-		 * TODO: à reporter dans le paginator "normal"
-		 *
-		 * @see CakePHP 2.2.2
-		 *
-		 * @param Model $object
-		 * @param array $options
-		 * @param array $whitelist
-		 * @return array
-		 */
-		public function validateSort( $object, $options, $whitelist = array( ) ) {
-			if( isset( $options['sort'] ) ) {
-				$direction = null;
-				if( isset( $options['direction'] ) ) {
-					$direction = strtolower( $options['direction'] );
-				}
-				if( $direction != 'asc' && $direction != 'desc' ) {
-					$direction = 'asc';
-				}
-				$options['order'] = array( $options['sort'] => $direction );
-			}
-
-			if( !empty( $whitelist ) && isset( $options['order'] ) && is_array( $options['order'] ) ) {
-				$field = key( $options['order'] );
-				if( !in_array( $field, $whitelist ) ) {
-					$options['order'] = null;
-				}
-			}
-
-			if( !empty( $options['order'] ) && is_array( $options['order'] ) ) {
-				$order = array( );
-				foreach( $options['order'] as $key => $value ) {
-					$field = $key;
-					$alias = $object->alias;
-					if( strpos( $key, '.' ) !== false ) {
-						list($alias, $field) = explode( '.', $key );
-					}
-
-					if( $object->hasField( $field ) ) {
-						$order[$alias.'.'.$field] = $value;
-					}
-					elseif( $object->hasField( $key, true ) ) {
-						$order[$field] = $value;
-					}
-					elseif( isset( $object->{$alias} ) && $object->{$alias}->hasField( $field, true ) ) {
-						$order[$alias.'.'.$field] = $value;
-					}
-					// Début modification
-					else {
-						$order[$key] = $value;
-					}
-					// Fin modification
-				}
-				$options['order'] = $order;
-			}
-
-			return $options;
-		}
-
 		/**
 		 * Surcharge de la méthode paginate pour obtenir la pagination progressive
 		 *
