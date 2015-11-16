4612ab5ec23f94669e9458a8a892200c7b52994e
ludodev <ludodev@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-11-16 09:30:30 +0000

    Amélioration : moteur de recherche par Non orientation

--- trunk/app/Config/Cg66/Nonorientationsproseps.php
+++ trunk/app/Config/Cg66/Nonorientationsproseps.php
@@ -1,20 +1,44 @@
 <?php
+	/**
+	 * Menu "Recherches"
+	 */
 	Configure::write(
-		'Filtresdefaut.Nonorientationsproseps_search',
+		'ConfigurableQuery.Nonorientationsproseps.search',
 		array(
-			'Pagination' => array(
-				'nombre_total' => 0
+			// 1. Filtres de recherche
+			'filters' => array(
+				// 1.1 Valeurs par défaut des filtres de recherche
+				'defaults' => array(
+					'Dossier' => array(
+						// Case à cocher "Uniquement la dernière demande RSA pour un même allocataire"
+						'dernier' => '1'
+					)
+				),
+				// 1.2 Restriction des valeurs qui apparaissent dans les filtres de recherche
+				'accepted' => array(),
+				// 1.3 Ne pas afficher ni traiter certains filtres de recherche
+				'skip' => array(),
+				// 1.4 Filtres additionnels : La personne possède un(e)...
+				'has' => array()
 			),
-			'Dossier' => array(
-				'dernier' => '1'
-			)
-		)
-	);
-
-	Configure::write(
-		'ConfigurableQueryNonorientationsproseps',
-		array(
-			'search' => array(
+			// 2. Recherche
+			'query' => array(
+				// 2.1 Restreindre ou forcer les valeurs renvoyées par le filtre de recherche
+				'restrict' => array(),
+				// 2.2 Conditions supplémentaires optionnelles
+				'conditions' => array(),
+				// 2.3 Tri par défaut
+				'order' => array("(DATE_PART('day', NOW() - Contratinsertion.df_ci))" => 'DESC')
+			),
+			// 3. Nombre d'enregistrements par page
+			'limit' => 10,
+			// 4. Lancer la recherche au premier accès à la page ?
+			'auto' => false,
+			// 5. Résultats de la recherche
+			'results' => array(
+				// 5.1 Ligne optionnelle supplémentaire d'en-tête du tableau de résultats
+				'header' => array(),
+				// 5.2 Colonnes du tableau de résultats
 				'fields' => array (
 					'Dossier.numdemrsa',
 					'Personne.nom_complet',
@@ -28,12 +52,14 @@
 					'Referent.nom_complet',
 					'/Rendezvous/index/#Personne.id#' => array( 'class' => 'view' ),
 				),
+				// 5.3 Infobulle optionnelle du tableau de résultats
 				'innerTable' => array(
 					'Structurereferenteparcours.lib_struc',
 					'Referentparcours.nom_complet'
-				),
-				'order' => array( "(DATE_PART('day', NOW() - Contratinsertion.df_ci))" => 'DESC' )
+				)
 			),
+			// 6. Temps d'exécution, mémoire maximum, ...
+			'ini_set' => array()
 		)
 	);
 ?>
