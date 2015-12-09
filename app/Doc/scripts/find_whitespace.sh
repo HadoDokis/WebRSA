325a07b1fb61d7f4915d5613ec240fad86210c57
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2015-12-09 11:01:21 +0000

    Amélioration: à présent le script utilitaire find_whitespace.sh détectera également les fichiers ayant un espace blanc tout au début du fichier.

--- app/Doc/scripts/find_whitespace.sh
+++ app/Doc/scripts/find_whitespace.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Petit script bash permettant de détecter les fichiers .php, .inc et .ctp
-# contenant uniquement des espaces blancs après la balise fermante php (?>).
+# contenant uniquement des espaces blancs après la balise fermante php (?>) ou
+# contenant des espaces blancs en début de fichier
 # Nécessite le programme pcregrep
 # @link http://www.johnnyslink.com/technology/2012/06/15/use-grep-find-php-files-white-space
 
@@ -18,5 +19,12 @@ APP_DIR="`dirname "$ME"`/../.."
 
 find "$APP_DIR" -type f -iregex '.*\.\(php\|inc\|ctp\)' \! -path \*/\.svn/\* | while read -r ;
 do
+	# Espace(s) en début de fichier
+	head -n1 "$REPLY" | pcregrep -M '\A\s+<\?' >> /dev/null
+	if [ "$?" -eq "0" ] ; then
+		echo "$REPLY"
+	fi
+
+	# Espace(s) en fin de fichier
 	pcregrep -Ml '\?>\s\s+\z' "$REPLY"
 done
