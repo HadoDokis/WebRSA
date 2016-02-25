9b66d35aea6f89632db13f9f1669defe24370786
ludodev <ludodev@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-02-25 15:09:04 +0000

    Correction : ticket #14429 intégration des courriers à imprimer dans la cohorte d'impression des dossiers PCG meme si il n'y a pas de lien avec une décision (CG 66)

--- trunk/app/Model/WebrsaCohorteDossierpcg66Imprimer.php
+++ trunk/app/Model/WebrsaCohorteDossierpcg66Imprimer.php
@@ -23,5 +23,43 @@
 		 * @var string
 		 */
 		public $name = 'WebrsaCohorteDossierpcg66Imprimer';
+		
+		/**
+		 * Retourne le querydata de base, en fonction du département, à utiliser
+		 * dans le moteur de recherche.
+		 *
+		 * @param array $types Les types de jointure alias => type
+		 * @return array
+		 */
+		public function searchQuery( array $types = array() ) {
+			$types += array(
+				'Typepdo' => 'INNER',
+				'Originepdo' => 'INNER',
+				'Serviceinstructeur' => 'LEFT OUTER',
+			);
+			
+			$query = $this->WebrsaRechercheDossierpcg66->searchQuery( $types );
+			
+			$query['conditions'] = array(
+				'OR' => array(
+					array(
+						'Decisiondossierpcg66.etatdossierpcg' => null,
+						'Dossierpcg66.etatdossierpcg' => 'decisionvalid',
+						'Dossierpcg66.dateimpression' => null
+					),
+					array(
+						'Dossierpcg66.id IN ('
+						. 'SELECT a.id FROM dossierspcgs66 AS a '
+						. 'INNER JOIN personnespcgs66 AS b ON a.id = b.dossierpcg66_id '
+						. 'INNER JOIN traitementspcgs66 AS c ON c.personnepcg66_id = b.id '
+						. 'WHERE a.id = "Dossierpcg66"."id" '
+						. 'AND c.imprimer = 1 '
+						. 'AND c.etattraitementpcg = \'imprimer\')'
+					)
+				)
+			);
+			
+			return $query;
+		}
 	}
 ?>
