9fd841b78f00401c5980fd3ea528acc786b11a73
ludodev <ludodev@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-03-22 16:48:01 +0000

    Amélioration : ajout du Plugin SuperFixture

---
+++ trunk/app/Plugin/SuperFixture/Test/SuperFixture/FooBazSuperFixture.php
@@ -0,0 +1,43 @@
+<?php
+	/**
+	 * Code source de la classe SuperFixture.
+	 *
+	 * @package SuperFixture
+	 * @subpackage Utility
+	 */
+
+	App::uses('SuperFixtureInterface', 'SuperFixture.Interface');
+
+	/**
+	 * SuperFixture
+	 *
+	 * @package SuperFixture
+	 * @subpackage Utility
+	 */
+	class FooBazSuperFixture implements SuperFixtureInterface {
+		/**
+		 * Permet d'obtenir les informations nécéssaire pour charger la SuperFixture
+		 * 
+		 * @return array
+		 */
+		public static function getData() {
+			return array(
+				'SuperFixtureFoo' => array(
+					1 => array(
+						'name' => 'Foo 1',
+						'super_fixture_bar_id' => 1
+					),
+					2 => array(
+						'name' => 'Foo 2',
+						'super_fixture_bar_id' => 1
+					),
+				),
+				'SuperFixtureBar' => array(
+					1 => array(
+						'name' => 'Bar',
+						'super_fixture_baz_id' => 1
+					)
+				),
+			);
+		}
+	}
--- trunk/app/Test/Fixture/AvispcgdroitrsaFixture.php
+++ trunk/app/Test/Fixture/AvispcgdroitrsaFixture.php
@@ -1,20 +1,19 @@
 <?php
 	/**
-	 * Code source de la classe CalculdroitrsaFixture.
+	 * Code source de la classe AvispcgdroitrsaFixture.
 	 *
 	 * PHP 5.3
 	 *
 	 * @package app.Test.Fixture
 	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
 	 */
-	require_once( dirname( __FILE__ ).DS.'cake_app_test_fixture.php' );
 
 	/**
-	 * Classe CalculdroitrsaFixture.
+	 * Classe AvispcgdroitrsaFixture.
 	 *
 	 * @package app.Test.Fixture
 	 */
-	class CalculdroitrsaFixture extends CakeTestFixture
+	class AvispcgdroitrsaFixture extends CakeTestFixture
 	{
 		/**
 		 * On importe la définition de la table, pas les enregistrements.
@@ -22,7 +21,7 @@
 		 * @var array
 		 */
 		public $import = array(
-			'model' => 'Calculdroitrsa',
+			'model' => 'Avispcgdroitrsa',
 			'records' => false
 		);
 
@@ -32,13 +31,6 @@
 		 * @var array
 		 */
 		public $records = array(
-			array(
-				'personne_id' => 1,
-				'mtpersressmenrsa' => null,
-				'mtpersabaneursa' => null,
-				'toppersdrodevorsa' => '1',
-				'toppersentdrodevorsa' => null
-			)
 		);
 
 	}
