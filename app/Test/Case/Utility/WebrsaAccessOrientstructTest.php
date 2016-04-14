5c5c632556e7b6ccd2dee79ef3bf1912994940d2
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-04-14 08:47:36 +0000

    Amélioration: ajout du brouillon de la classe utilitaire WebrsaAccessOrientstruct (et des tests unitaires associés) qui contient la logique métier d'accès aux différentes actions possibles des enregistrements de la table orientsstructs.

---
+++ app/Test/Case/Utility/WebrsaAccessOrientstructTest.php
@@ -0,0 +1,88 @@
+<?php
+	/**
+	 * Code source de la classe WebrsaAccessOrientstructTest.
+	 *
+	 * PHP 5.3
+	 *
+	 * @package app.Test.Case.Model
+	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
+	 */
+	App::uses( 'WebrsaAccessOrientstruct', 'Utility' );
+
+	/**
+	 * La classe WebrsaAccessOrientstructTest réalise les tests unitaires de la classe WebrsaAccessOrientstruct.
+	 *
+	 * @package app.Test.Case.Model
+	 */
+	class WebrsaAccessOrientstructTest extends CakeTestCase
+	{
+		/**
+		 * Fixtures utilisés.
+		 *
+		 * @var array
+		 */
+		public $fixtures = array();
+
+		/**
+		 * Test de la méthode WebrsaAccessOrientstruct::access()
+		 *
+		 * @covers WebrsaAccessOrientstruct::access
+		 */
+		public function testAccess() {
+			Configure::write( 'Cg.departement', 58 );
+			$record = array(
+				'Orientstruct' => array(
+					'rgorient' => 1,
+					'printable' => true,
+					'linked_records' => true,
+					'dernier' => true,
+					'dernier_oriente' => true
+				)
+			);
+			$params = array(
+				'ajout_possible' => true,
+				'reorientationseps' => false
+			);
+			$result = WebrsaAccessOrientstruct::access( $record, $params );
+			$expected = array(
+				'Orientstruct' => array(
+					'rgorient' => 1,
+					'printable' => true,
+					'linked_records' => true,
+					'dernier' => true,
+					'dernier_oriente' => true,
+					'edit' => true,
+					'impression' => true,
+					'delete' => false,
+				)
+			);
+			$this->assertEqual( $result, $expected, var_export( $result, true ) );
+		}
+
+		/**
+		 * @covers WebrsaAccessOrientstruct::check
+		 */
+		public function testCheck() {
+			Configure::write( 'Cg.departement', 58 );
+			$record = array(
+				'Orientstruct' => array(
+					'rgorient' => 1,
+					'printable' => true,
+					'linked_records' => true,
+				)
+			);
+			$params = array(
+				'ajout_possible' => true,
+				'reorientationseps' => false
+			);
+
+			// Défini
+			$result = WebrsaAccessOrientstruct::check( 'impression', $record, $params );
+			$this->assertTrue( $result );
+
+			// Non défini
+			$result = WebrsaAccessOrientstruct::check( 'foo', $record, $params );
+			$this->assertFalse( $result );
+		}
+	}
+?>
