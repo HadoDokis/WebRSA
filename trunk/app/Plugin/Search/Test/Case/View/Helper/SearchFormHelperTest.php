fa0a46ed43522fd76ef65f1dac518bec2a6e6424
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-04-14 14:20:10 +0000

    Correction, amélioration: à présent, les boutons "Tout cocher" / "Tout décocher" des cases à cocher de la partie "Numéro de commune au sens INSEE" sont de type "button", ce qui signifie qu'ils ne seront pas activés lorsque l'utilisateur appuie sur "entrée" lorsqu'il est dans un champ de formulaire. Lorsque l'on coche la case "Filtrer par numéro de commune au sens INSEE", alors les cases à cocher des zones géographiques seront toutes automatiquement cochées. En lien avec le ticket #8795.

--- trunk/app/Plugin/Search/Test/Case/View/Helper/SearchFormHelperTest.php
+++ trunk/app/Plugin/Search/Test/Case/View/Helper/SearchFormHelperTest.php
@@ -120,7 +120,37 @@ document.observe( \'dom:loaded\', function() { observeDisableFieldsetOnCheckbox(
 			$options = array( '2' => 'Ouvert', '6' => 'Clos' );
 
 			$result = $this->SearchForm->dependantCheckboxes( 'Search.Dossier.etatdosrsa', array( 'options' => $options, 'buttons' => true ) );
-			$expected = '<div class="input checkbox"><input type="hidden" name="data[Search][Dossier][etatdosrsa_choice]" id="SearchDossierEtatdosrsaChoice_" value="0"/><input type="checkbox" name="data[Search][Dossier][etatdosrsa_choice]"  value="1" id="SearchDossierEtatdosrsaChoice"/><label for="SearchDossierEtatdosrsaChoice">Search.Dossier.etatdosrsa_choice</label></div><fieldset id="SearchDossierEtatdosrsaFieldset"><legend>Search.Dossier.etatdosrsa</legend><div class="buttons"><button onclick="try { toutCocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); } catch( e ) { console.log( e ); }; return false;" type="submit">Tout cocher</button><button onclick="toutDecocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); return false;" type="submit">Tout décocher</button></div><div class="input select"><input type="hidden" name="data[Search][Dossier][etatdosrsa]" value="" id="SearchDossierEtatdosrsa"/>
+			$expected = '<div class="input checkbox"><input type="hidden" name="data[Search][Dossier][etatdosrsa_choice]" id="SearchDossierEtatdosrsaChoice_" value="0"/><input type="checkbox" name="data[Search][Dossier][etatdosrsa_choice]"  value="1" id="SearchDossierEtatdosrsaChoice"/><label for="SearchDossierEtatdosrsaChoice">Search.Dossier.etatdosrsa_choice</label></div><fieldset id="SearchDossierEtatdosrsaFieldset"><legend>Search.Dossier.etatdosrsa</legend><div class="buttons"><button type="button" onclick="try { toutCocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); } catch( e ) { console.log( e ); }; return false;">Tout cocher</button><button type="button" onclick="toutDecocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); return false;">Tout décocher</button></div><div class="input select"><input type="hidden" name="data[Search][Dossier][etatdosrsa]" value="" id="SearchDossierEtatdosrsa"/>
+
+<div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="2" id="SearchDossierEtatdosrsa2" /><label for="SearchDossierEtatdosrsa2">Ouvert</label></div>
+<div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="6" id="SearchDossierEtatdosrsa6" /><label for="SearchDossierEtatdosrsa6">Clos</label></div>
+</div></fieldset><script type="text/javascript">
+//<![CDATA[
+document.observe( \'dom:loaded\', function() { observeDisableFieldsetOnCheckbox( \'SearchDossierEtatdosrsaChoice\', \'SearchDossierEtatdosrsaFieldset\', false, false ); } );
+//]]>
+</script>';
+			$this->assertEquals( $result, $expected, var_export( $result, true ) );
+		}
+
+		/**
+		 * Test de la méthode SearchFormHelper::dependantCheckboxes() avec coche
+		 * automatique losque l'on coche le checkbox parent.
+		 *
+		 * @medium
+		 */
+		public function testDependantCheckboxesAutoCheck() {
+			$this->Controller->request->addParams(
+				array(
+					'controller' => 'users',
+					'action' => 'index',
+					'pass' => array( ),
+					'named' => array( )
+				)
+			);
+			$options = array( '2' => 'Ouvert', '6' => 'Clos' );
+
+			$result = $this->SearchForm->dependantCheckboxes( 'Search.Dossier.etatdosrsa', array( 'options' => $options, 'buttons' => true, 'autoCheck' => true ) );
+			$expected = '<div class="input checkbox"><input type="hidden" name="data[Search][Dossier][etatdosrsa_choice]" id="SearchDossierEtatdosrsaChoice_" value="0"/><input type="checkbox" name="data[Search][Dossier][etatdosrsa_choice]"  onclick="try { toutCocher( &#039;input[name=\\&#039;data[Search][Dossier][etatdosrsa][]\\&#039;]&#039; ); } catch( e ) { console.log( e ); };" value="1" id="SearchDossierEtatdosrsaChoice"/><label for="SearchDossierEtatdosrsaChoice">Search.Dossier.etatdosrsa_choice</label></div><fieldset id="SearchDossierEtatdosrsaFieldset"><legend>Search.Dossier.etatdosrsa</legend><div class="buttons"><button type="button" onclick="try { toutCocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); } catch( e ) { console.log( e ); }; return false;">Tout cocher</button><button type="button" onclick="toutDecocher( \'input[name=\\\'data[Search][Dossier][etatdosrsa][]\\\']\' ); return false;">Tout décocher</button></div><div class="input select"><input type="hidden" name="data[Search][Dossier][etatdosrsa]" value="" id="SearchDossierEtatdosrsa"/>
 
 <div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="2" id="SearchDossierEtatdosrsa2" /><label for="SearchDossierEtatdosrsa2">Ouvert</label></div>
 <div class="checkbox"><input type="checkbox" name="data[Search][Dossier][etatdosrsa][]" value="6" id="SearchDossierEtatdosrsa6" /><label for="SearchDossierEtatdosrsa6">Clos</label></div>
