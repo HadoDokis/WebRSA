cd87e40e28912d3f0b68324851aad478c06a5b9a
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-08-31 07:48:48 +0000

    Qualité du code: ajout de commentaires et de tests unitaires pour la classe WebrsaStructurereferentelieeBehavior.

--- app/Model/Behavior/WebrsaStructurereferentelieeBehavior.php
+++ app/Model/Behavior/WebrsaStructurereferentelieeBehavior.php
@@ -7,43 +7,41 @@
 	 * @package app.Model.Behavior
 	 * @license CeCiLL V2 (http://www.cecill.info/licences/Licence_CeCILL_V2-fr.html)
 	 */
+
 	/**
-	 * La classe WebrsaStructurereferentelieeBehavior ...
+	 * La classe WebrsaStructurereferentelieeBehavior fournit des méthodes
+	 * utilitaires permettant d'ajouter des champs ou des conditions à un querydata
+	 * lorsque le modèle métier auquel il est attaché est lié à une structure
+	 * référente ou à un référent.
+	 *
+	 * @todo liens indirects, comme Questionnaired1pdv93, Questionnaired2pdv93, ...
 	 *
 	 * @package app.Model.Behavior
 	 */
 	class WebrsaStructurereferentelieeBehavior extends ModelBehavior
 	{
-
 		/**
-		 * Contains configuration settings for use with individual model objects. This
-		 * is used because if multiple models use this Behavior, each will use the same
-		 * object instance. Individual model settings should be stored as an
-		 * associative array, keyed off of the model name.
-		 *
-		 * @var array
-		 * @see Model::$alias
-		 */
-		public $settings = array();
-
-		/**
-		 * "Live" cache pour les clés étrangères.
+		 * "Live" cache par nom de modèle et de méthode.
 		 *
 		 * @var array
 		 */
 		protected $_cache = array();
 
 		/**
-		 * Setup this behavior with the specified configuration settings.
+		 * Retourne un champ virtuel aliasé ou une condition indiquant si la
+		 * structure référente à laquelle sont liés les enregistrements du modèle
+		 * fait partie des structures référentes passées en paramètre.
 		 *
-		 * @param Model $Model Model using this behavior
-		 * @param array $config Configuration settings for $model
-		 * @return void
+		 * @param Model $Model Le modèle auquel le behavior est attaché
+		 * @param string $fieldName Le champ par lequel la table est liée à la
+		 *	table structuresreferentes
+		 * @param array $structuresreferentes_ids Les ids des structures référentes
+		 *	à vérifier
+		 * @param array $params La clé "alias" (par défaut Referent.horszone)
+		 *	permet de spécifier un alias pouyr le champ; une valeur false ou null
+		 *	permet de ne pas en spécifier
+		 * @return string
 		 */
-		public function setup( Model $Model, $config = array() ) {
-			parent::setup( $Model, $config );
-		}
-
 		public function structurereferenteHorszone( Model $Model, $fieldName, array $structuresreferentes_ids, array $params = array() ) {
 			$params += array(
 				'alias' => 'Referent.horszone'
@@ -74,6 +72,22 @@
 			return $result;
 		}
 
+		/**
+		 *
+		 * Retourne un champ virtuel aliasé ou une condition indiquant si le
+		 * référent auquel sont liés les enregistrements du modèle appartient aux
+		 * structures référentes passées en paramètre.
+		 *
+		 * @param Model $Model Le modèle auquel le behavior est attaché
+		 * @param string $fieldName Le champ par lequel la table est liée à la
+		 *	table referents
+		 * @param array $structuresreferentes_ids Les ids des structures référentes
+		 *	à vérifier
+		 * @param array $params La clé "alias" (par défaut Referent.horszone)
+		 *	permet de spécifier un alias pouyr le champ; une valeur false ou null
+		 *	permet de ne pas en spécifier.
+		 * @return string
+		 */
 		public function referentHorszone( Model $Model, $fieldName, array $structuresreferentes_ids, array $params = array() ) {
 			$params += array(
 				'alias' => 'Referent.horszone'
@@ -108,6 +122,22 @@
 			return $result;
 		}
 
+		/**
+		 * Complète un querydata avec un champ virtuel indiquant si l'enregistrement
+		 * est lié (via la structure référente ou le référent) aux structures
+		 * référentes passées en paramètres.
+		 *
+		 * @param Model $Model Le modèle auquel le behavior est attaché.
+		 * @param array $query Le querydata à compléter
+		 * @param array $structuresreferentes_ids Les ids des structures référentes
+		 *	à vérifier
+		 * @param array $params La clé "alias" (par défaut Referent.horszone)
+		 *	permet de spécifier un alias pouyr le champ; une valeur false ou null
+		 *	permet de ne pas en spécifier; les clés "structurereferente_id" et
+		 *	"referent_id" permettent de spécifier quel(s) champ(s) contiennent
+		 *	les clés étrangères vers les tables structuresreferentes et referents.
+		 * @return array
+		 */
 		public function completeQueryHorsZone( Model $Model, array $query, $structuresreferentes_ids = null, array $params = array() ) {
 			$structuresreferentes_ids = (array)$structuresreferentes_ids;
 			$params += array(
@@ -130,8 +160,13 @@
 			return $query;
 		}
 
-		// Clés étrangères vers les tables structuresreferentes et referents ?
-		// FIXME: liens indirects, comme Questionnaired1pdv93 / Questionnaired2pdv93
+		/**
+		 * Retourne les clés étrangères vers les tables structuresreferentes et
+		 * referents, dans les clés "structurereferente_id" et "referent_id".
+		 *
+		 * @param Model $Model Le modèle auquel le behavior est attaché.
+		 * @return array
+		 */
 		public function links( Model $Model ) {
 			if( false === isset( $this->_cache[$Model->name][__FUNCTION__] ) ) {
 				if( false === $Model->Behaviors->attached( 'Postgres.PostgresTable' ) ) {
