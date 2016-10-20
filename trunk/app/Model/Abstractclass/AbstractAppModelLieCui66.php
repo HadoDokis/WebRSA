035a1322f091d20febd0ab037efcc341db04d30a
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-10-20 13:16:43 +0000

    Qualité du code: ajout des App::uses manquants de la classe parente pour le répertoire Model/Abstractclass.

--- trunk/app/Model/Abstractclass/AbstractAppModelLieCui66.php
+++ trunk/app/Model/Abstractclass/AbstractAppModelLieCui66.php
@@ -4,6 +4,7 @@
 	 *
 	 * @package app.Model
 	 */
+	App::uses( 'AppModel', 'Model' );
 
 	/**
 	 * La classe AbstractAppModelLieCui66 ...
@@ -13,19 +14,19 @@
 	abstract class AbstractAppModelLieCui66 extends AppModel
 	{
 		/**
-		 * Recurcivité du model 
+		 * Recurcivité du model
 		 * @var integer
 		 */
 		public $recursive = -1;
-		
+
 		/**
 		 * Order des find par défaut
-		 * @var type 
+		 * @var type
 		 */
 		public $order =  array(
 			'created' => 'DESC'
 		);
-		
+
 		/**
 		 * Possède des clefs étrangères vers d'autres models
 		 * @var array
@@ -37,7 +38,7 @@
 				'dependent' => true,
 			),
         );
-		
+
 		/**
 		 * Ces models possèdent une clef étrangère vers ce model
 		 * @var array
@@ -57,7 +58,7 @@
 				'counterQuery' => ''
 			),
 		);
-		
+
 		/**
 		 * Behaviors utilisés par le modèle.
 		 *
@@ -68,7 +69,7 @@
 			'Postgres.PostgresAutovalidate',
 			'Validation2.Validation2Formattable',
 		);
-		
+
 		/**
 		* Chemin relatif pour les modèles de documents .odt utilisés lors des
 		* impressions. Utiliser %s pour remplacer par l'alias.
@@ -76,30 +77,30 @@
 		public $modelesOdt = array(
 			'default' => 'CUI/%s/impression.odt',
 		);
-		
+
 		/**
 		 * Constructeur
 		 * Défini une relation hasMany avec Fichiermodule en fonction de l'alias de l'enfant
 		 * Défini un order par défaut avec l'alias de l'enfant
-		 * 
+		 *
 		 * @param type $id
 		 * @param type $table
 		 * @param type $ds
 		 */
 		public function __construct($id = false, $table = null, $ds = null) {
 			parent::__construct($id, $table, $ds);
-			
+
 			$this->order = array( $this->alias . '.created' => 'DESC' );
-			
+
 			$this->hasMany['Fichiermodule']['conditions'] = array(
 				'Fichiermodule.modele = \'' . $this->alias . '\'',
 				'Fichiermodule.fk_value = {$__cakeID__$}'
 			);
 		}
-		
+
 		/**
 		 * Récupère les donnés par defaut dans le cas d'un ajout, ou récupère les données stocké en base dans le cas d'une modification
-		 * 
+		 *
 		 * @param integer $cui66_id
 		 * @param integer $id
 		 * @return array
@@ -118,17 +119,17 @@
 				$query = $this->queryView( $id, false );
 				$result = $this->find( 'first', $query );
 			}
-			
+
 			if ( empty($result) ){
 				throw new HttpException(404, "HTTP/1.1 404 Not Found");
 			}
 
 			return $result;
 		}
-		
+
 		/**
 		 * FIXME: doc
-		 * 
+		 *
 		 * @param type $cui66_id
 		 * @return string
 		 */
@@ -139,28 +140,28 @@
 					$this->alias . '.cui66_id' => $cui66_id
 				)
 			);
-					
+
 			return $query;
 		}
-		
+
 		/**
 		 * Sauvegarde du formulaire
-		 * 
+		 *
 		 * @param array $data
 		 * @return boolean
 		 */
 		public function saveAddEditFormData( array $data, $user_id = null ) {
 			$data[$this->alias]['user_id'] = $user_id;
-			
+
 			$this->create($data);
 			$success = $this->save();
-			
+
 			return $success;
 		}
-		
+
 		/**
 		 * Query utilisé pour la visualisation
-		 * 
+		 *
 		 * @param integer $id
 		 * @return array
 		 */
