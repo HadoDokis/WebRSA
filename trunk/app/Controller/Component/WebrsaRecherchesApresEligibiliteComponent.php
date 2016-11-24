4586aee4df1461ba5a772317b56a2367351ca433
cbuffin <cbuffin@3778e0a8-b4ff-47d2-9f70-5e749816a60c>
2016-11-24 16:12:15 +0000

    Correction: dans le nouveau moteur de recherche par "APRE" > "Liste des demandes d'APRE" > "Eligibilité des APREs", il y avait des notices et la colonne "Date de relance" n'avait pas la bonne valeur.

--- trunk/app/Controller/Component/WebrsaRecherchesApresEligibiliteComponent.php
+++ trunk/app/Controller/Component/WebrsaRecherchesApresEligibiliteComponent.php
@@ -16,5 +16,49 @@
 	 */
 	class WebrsaRecherchesApresEligibiliteComponent extends WebrsaRecherchesApresComponent
 	{
+		/**
+		 * Stocke les anciennes valeurs des attributs deepAfterFind des différents
+		 * modèles.
+		 *
+		 * @var array
+		 */
+		protected $_deepAfterFind = array();
+
+		/**
+		 * Stocke les anciennes valeurs et modifie les valeurs des attributs
+		 * deepAfterFind des modèles Apre et Relanceapre à false avant la requête.
+		 *
+		 * @param array $params Les paramètres de la recherche
+		 * @param array $query Le querydata à compléter
+		 * @return array
+		 */
+		public function beforeSearch( array $params, array $query ) {
+			$Controller = $this->_Collection->getController();
+
+			$this->_deepAfterFind['Apre'] = $Controller->{$params['modelName']}->deepAfterFind;
+			$this->_deepAfterFind['Relanceapre'] = $Controller->{$params['modelName']}->Relanceapre->deepAfterFind;
+
+			$Controller->{$params['modelName']}->deepAfterFind = false;
+			$Controller->{$params['modelName']}->Relanceapre->deepAfterFind = false;
+
+			return $query;
+		}
+
+		/**
+		 * Modifie les valeurs des attributs deepAfterFind des modèles Apre et
+		 * Relanceapre à leur ancienne valeur après la requête.
+		 *
+		 * @param array $params Les paramètres de la recherche
+		 * @param array $results Les enregistrements à compléter
+		 * @return array
+		 */
+		public function afterSearch( array $params, array $results ) {
+			$Controller = $this->_Collection->getController();
+
+			$Controller->{$params['modelName']}->deepAfterFind = $this->_deepAfterFind[$params['modelName']];
+			$Controller->{$params['modelName']}->Relanceapre->deepAfterFind = $this->_deepAfterFind['Relanceapre'];
+
+			return $results;
+		}
 	}
 ?>
